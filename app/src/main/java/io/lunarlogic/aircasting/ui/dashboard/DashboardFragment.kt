dd1fcf16f28601b6d2e1a568eafb90c3531e07e1
Anna Ślimak <slimakania@gmail.com>
2020-04-03 15:29:46 +0200

    Dummy empty dashboard view

--- app/src/main/java/io/lunarlogic/aircasting/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/io/lunarlogic/aircasting/ui/dashboard/DashboardFragment.kt
@@ -4,28 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.content.Intent
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import io.lunarlogic.aircasting.R
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 
 class DashboardFragment : Fragment() {
-
-    private lateinit var dashboardViewModel: DashboardViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-            ViewModelProviders.of(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        dashboard_record_new_session_button.setOnClickListener {
+            activity?.let{
+                val selectDeviceIntent = Intent(it, SelectDeviceActivity::class.java)
+                it.startActivity(selectDeviceIntent)
+            }
+        }
     }
 }
---
+++ app/src/main/java/io/lunarlogic/aircasting/ui/dashboard/SelectDeviceActivity.kt
@@ -0,0 +1,13 @@
+package io.lunarlogic.aircasting.ui.dashboard
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.lunarlogic.aircasting.R
+
+class SelectDeviceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_device)
+    }
+}
