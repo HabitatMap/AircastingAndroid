5715474ef1b5881d63590e722ce11c51ba4f6ac4
Maria Turnau-Gurgul <maria.turnau@lunarlogic.io>
2020-07-29 15:06:21 +0200

    Styles for mobile/fixed session cards.

--- app/src/main/java/io/lunarlogic/aircasting/screens/new_session/select_session_type/SelectSessionTypeViewMvcImpl.kt
+++ app/src/main/java/io/lunarlogic/aircasting/screens/new_session/select_session_type/SelectSessionTypeViewMvcImpl.kt
@@ -1,6 +1,7 @@
 package io.lunarlogic.aircasting.screens.new_session.select_session_type
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import io.lunarlogic.aircasting.R
@@ -11,12 +12,12 @@ class SelectSessionTypeViewMvcImpl : BaseObservableViewMvc<SelectSessionTypeView
         inflater: LayoutInflater, parent: ViewGroup?): super() {
         this.rootView = inflater.inflate(R.layout.fragment_select_session_type, parent, false)
 
-        val fixedSessionButton = rootView?.findViewById<Button>(R.id.fixed_session_button)
+        val fixedSessionButton = rootView?.findViewById<View>(R.id.fixed_session_start_card)
         fixedSessionButton?.setOnClickListener {
             onFixedSessionSelected()
         }
 
-        val mobileSessionButton = rootView?.findViewById<Button>(R.id.mobile_session_button)
+        val mobileSessionButton = rootView?.findViewById<View>(R.id.mobile_session_start_card)
         mobileSessionButton?.setOnClickListener {
             onMobileSessionSelected()
         }
