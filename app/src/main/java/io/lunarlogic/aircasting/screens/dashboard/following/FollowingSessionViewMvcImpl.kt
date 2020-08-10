01a7e70f08aaffb3753b4fc1a3add0fc037f45ca
Anna Ślimak <slimakania@gmail.com>
2020-08-10 13:02:06 +0200

    More actions button should not be visible on the following tab

--- app/src/main/java/io/lunarlogic/aircasting/screens/dashboard/following/FollowingSessionViewMvcImpl.kt
+++ app/src/main/java/io/lunarlogic/aircasting/screens/dashboard/following/FollowingSessionViewMvcImpl.kt
@@ -1,18 +1,27 @@
 package io.lunarlogic.aircasting.screens.dashboard.following
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.FragmentManager
+import io.lunarlogic.aircasting.R
 import io.lunarlogic.aircasting.screens.common.BottomSheet
 import io.lunarlogic.aircasting.screens.dashboard.ActiveSessionViewMvcImpl
 
-class FollowingSessionViewMvcImpl(
-    inflater: LayoutInflater,
-    parent: ViewGroup,
-    supportFragmentManager: FragmentManager
-):
-    ActiveSessionViewMvcImpl<FollowingSessionViewMvc.Listener>(inflater, parent, supportFragmentManager),
+class FollowingSessionViewMvcImpl:
+    ActiveSessionViewMvcImpl<FollowingSessionViewMvc.Listener>,
     FollowingSessionViewMvc {
+
+    constructor(
+        inflater: LayoutInflater,
+        parent: ViewGroup,
+        supportFragmentManager: FragmentManager
+    ): super(inflater, parent, supportFragmentManager) {
+        val actionsView = this.rootView?.findViewById<ImageView>(R.id.session_actions_button)
+        actionsView?.visibility = View.INVISIBLE
+    }
+
     override fun buildBottomSheet(): BottomSheet? {
         return null;
     }
