c42a90145a435819ed100ada0a0461b5bdf54f69
Anna Ślimak <slimakania@gmail.com>
2020-08-17 10:57:58 +0200

    Added useful comment

--- app/src/main/java/io/lunarlogic/aircasting/sensor/Measurement.kt
+++ app/src/main/java/io/lunarlogic/aircasting/sensor/Measurement.kt
@@ -20,6 +20,16 @@ class Measurement(
                 measurementDBObject.longitude
             )
 
+
+/*
+    T1..T5 are integer thresholds which guide how values should be displayed:
+    - lower than T1 - extremely low / won't be displayed - level = null
+    - between T1 and T2 - low / green - level = 0
+    - between T2 and T3 - medium / yellow - level = 1
+    - between T3 and T4 - high / orange - level = 2
+    - between T4 and T5 - very high / red - level = 3
+    - higher than T5 - extremely high / won't be displayed - level = null
+ */
     fun getLevel(stream: MeasurementStream): Int? {
         if (value < stream.thresholdVeryLow) return null
         if (value >= stream.thresholdVeryHigh) return null
