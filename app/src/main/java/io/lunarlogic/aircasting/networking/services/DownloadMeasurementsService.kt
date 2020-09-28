7723fa273a927e30c4c4d6561912faf74149d26d
Anna Ślimak <slimakania@gmail.com>
2020-09-28 13:14:07 +0200

    Refactor of download measurements callback for both fixed and mobile sessions

--- app/src/main/java/io/lunarlogic/aircasting/networking/services/DownloadMeasurementsService.kt
+++ app/src/main/java/io/lunarlogic/aircasting/networking/services/DownloadMeasurementsService.kt
@@ -5,18 +5,10 @@ import io.lunarlogic.aircasting.database.repositories.MeasurementStreamsReposito
 import io.lunarlogic.aircasting.database.repositories.MeasurementsRepository
 import io.lunarlogic.aircasting.database.repositories.SessionsRepository
 import io.lunarlogic.aircasting.exceptions.ErrorHandler
-import io.lunarlogic.aircasting.exceptions.DownloadMeasurementsError
-import io.lunarlogic.aircasting.networking.responses.SessionStreamWithMeasurementsResponse
-import io.lunarlogic.aircasting.networking.responses.SessionWithMeasurementsResponse
-import io.lunarlogic.aircasting.sensor.Measurement
-import io.lunarlogic.aircasting.sensor.MeasurementStream
 import io.lunarlogic.aircasting.sensor.Session
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class DownloadMeasurementsService(private val apiService: ApiService, private val errorHandler: ErrorHandler) {
     private val sessionsRepository = SessionsRepository()
@@ -36,52 +28,10 @@ class DownloadMeasurementsService(private val apiService: ApiService, private va
         GlobalScope.launch(Dispatchers.Main) {
             val call = apiService.downloadSessionWithMeasurements(session.uuid)
 
-            call.enqueue(DownloadCallback(sessionId, session))
-        }
-    }
-
-    private inner class DownloadCallback(private val sessionId: Long, private val session: Session): Callback<SessionWithMeasurementsResponse> {
-        override fun onResponse(
-            call: Call<SessionWithMeasurementsResponse>,
-            response: Response<SessionWithMeasurementsResponse>
-        ) {
-            if (response.isSuccessful) {
-                val body = response.body()
-                body?.streams?.let { streams ->
-                    DatabaseProvider.runQuery {
-                        val streamResponses = streams.values
-                        streamResponses.forEach { streamResponse ->
-                            saveStreamData(streamResponse)
-                        }
-                    }
-                }
-            } else {
-                errorHandler.handleAndDisplay(DownloadMeasurementsError())
-            }
-        }
-
-        override fun onFailure(
-            call: Call<SessionWithMeasurementsResponse>,
-            t: Throwable
-        ) {
-            errorHandler.handleAndDisplay(DownloadMeasurementsError(t))
-        }
-
-        private fun saveStreamData(streamResponse: SessionStreamWithMeasurementsResponse) {
-            val stream = MeasurementStream(streamResponse)
-            val streamId = measurementStreamsRepository.getIdOrInsert(
-                sessionId,
-                stream
-            )
-            streamResponse.measurements.forEach { measurementResponse ->
-                val measurement = Measurement(measurementResponse)
-                measurementsRepository.insert(
-                    streamId,
-                    sessionId,
-                    measurement
-                )
-                sessionsRepository.update(session)
-            }
+            call.enqueue(DownloadMeasurementsCallback(
+                sessionId, session, sessionsRepository, measurementStreamsRepository,
+                measurementsRepository, errorHandler))
         }
     }
 }
+
--- app/src/main/java/io/lunarlogic/aircasting/networking/services/FixedSessionDownloadMeasurementsService.kt
+++ app/src/main/java/io/lunarlogic/aircasting/networking/services/FixedSessionDownloadMeasurementsService.kt
@@ -1,24 +1,15 @@
 package io.lunarlogic.aircasting.networking.services
 
-import io.lunarlogic.aircasting.database.DatabaseProvider
 import io.lunarlogic.aircasting.database.repositories.MeasurementStreamsRepository
 import io.lunarlogic.aircasting.database.repositories.MeasurementsRepository
 import io.lunarlogic.aircasting.database.repositories.SessionsRepository
 import io.lunarlogic.aircasting.exceptions.ErrorHandler
-import io.lunarlogic.aircasting.exceptions.DownloadMeasurementsError
 import io.lunarlogic.aircasting.lib.DateConverter
-import io.lunarlogic.aircasting.networking.responses.SessionStreamWithMeasurementsResponse
-import io.lunarlogic.aircasting.networking.responses.SessionWithMeasurementsResponse
-import io.lunarlogic.aircasting.sensor.Measurement
-import io.lunarlogic.aircasting.sensor.MeasurementStream
 import io.lunarlogic.aircasting.sensor.Session
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import java.util.*
 
 
@@ -69,52 +60,9 @@ class FixedSessionDownloadMeasurementsService(private val apiService: ApiService
                     DateConverter.toDateString(lastMeasurementSyncTime)
                 val call = apiService.downloadMeasurements(session.uuid, lastMeasurementSyncTimeString)
 
-                call.enqueue(DownloadCallback(sessionId, session))
-            }
-        }
-
-        private inner class DownloadCallback(private val sessionId: Long, private val session: Session): Callback<SessionWithMeasurementsResponse> {
-            override fun onResponse(
-                call: Call<SessionWithMeasurementsResponse>,
-                response: Response<SessionWithMeasurementsResponse>
-            ) {
-                if (response.isSuccessful) {
-                    val body = response.body()
-                    body?.streams?.let { streams ->
-                        DatabaseProvider.runQuery {
-                            val streamResponses = streams.values
-                            streamResponses.forEach { streamResponse ->
-                                saveStreamData(streamResponse)
-                            }
-                        }
-                    }
-                } else {
-                    errorHandler.handleAndDisplay(DownloadMeasurementsError())
-                }
-            }
-
-            override fun onFailure(
-                call: Call<SessionWithMeasurementsResponse>,
-                t: Throwable
-            ) {
-                errorHandler.handleAndDisplay(DownloadMeasurementsError(t))
-            }
-
-            private fun saveStreamData(streamResponse: SessionStreamWithMeasurementsResponse) {
-                val stream = MeasurementStream(streamResponse)
-                val streamId = measurementStreamsRepository.getIdOrInsert(
-                    sessionId,
-                    stream
-                )
-                streamResponse.measurements.forEach { measurementResponse ->
-                    val measurement = Measurement(measurementResponse)
-                    measurementsRepository.insert(
-                        streamId,
-                        sessionId,
-                        measurement
-                    )
-                    sessionsRepository.update(session)
-                }
+                call.enqueue(DownloadMeasurementsCallback(
+                    sessionId, session, sessionsRepository, measurementStreamsRepository,
+                    measurementsRepository, errorHandler))
             }
         }
     }
