49a3716e02c318e51f6d309f27860453146c7cf1
Anna Ślimak <slimakania@gmail.com>
2020-10-23 11:56:39 +0200

    Polling all the sensors

--- app/src/main/java/io/lunarlogic/aircasting/bluetooth/BLEManager.kt
+++ app/src/main/java/io/lunarlogic/aircasting/bluetooth/BLEManager.kt
@@ -8,14 +8,9 @@ import android.util.Log
 import io.lunarlogic.aircasting.sensor.ResponseParser
 import io.lunarlogic.aircasting.sensor.airbeam2.HexMessagesBuilder
 import no.nordicsemi.android.ble.BleManager
+import no.nordicsemi.android.ble.data.Data
 import org.greenrobot.eventbus.EventBus
 import java.util.*
-
-//    0000ffe1-0000-1000-8000-00805f9b34fb F
-//    0000ffe3-0000-1000-8000-00805f9b34fb Humidity
-//    0000ffe4-0000-1000-8000-00805f9b34fb PM1
-//    0000ffe5-0000-1000-8000-00805f9b34fb PM2.5
-//    0000ffe6-0000-1000-8000-00805f9b34fb PM10
 
 class BLEManager(context: Context) : BleManager(context) {
     companion object {
@@ -23,12 +18,17 @@ class BLEManager(context: Context) : BleManager(context) {
         val SERVICE_UUID = UUID.fromString("0000ffdd-0000-1000-8000-00805f9b34fb")
     }
 
-    private val WRITE_CHARACTERISTIC_UUID = UUID.fromString("0000ffde-0000-1000-8000-00805f9b34fb")
-    private val READ_CHARACTERISTIC_UUID = UUID.fromString("0000ffe1-0000-1000-8000-00805f9b34fb")
+    private val CONFIGURATION_CHARACTERISTIC_UUID = UUID.fromString("0000ffde-0000-1000-8000-00805f9b34fb")
+    private val READ_CHARACTERISTIC_UUIDS = arrayListOf(
+        UUID.fromString("0000ffe1-0000-1000-8000-00805f9b34fb"),    // Temperature
+        UUID.fromString("0000ffe3-0000-1000-8000-00805f9b34fb"),    // Humidity
+        UUID.fromString("0000ffe4-0000-1000-8000-00805f9b34fb"),    // PM1
+        UUID.fromString("0000ffe5-0000-1000-8000-00805f9b34fb"),    // PM2.5
+        UUID.fromString("0000ffe6-0000-1000-8000-00805f9b34fb")     // PM10
+    )
 
-    // Client characteristics
-    private var readCharacteristic: BluetoothGattCharacteristic? = null
-    private var writeCharacteristic: BluetoothGattCharacteristic? = null
+    private var readCharacteristics: List<BluetoothGattCharacteristic>? = null
+    private var configurationCharacteristic: BluetoothGattCharacteristic? = null
 
     val responseParser = ResponseParser()
 
@@ -40,33 +40,39 @@ class BLEManager(context: Context) : BleManager(context) {
         Log.println(priority, TAG, message)
     }
 
-    /**
-     * BluetoothGatt callbacks object.
-     */
+    private fun validateReadCharacteristic(characteristic: BluetoothGattCharacteristic): Boolean {
+        val properties: Int = characteristic.properties
+        return properties and BluetoothGattCharacteristic.PROPERTY_NOTIFY != 0
+    }
+
+    private fun validateConfigurationCharacteristic(characteristic: BluetoothGattCharacteristic?): Boolean {
+        characteristic ?: return false
+
+        val properties: Int = characteristic.properties
+        return properties and BluetoothGattCharacteristic.PROPERTY_WRITE != 0
+    }
+
     private inner class MyManagerGattCallback : BleManagerGattCallback() {
-        // This method will be called when the device is connected and services are discovered.
-        // You need to obtain references to the characteristics and descriptors that you will use.
-        // Return true if all required services are found, false otherwise.
         public override fun isRequiredServiceSupported(gatt: BluetoothGatt): Boolean {
             val service = gatt.getService(SERVICE_UUID)
-            if (service != null) {
-                readCharacteristic = service.getCharacteristic(READ_CHARACTERISTIC_UUID)
-                writeCharacteristic = service.getCharacteristic(WRITE_CHARACTERISTIC_UUID)
+
+            service ?: return false
+
+            readCharacteristics = READ_CHARACTERISTIC_UUIDS.mapNotNull { uuid -> service.getCharacteristic(uuid) }
+
+            readCharacteristics?.isEmpty() ?: return false
+
+            configurationCharacteristic = service.getCharacteristic(CONFIGURATION_CHARACTERISTIC_UUID)
+
+            if (readCharacteristics!!.any { characteristic -> !validateReadCharacteristic(characteristic) }) {
+                return false
             }
-            // Validate properties
-            var notify = false
-            if (readCharacteristic != null) {
-                val properties: Int = readCharacteristic!!.properties
-                notify = properties and BluetoothGattCharacteristic.PROPERTY_NOTIFY != 0
+
+            if (!validateConfigurationCharacteristic(configurationCharacteristic)) {
+                return false
             }
-            var writeRequest = false
-            if (writeCharacteristic != null) {
-                val properties: Int = writeCharacteristic!!.properties
-                writeRequest = properties and BluetoothGattCharacteristic.PROPERTY_WRITE != 0
-                writeCharacteristic!!.writeType = BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT
-            }
-            // Return true if all required services have been found
-            return readCharacteristic != null && writeCharacteristic != null && notify && writeRequest
+
+            return true
         }
 
         // Initialize your device here. Often you need to enable notifications and set required
@@ -74,61 +80,60 @@ class BLEManager(context: Context) : BleManager(context) {
         override fun initialize() {
             val hexMessagesBuilder = HexMessagesBuilder()
 
-//            writeCharacteristic(writeCharacteristic, hexMessagesBuilder.bluetoothConfigurationMessage)
-//                .done { device: BluetoothDevice? ->
-//                    log(Log.INFO, "Configuration message sent!")
-//                }
-//                .enqueue()
+            configurationCharacteristic?.writeType = BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT
 
-            // Set a callback for your notifications. You may also use waitForNotification(...).
-            // Both callbacks will be called when notification is received.
-            val callback = setNotificationCallback(readCharacteristic)
-            callback.with { _, data ->
-                log(Log.INFO, "On characteristic changed! " + data.value.toString())
-            }
+//            val callback = setNotificationCallback(readCharacteristic)
+//            callback.with { _, data ->
+//                log(Log.INFO, "ANIA On characteristic changed! " + data.value.toString())
+//                onCharacteristicChanged(data)
+//            }
 
-            // You may enqueue multiple operations. A queue ensures that all operations are
-            // performed one after another, but it is not required.
             beginAtomicRequestQueue()
                 .add(
-                    writeCharacteristic(writeCharacteristic, hexMessagesBuilder.bluetoothConfigurationMessage)
+                    writeCharacteristic(configurationCharacteristic, hexMessagesBuilder.bluetoothConfigurationMessage)
                         .done { device: BluetoothDevice? ->
                             log(Log.INFO, "Configuration message sent!")
                         }
                 )
 //                .add(
-//                    requestMtu(247)
-//                        .with { device, mtu -> log(Log.INFO, "MTU set to " + mtu) }
-//                        .fail { device, status -> log(Log.WARN, "Requested MTU not supported: " + status) }
+//                    enableNotifications(readCharacteristic)
+//                        .done { device: BluetoothDevice? ->
+//                            log(Log.INFO, "Notification enabled")
+//                        }
+//                        .fail { _, status ->
+//                            log(Log.ERROR, "Notification enable failed $status")
+//                        }
 //                )
-//                .add(
-//                    setPreferredPhy(PhyRequest.PHY_LE_2M_MASK, PhyRequest.PHY_LE_2M_MASK, PhyRequest.PHY_OPTION_NO_PREFERRED)
-//                        .fail { device, status -> log(Log.WARN, "Requested PHY not supported: " + status) }
-//                )
-                .add(
-                    enableNotifications(readCharacteristic)
-                        .done { device: BluetoothDevice? ->
-                            log(Log.INFO, "Notification enabled")
-                        }
-                        .fail { _, status ->
-                            log(Log.ERROR, "Notification enable failed $status")
-                        }
-                )
                 .enqueue()
         }
 
         override fun onDeviceDisconnected() {
-            // Device disconnected. Release your references here.
-            readCharacteristic = null
-            writeCharacteristic = null
+            readCharacteristics = null
+            configurationCharacteristic = null
         }
     }
 
-    // TODO: move it to a separate service
+    // TODO: move it to a separate service?
+//    fun onCharacteristicChanged(data: Data) {
+//        val value = data.value ?: return
+//
+//        val dataString = String(value)
+//
+//        if (!dataString.isEmpty()) {
+//            val newMeasurementEvent = responseParser.parse(dataString)
+//            newMeasurementEvent?.let { EventBus.getDefault().post(newMeasurementEvent) }
+//        }
+//    }
     fun run() {
-        readCharacteristic(readCharacteristic)
+        readCharacteristics?.forEach { characteristic ->
+            pollCharacteristic(characteristic)
+        }
+    }
+
+    fun pollCharacteristic(characteristic: BluetoothGattCharacteristic) {
+        readCharacteristic(characteristic)
             .done {
-                val data = String(readCharacteristic!!.value)
+                val data = String(characteristic.value)
 
                 if (!data.isEmpty()) {
                     log(Log.INFO, "READ finished " + data)
@@ -136,7 +141,7 @@ class BLEManager(context: Context) : BleManager(context) {
                     newMeasurementEvent?.let { EventBus.getDefault().post(newMeasurementEvent) }
                 }
 
-                run()
+                pollCharacteristic(characteristic)
             }
             .enqueue()
     }
