d55bafcab16ed8a73769a05bb737ee53ec7e3cbd
Anna Ślimak <slimakania@gmail.com>
2020-10-29 13:23:09 +0100

    Removed unnesesery thread

--- app/src/main/java/io/lunarlogic/aircasting/sensor/airbeam3/AirBeam3Connector.kt
+++ app/src/main/java/io/lunarlogic/aircasting/sensor/airbeam3/AirBeam3Connector.kt
@@ -18,11 +18,9 @@ open class AirBeam3Connector(
     private val mContext: Context,
     private val mSettinngs: Settings,
     private val mErrorHandler: ErrorHandler
-): AirBeamConnector() {
+): AirBeamConnector(), ConnectionObserver {
     private val connectionStarted = AtomicBoolean(false)
     private val cancelStarted = AtomicBoolean(false)
-
-    private var mThread: ConnectThread? = null
 
     private var airBeam3Reader: AirBeam3Reader? = null
 
@@ -30,84 +28,62 @@ open class AirBeam3Connector(
         if (connectionStarted.get() == false) {
             connectionStarted.set(true)
             registerToEventBus()
-            mThread = ConnectThread(deviceItem)
-            mThread?.start()
+            openConnection(deviceItem)
         }
+    }
+
+    private fun openConnection(deviceItem: DeviceItem) {
+        val bluetoothAdapter =  BluetoothAdapter.getDefaultAdapter()
+        bluetoothAdapter?.cancelDiscovery()
+
+        // TODO: inject this
+        airBeam3Reader = AirBeam3Reader(mContext, mErrorHandler, mSettinngs)
+        airBeam3Reader!!.setConnectionObserver(this)
+
+        airBeam3Reader!!.connect(deviceItem.bluetoothDevice)
+            .timeout(100000)
+            .retry(3, 100)
+            .done { _ -> onConnectionSuccessful(deviceItem.id) }
+            .enqueue()
     }
 
     override fun disconnect() {
-        mThread?.cancel()
+        unregisterFromEventBus()
+
+        if (cancelStarted.get() == false) {
+            cancelStarted.set(true)
+            connectionStarted.set(false)
+            airBeam3Reader?.close()
+            cancelStarted.set(false)
+        }
     }
 
     override fun configureSession(session: Session, wifiSSID: String?, wifiPassword: String?) {
-        mThread?.configureSession(session, wifiSSID, wifiPassword)
+        try {
+            if (session.isFixed()) {
+                val location = session.location!! // TODO: handle !! in a better way
+
+                when (session.streamingMethod) {
+                    Session.StreamingMethod.WIFI -> airBeam3Reader?.configureFixedWifi(location, wifiSSID!!, wifiPassword!!)
+                    Session.StreamingMethod.CELLULAR -> airBeam3Reader?.configureFixedCellular(location)
+                }
+            } else {
+                airBeam3Reader?.configureMobile()
+            }
+        } catch (e: IOException) {
+            // TODO: is it really thrown for BLE?
+            mErrorHandler.handle(AirBeam2ConfiguringFailed(e))
+        }
     }
 
     override fun sendAuth(uuid: String) {
-        mThread?.sendAuth(uuid)
+        airBeam3Reader?.sendAuth(uuid)
     }
 
-    // TODO: actually this thread can be not needed here as we already use async connection method - enqueue
-    private inner class ConnectThread(private val deviceItem: DeviceItem) : Thread(), ConnectionObserver {
-        override fun run() {
-            // Cancel discovery because it otherwise slows down the connection.
-            val bluetoothAdapter =  BluetoothAdapter.getDefaultAdapter()
-            bluetoothAdapter?.cancelDiscovery()
-
-            // TODO: inject this
-            airBeam3Reader = AirBeam3Reader(mContext, mErrorHandler, mSettinngs)
-            airBeam3Reader!!.setConnectionObserver(this)
-
-            airBeam3Reader!!.connect(deviceItem.bluetoothDevice)
-                .timeout(100000)
-                .retry(3, 100)
-                .done { _ -> onConnectionSuccessful(deviceItem.id) }
-                .enqueue()
-        }
-
-        fun cancel() {
-            unregisterFromEventBus()
-
-            if (cancelStarted.get() == false) {
-                cancelStarted.set(true)
-                connectionStarted.set(false)
-                airBeam3Reader?.close()
-                cancelStarted.set(false)
-            }
-        }
-
-        fun sendAuth(uuid: String) {
-            airBeam3Reader?.sendAuth(uuid)
-        }
-
-        fun configureSession(session: Session, wifiSSID: String?, wifiPassword: String?) {
-            try {
-                if (session.isFixed()) {
-                    val location = session.location!! // TODO: handle !! in a better way
-
-                    when (session.streamingMethod) {
-                        Session.StreamingMethod.WIFI -> airBeam3Reader?.configureFixedWifi(location, wifiSSID!!, wifiPassword!!)
-                        Session.StreamingMethod.CELLULAR -> airBeam3Reader?.configureFixedCellular(location)
-                    }
-                } else {
-                    airBeam3Reader?.configureMobile()
-                }
-            } catch (e: IOException) {
-                // TODO: is it really thrown for BLE?
-                mErrorHandler.handle(AirBeam2ConfiguringFailed(e))
-            }
-        }
-
-        override fun onDeviceConnecting(device: BluetoothDevice) {}
-
-        override fun onDeviceConnected(device: BluetoothDevice) {}
-
-        override fun onDeviceFailedToConnect(device: BluetoothDevice, reason: Int) {}
-
-        override fun onDeviceReady(device: BluetoothDevice) {}
-
-        override fun onDeviceDisconnecting(device: BluetoothDevice) {}
-
-        override fun onDeviceDisconnected(device: BluetoothDevice, reason: Int) {}
-    }
+    override fun onDeviceConnecting(device: BluetoothDevice) {}
+    override fun onDeviceConnected(device: BluetoothDevice) {}
+    override fun onDeviceFailedToConnect(device: BluetoothDevice, reason: Int) {}
+    override fun onDeviceReady(device: BluetoothDevice) {}
+    override fun onDeviceDisconnecting(device: BluetoothDevice) {}
+    override fun onDeviceDisconnected(device: BluetoothDevice, reason: Int) {}
 }
