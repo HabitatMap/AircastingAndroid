2611b36cbd685f1b03a2134ad8c50a5a1a7c5e33
Anna Ślimak <slimakania@gmail.com>
2020-11-09 13:18:05 +0100

    Added thresholds color indicators

--- app/src/main/java/io/lunarlogic/aircasting/screens/session_view/graph/GraphContainer.kt
+++ app/src/main/java/io/lunarlogic/aircasting/screens/session_view/graph/GraphContainer.kt
@@ -1,15 +1,15 @@
 package io.lunarlogic.aircasting.screens.session_view.graph
 
 import android.content.Context
+import android.graphics.Color
 import android.view.View
 import androidx.fragment.app.FragmentManager
-import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
-import io.lunarlogic.aircasting.screens.dashboard.SessionPresenter
+import com.github.mikephil.charting.data.*
+import io.lunarlogic.aircasting.lib.MeasurementColor
 import io.lunarlogic.aircasting.models.Measurement
+import io.lunarlogic.aircasting.screens.dashboard.SessionPresenter
 import io.lunarlogic.aircasting.screens.session_view.SessionDetailsViewMvc
+import io.lunarlogic.aircasting.screens.session_view.graph.TargetZoneCombinedChart.TargetZone
 import kotlinx.android.synthetic.main.graph.view.*
 
 class GraphContainer {
@@ -18,12 +18,14 @@ class GraphContainer {
 
     private var mSessionPresenter: SessionPresenter? = null
     private var mMeasurements: List<Measurement> = emptyList()
-    private val mGraph: LineChart?
+    private val mGraph: TargetZoneCombinedChart?
 
 
     constructor(rootView: View?, context: Context, supportFragmentManager: FragmentManager?) {
         mContext = context
         mGraph = rootView?.graph
+
+        setupGraph()
     }
 
     fun registerListener(listener: SessionDetailsViewMvc.Listener) {
@@ -51,11 +53,59 @@ class GraphContainer {
     }
 
     private fun drawSession() {
-        val entries = mMeasurements.mapIndexed { index, measurement ->  Entry(index.toFloat(), measurement.value.toFloat()) }
-        val dataset = LineDataSet(entries, "")
-        val lineData = LineData(dataset)
-
-        mGraph?.data = lineData
+        updateData()
+        updateThresholds()
         mGraph?.invalidate()
+    }
+
+    private fun updateData() {
+        val combinedData = CombinedData()
+        combinedData.setData(buildEntries())
+        mGraph?.data = combinedData
+    }
+
+    private fun updateThresholds() {
+        val threshold = mSessionPresenter?.selectedSensorThreshold()
+        threshold ?: return
+
+        updateValueAxis(threshold.from, threshold.to)
+
+        mGraph?.clearTargetZones()
+        MeasurementColor.levels(threshold, mContext).forEach { level ->
+            mGraph?.addTargetZone(TargetZone(level.color, level.from.toFloat(), level.to.toFloat()))
+        }
+    }
+
+    private fun buildEntries(): LineData {
+        val entries = mMeasurements.mapIndexed { index, measurement ->  Entry(
+            index.toFloat(),
+            measurement.value.toFloat()
+        ) }
+
+        val dataSet = LineDataSet(entries, "")
+
+        return LineData(dataSet)
+    }
+
+    private fun updateValueAxis(from: Float, to: Float) {
+        mGraph?.axisLeft?.axisMinimum = from
+        mGraph?.axisLeft?.axisMaximum = to
+        mGraph?.axisRight?.axisMinimum = from
+        mGraph?.axisRight?.axisMaximum = to
+    }
+
+    private fun setupGraph() {
+        mGraph ?: return
+
+        mGraph.setPinchZoom(true)
+        mGraph.description = null
+        mGraph.legend?.isEnabled = false
+        mGraph.axisLeft?.setDrawLabels(false)
+        mGraph.axisRight?.setDrawLabels(false)
+        mGraph.axisLeft?.setDrawGridLines(false)
+        mGraph.axisRight?.setDrawGridLines(false)
+        mGraph.xAxis?.setDrawLabels(false)
+        mGraph.xAxis?.setDrawGridLines(false)
+        mGraph.setDrawGridBackground(false)
     }
 }
