300dcbde92e2a2cc41fd4656f0c16d464a68d243
Maria Turnau-Gurgul <maria.turnau@lunarlogic.io>
2020-12-01 14:20:14 +0100

    Graph view - differ mobile active and dormant views

--- app/src/main/java/io/lunarlogic/aircasting/screens/session_view/graph/GraphActivity.kt
+++ app/src/main/java/io/lunarlogic/aircasting/screens/session_view/graph/GraphActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.viewModels
 import io.lunarlogic.aircasting.lib.AppBar
 import io.lunarlogic.aircasting.models.Session
 import io.lunarlogic.aircasting.models.SessionsViewModel
+import io.lunarlogic.aircasting.screens.session_view.map.MapActivity
 
 class GraphActivity: AppCompatActivity() {
     private var controller: GraphController? = null
@@ -17,13 +18,15 @@ class GraphActivity: AppCompatActivity() {
         val SESSION_UUID_KEY = "SESSION_UUID"
         val SENSOR_NAME_KEY = "SENSOR_NAME"
         val SESSION_TYPE_KEY = "SESSION_TYPE"
+        val SESSION_STATUS_KEY = "SESSION_STATUS"
 
-        fun start(context: Context?, sensorName: String?, sessionUUID: String, sessionType: Session.Type) {
+        fun start(context: Context?, sensorName: String?, sessionUUID: String, sessionType: Session.Type, sessionStatus: Session.Status) {
             context?.let{
                 val intent = Intent(it, GraphActivity::class.java)
                 intent.putExtra(SESSION_UUID_KEY, sessionUUID)
                 intent.putExtra(SENSOR_NAME_KEY, sensorName)
                 intent.putExtra(SESSION_TYPE_KEY, sessionType.value)
+                intent.putExtra(SESSION_STATUS_KEY, sessionStatus.value)
                 it.startActivity(intent)
             }
         }
@@ -35,12 +38,14 @@ class GraphActivity: AppCompatActivity() {
         val sessionUUID: String = intent.extras?.get(SESSION_UUID_KEY) as String
         val sensorName: String? = intent.extras?.get(SENSOR_NAME_KEY) as String?
         val sessionType: Int = intent.extras?.getInt(SESSION_TYPE_KEY) as Int
+        val sessionStatus: Int = intent.extras?.getInt(MapActivity.SESSION_STATUS_KEY) as Int
 
         val view = GraphViewMvcImplFactory.get(
             layoutInflater,
             null,
             supportFragmentManager,
-            Session.Type.fromInt(sessionType)
+            Session.Type.fromInt(sessionType),
+            Session.Status.fromInt(sessionStatus)
         )
         controller = GraphController(this, sessionsViewModel, view, sessionUUID, sensorName)
 
--- app/src/main/java/io/lunarlogic/aircasting/screens/session_view/graph/GraphViewMvcImplFactory.kt
+++ app/src/main/java/io/lunarlogic/aircasting/screens/session_view/graph/GraphViewMvcImplFactory.kt
@@ -4,6 +4,9 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
 import io.lunarlogic.aircasting.models.Session
+import io.lunarlogic.aircasting.screens.session_view.map.MapViewFixedMvcImpl
+import io.lunarlogic.aircasting.screens.session_view.map.MapViewMobileActiveMvcImpl
+import io.lunarlogic.aircasting.screens.session_view.map.MapViewMobileDormantMvcImpl
 
 
 class GraphViewMvcImplFactory {
@@ -12,13 +15,23 @@ class GraphViewMvcImplFactory {
             inflater: LayoutInflater,
             parent: ViewGroup?,
             supportFragmentManager: FragmentManager?,
-            sessionType: Session.Type
+            sessionType: Session.Type,
+            sessionStatus: Session.Status
         ): GraphViewMvcImpl {
-            return when(sessionType) {
-                Session.Type.MOBILE -> GraphViewMobileMvcImpl(inflater, parent, supportFragmentManager)
-                Session.Type.FIXED -> GraphViewFixedMvcImpl(inflater, parent, supportFragmentManager)
-                else -> GraphViewMobileMvcImpl(inflater, parent, supportFragmentManager)
+            if (sessionType == Session.Type.FIXED) {
+                return GraphViewFixedMvcImpl(inflater, parent, supportFragmentManager)
             }
+//
+            return when(sessionStatus) {
+                Session.Status.FINISHED -> GraphViewMobileMvcImpl(inflater, parent, supportFragmentManager)
+                Session.Status.RECORDING -> GraphViewMobileMvcImpl(inflater, parent, supportFragmentManager)
+                else -> GraphViewFixedMvcImpl(inflater, parent, supportFragmentManager)
+            } //TODO: differ between dormant and actove session
+//            return when(sessionType) {
+//                Session.Type.MOBILE -> GraphViewMobileMvcImpl(inflater, parent, supportFragmentManager)
+//                Session.Type.FIXED -> GraphViewFixedMvcImpl(inflater, parent, supportFragmentManager)
+//                else -> GraphViewMobileMvcImpl(inflater, parent, supportFragmentManager)
+//            }
         }
     }
 }
