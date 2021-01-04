ba25d96b34a48a27821c4828d161d1d0a05b1a5c
michalrazny2 <michalrazny2@o2.pl>
2021-01-04 11:37:04 +0100

    small changes with in ExportSessionService.kt

--- app/src/main/java/io/lunarlogic/aircasting/networking/services/ExportSessionService.kt
+++ app/src/main/java/io/lunarlogic/aircasting/networking/services/ExportSessionService.kt
@@ -1,5 +1,6 @@
 package io.lunarlogic.aircasting.networking.services
 
+import io.lunarlogic.aircasting.R
 import io.lunarlogic.aircasting.exceptions.ErrorHandler
 import io.lunarlogic.aircasting.exceptions.UnexpectedAPIError
 import io.lunarlogic.aircasting.networking.responses.ExportSessionResponse
@@ -13,13 +14,17 @@ class ExportSessionService(private val apiService: ApiService, private val error
 
         call.enqueue(object : Callback<ExportSessionResponse> {
             override fun onResponse(call: Call<ExportSessionResponse>, response: Response<ExportSessionResponse>) {
-                if (!response.isSuccessful) {
+                if (response.isSuccessful) {
+                    successCallback.invoke()
+                }else{
                     errorHandler.handle(UnexpectedAPIError())
+                    errorHandler.showError(getString(R.string.errors_share_failure))
                 }
             }
 
             override fun onFailure(call: Call<ExportSessionResponse>, t: Throwable) {
                 errorHandler.handle(UnexpectedAPIError(t))
+                errorHandler.showError(getString(R.string.errors_network_required_share))
             }
         })
     }
