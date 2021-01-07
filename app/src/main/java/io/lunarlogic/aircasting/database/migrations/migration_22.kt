8d916f1876a4fed24299c0381f3ac64fa6b6ee5c
michalrazny2 <michalrazny2@o2.pl>
2021-01-07 17:56:22 +0100

    minor change in migration

--- app/src/main/java/io/lunarlogic/aircasting/database/migrations/migration_22.kt
+++ app/src/main/java/io/lunarlogic/aircasting/database/migrations/migration_22.kt
@@ -5,6 +5,6 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 
 val MIGRATION_21_22 = object : Migration(21, 22) {
     override fun migrate(database: SupportSQLiteDatabase) {
-        database.execSQL("ALTER TABLE `sessions` ADD `urlLocation` INTEGER NOT NULL DEFAULT 0")
+        database.execSQL("ALTER TABLE `sessions` ADD `urlLocation` TEXT DEFAULT NULL")
     }
 }
