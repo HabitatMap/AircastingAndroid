18ca33d8bd7c29b90f0e18c337ac03906108f3c9
michalrazny2 <michalrazny2@o2.pl>
2021-01-11 15:10:20 +0100

    url encoding added to ShareHelper.kt

--- app/src/main/java/io/lunarlogic/aircasting/lib/ShareHelper.kt
+++ app/src/main/java/io/lunarlogic/aircasting/lib/ShareHelper.kt
@@ -1,15 +1,14 @@
 package io.lunarlogic.aircasting.lib
 
-import android.app.Activity
 import android.content.Context
-import android.provider.ContactsContract
 import io.lunarlogic.aircasting.R
 import io.lunarlogic.aircasting.models.Session
+import java.net.URLEncoder
 
 class ShareHelper {
     companion object{
         fun shareLink(session: Session, selectedSensorName: String, context: Context?): String {
-            val sessionLink = "${session.urlLocation}?${context?.getString(R.string.sensor_param)}=${selectedSensorName}"
+            val sessionLink = "${session.urlLocation}?${context?.getString(R.string.sensor_param)}=${URLEncoder.encode(selectedSensorName)}"
             return context?.getString(R.string.share_text) + sessionLink
         }
     }
