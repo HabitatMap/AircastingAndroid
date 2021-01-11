92624a78f68efecaa7642729b19ff503726db2b1
michalrazny2 <michalrazny2@o2.pl>
2021-01-11 12:16:20 +0100

    'front' part of Mic Settings is now prepared

--- app/src/main/java/io/lunarlogic/aircasting/screens/settings/MicrophoneSettingsDialog.kt
+++ app/src/main/java/io/lunarlogic/aircasting/screens/settings/MicrophoneSettingsDialog.kt
@@ -3,15 +3,37 @@ package io.lunarlogic.aircasting.screens.settings
 import android.view.LayoutInflater
 import android.view.View
 import androidx.fragment.app.FragmentManager
+import io.lunarlogic.aircasting.R
 import io.lunarlogic.aircasting.screens.common.BaseDialog
+import kotlinx.android.synthetic.main.microphone_settings_dialog.view.*
 
 class MicrophoneSettingsDialog(
     mFragmentManager : FragmentManager,
+    private val micValue: String?,
     private val listener: SettingsViewMvc.MicrophoneSettingsDialogListener
     ) : BaseDialog(mFragmentManager) {
+    private lateinit var mView: View
 
     override fun setupView(inflater: LayoutInflater): View {
-        TODO("Not yet implemented")
+        mView = inflater.inflate(R.layout.microphone_settings_dialog, null)
+
+        mView.mic_setting_input.setText(micValue)
+
+        mView.ok_button.setOnClickListener {
+            microphoneSettingsConfirmed()
+        }
+
+        mView.cancel_button.setOnClickListener {
+            dismiss()
+        }
+
+        return mView
+    }
+
+    private fun microphoneSettingsConfirmed(){
+        val micValue = mView.mic_setting_input.toString().trim()
+        listener.confirmMicrophoneSettingsClicked(micValue)
+        dismiss()
     }
 
 
