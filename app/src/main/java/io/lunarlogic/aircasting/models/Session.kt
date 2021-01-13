5fe4cde83bdfbf11dfc32cbc4654be07564afbe4
michalrazny2 <michalrazny2@o2.pl>
2021-01-13 11:43:10 +0100

    minor changes during debug that has to be backed off

--- app/src/main/java/io/lunarlogic/aircasting/models/Session.kt
+++ app/src/main/java/io/lunarlogic/aircasting/models/Session.kt
@@ -255,11 +255,13 @@ class Session(
 
         var durationString = "${dateFormatter.format(mStartTime)} ${hourFormatter.format(mStartTime)}"
 
-        if (endTime == null) return durationString
+        if (endTime == null) return durationString // if endTime == null -> wyswietla tylko start time ?
 
-        if (endTime!!.day != startTime.day) {
+        if (endTime!!.day != startTime.day) { // todo: btw i think we might need some more accurate check than only day (also month and year?))
             durationString += " - ${dateFormatter.format(endTime)} ${hourFormatter.format(endTime)}"
         } else {
+            val dayStart = mStartTime.day
+            val dayEnd = endTime!!.day
             durationString += "-${hourFormatter.format(endTime)}"
         }
 
