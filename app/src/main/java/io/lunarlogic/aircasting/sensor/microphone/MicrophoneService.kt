708a07e964e6d2206d1a7cac8a0d7ae1db55d044
Maria Turnau-Gurgul <maria.turnau@lunarlogic.io>
2021-01-18 11:17:09 +0100

    Fix after merge with master.

--- app/src/main/java/io/lunarlogic/aircasting/sensor/microphone/MicrophoneService.kt
+++ app/src/main/java/io/lunarlogic/aircasting/sensor/microphone/MicrophoneService.kt
@@ -3,15 +3,17 @@ package io.lunarlogic.aircasting.sensor.microphone
 import android.content.Context
 import android.content.Intent
 import androidx.core.content.ContextCompat
+import io.lunarlogic.aircasting.AircastingApplication
 import io.lunarlogic.aircasting.R
 import io.lunarlogic.aircasting.events.StopRecordingEvent
 import io.lunarlogic.aircasting.exceptions.ErrorHandler
+import io.lunarlogic.aircasting.lib.Settings
 import io.lunarlogic.aircasting.sensor.SensorService
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
 
 class MicrophoneService : SensorService() {
-    var mMicrophoneReader: MicrophoneReader = MicrophoneReader(AudioReader(), ErrorHandler(this))
+    var mMicrophoneReader: MicrophoneReader? = null
 
     companion object {
         fun startService(context: Context) {
@@ -22,11 +24,14 @@ class MicrophoneService : SensorService() {
 
     override fun startSensor(intent: Intent?) {
         // TODO: can we use injector here not to create this var from scratch?
-        mMicrophoneReader.start()
+        val app = application as AircastingApplication
+        mMicrophoneReader= MicrophoneReader(AudioReader(), ErrorHandler(this), Settings(app))
+
+        mMicrophoneReader?.start()
     }
 
     override fun onStopService() {
-        mMicrophoneReader.stop()
+        mMicrophoneReader?.stop()
     }
 
     override fun notificationMessage(): String {
