cc155414a2e56f2730820ef9e2b6e976c6adb963
michalrazny2 <michalrazny2@o2.pl>
2021-01-26 13:29:37 +0100

    Spannable strings added, some dialog layout changes

--- app/src/main/java/io/lunarlogic/aircasting/screens/dashboard/active/DisconnectedViewFinishDialog.kt
+++ app/src/main/java/io/lunarlogic/aircasting/screens/dashboard/active/DisconnectedViewFinishDialog.kt
@@ -1,7 +1,12 @@
 package io.lunarlogic.aircasting.screens.dashboard.active
 
+import android.graphics.Color
+import android.text.SpannableStringBuilder
 import android.view.LayoutInflater
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.text.bold
+import androidx.core.text.color
 import androidx.fragment.app.FragmentManager
 import io.lunarlogic.aircasting.R
 import io.lunarlogic.aircasting.models.Session
@@ -18,6 +23,9 @@ class DisconnectedViewFinishDialog(
     override fun setupView(inflater: LayoutInflater): View {
         mView = inflater.inflate(R.layout.disconnected_view_finish_session_dialog, null)
 
+        mView.informations_text_view.text = buildDescription()
+        mView.header.text = buildHeader()
+
         mView.finish_recording_button.setOnClickListener {
             finishSessionConfirmed()
         }
@@ -31,5 +39,31 @@ class DisconnectedViewFinishDialog(
 
     private fun finishSessionConfirmed(){
         mListener.onSessionStopClicked(mSession)
+    }
+
+    private fun buildDescription(): SpannableStringBuilder {
+        val blueColor = context?.let {
+            ResourcesCompat.getColor(it.resources, R.color.aircasting_blue_400, null)
+        } ?: Color.GRAY
+
+        return SpannableStringBuilder()
+            .append(getString(R.string.dialog_finish_recording_text_part1))
+            .append(" ")
+            .color(blueColor, { bold { append(getString(R.string.dialog_finish_recording_text_part2)) } })
+            .append(" ")
+            .append(getString(R.string.dialog_finish_recording_text_part3))
+    }
+
+    private fun buildHeader(): SpannableStringBuilder {
+        val blueColor = context?.let{
+            ResourcesCompat.getColor(it.resources, R.color.aircasting_blue_400, null)
+        } ?: Color.GRAY
+
+        return SpannableStringBuilder()
+            .append(getString(R.string.dialog_finish_recording_header_part1))
+            .append(" ")
+            .color(blueColor, { bold { append(mSession.name) } })
+            .append(" ")
+            .append(getString(R.string.dialog_finish_recording_header_part3))
     }
 }
