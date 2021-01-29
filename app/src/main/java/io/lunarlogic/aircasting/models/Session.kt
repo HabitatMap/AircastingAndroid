938c3578de8fa03b0e59b56de47a106cf140e10d
Ika Grabon <ika.grabon@lunarlogic.io>
2021-01-29 09:18:30 +0100

    Added remove stream from session [ci-skip]

--- app/src/main/java/io/lunarlogic/aircasting/models/Session.kt
+++ app/src/main/java/io/lunarlogic/aircasting/models/Session.kt
@@ -297,4 +297,11 @@ class Session(
         formatter.timeZone = TimeZone.getDefault()
         return formatter
     }
+
+    fun removeStream(stream: MeasurementStream) {
+        val mutableStreams  = streams.toMutableList()
+        mutableStreams.remove(stream)
+        mStreams = Collections.unmodifiableList(mutableStreams)
+    }
+
 }
