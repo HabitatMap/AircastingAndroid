501c84d63744c80c2497ec2f1540bf1b5dbde781
Anna Ślimak <slimakania@gmail.com>
2021-02-12 07:47:04 +0100

    Fixed 23 migration

--- app/src/main/java/io/lunarlogic/aircasting/database/migrations/migration_23.kt
+++ app/src/main/java/io/lunarlogic/aircasting/database/migrations/migration_23.kt
@@ -5,6 +5,6 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 
 val MIGRATION_22_23 = object : Migration(22, 23) {
     override fun migrate(database: SupportSQLiteDatabase) {
-        database.execSQL("ALTER TABLE `measurement_streams` ADD `deleted` INTEGER DEFAULT 0")
+        database.execSQL("ALTER TABLE `measurement_streams` ADD `deleted` INTEGER NOT NULL DEFAULT 0")
     }
 }
