3effcf822ca94f9515421e25e6a7768c1e613032
Maria Turnau-Gurgul <maria.turnau@lunarlogic.io>
2021-02-18 09:35:34 +0100

    Test fix - swipeUp instead of scroll to on delete streams

--- app/src/androidTest/java/io/lunarlogic/aircasting/MobileSessionTest.kt
+++ app/src/androidTest/java/io/lunarlogic/aircasting/MobileSessionTest.kt
@@ -247,7 +247,8 @@ class MobileSessionTest {
             .perform(click())
             .check(matches(isChecked()))
 
-        onView(withId(R.id.delete_streams_button)).perform(scrollTo(), click())
+        onView(isRoot()).perform(swipeUp())
+        onView(withId(R.id.delete_streams_button)).perform(click())
         Thread.sleep(2000)
         // check if session deleted
         onView(withText("Ania's mobile mic session")).check(matches(not(isDisplayed())))
