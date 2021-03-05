533a378e4a5effa0ac8f050c42a6143475d803d2
michalrazny2 <michalrazny2@o2.pl>
2021-03-05 13:02:48 +0100

    STEP_PROGRESS from ProgressBarCounter.kt moved to companion object so its more consistent

--- app/src/main/java/io/lunarlogic/aircasting/lib/ProgressBarCounter.kt
+++ app/src/main/java/io/lunarlogic/aircasting/lib/ProgressBarCounter.kt
@@ -3,9 +3,6 @@ package io.lunarlogic.aircasting.lib
 class ProgressBarCounter(
   var initialStepNumber: Int = DEFAULT_STEP_NUMBER
 ) {
-    private val STEP_PROGRESS = 10
-
-
     var currentProgressMax = STEP_PROGRESS * initialStepNumber
 
     fun increaseMaxProgress(stepsNumber: Int = DEFAULT_MAX_PROGRESS_INCREASE) {
@@ -16,5 +13,6 @@ class ProgressBarCounter(
         val DEFAULT_STEP_NUMBER = 4  // we got 3 basic steps in the flow (progress bar should have 1 more to look better)
         val DEFAULT_ONBOARDING_STEP_NUMBER = 6
         val DEFAULT_MAX_PROGRESS_INCREASE = 1
+        private val STEP_PROGRESS = 10
     }
 }
