6a6f7dc0d4c83bb4a49585ee4c0c90f7e87ffbf8
Anna Ślimak <slimakania@gmail.com>
2021-03-05 07:49:57 +0100

    Using base BottomSheet class in learn more about measure and map bottom sheet

--- app/src/main/java/io/lunarlogic/aircasting/screens/onboarding/measure_and_map/LearnMoreMeasureAndMapBottomSheet.kt
+++ app/src/main/java/io/lunarlogic/aircasting/screens/onboarding/measure_and_map/LearnMoreMeasureAndMapBottomSheet.kt
@@ -1,50 +1,33 @@
 package io.lunarlogic.aircasting.screens.onboarding.measure_and_map
 
 import android.graphics.Color
-import android.os.Bundle
 import android.text.SpannableStringBuilder
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.text.bold
 import androidx.core.text.color
-import androidx.fragment.app.FragmentManager
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import io.lunarlogic.aircasting.R
+import io.lunarlogic.aircasting.screens.common.BottomSheet
+import kotlinx.android.synthetic.main.learn_more_onboarding_measure_and_map.view.*
 
-class LearnMoreMeasureAndMapBottomSheet: BottomSheetDialogFragment() {
+class LearnMoreMeasureAndMapBottomSheet: BottomSheet() {
+    override fun layoutId(): Int {
+        return R.layout.learn_more_onboarding_measure_and_map
+    }
 
-    private val TAG = "LearnMorePage3BottomSheet"
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.learn_more_onboarding_measure_and_map, container, false)
-
-        val descriptionViewMobile = view?.findViewById<TextView>(R.id.learn_more_onboarding_measure_and_map_description_mobile)
+    override fun setup() {
+        val descriptionViewMobile = contentView?.learn_more_onboarding_measure_and_map_description_mobile
         descriptionViewMobile?.text = buildDescriptionMobile()
 
-        val descriptionViewFixed = view?.findViewById<TextView>(R.id.learn_more_onboarding_measure_and_map_description_fixed)
+        val descriptionViewFixed = contentView?.learn_more_onboarding_measure_and_map_description_fixed
         descriptionViewFixed?.text = buildDescriptionFixed()
 
-        val closeButton = view?.findViewById<ImageView>(R.id.close_button)
+        val closeButton = contentView?.close_button
         closeButton?.setOnClickListener {
             dismiss()
         }
-
-        return view
     }
 
-    fun show(manager: FragmentManager) {
-        show(manager, TAG)
-    }
-
-    fun buildDescriptionMobile(): SpannableStringBuilder {
+    private fun buildDescriptionMobile(): SpannableStringBuilder {
         val greenColor = context?.let {
             ResourcesCompat.getColor(it.resources, R.color.aircasting_green, null)
         } ?: Color.GRAY
@@ -57,7 +40,7 @@ class LearnMoreMeasureAndMapBottomSheet: BottomSheetDialogFragment() {
             .append(getString(R.string.onboarding_page3_description1_part3))
     }
 
-    fun buildDescriptionFixed(): SpannableStringBuilder {
+    private fun buildDescriptionFixed(): SpannableStringBuilder {
         val greenColor = context?.let {
             ResourcesCompat.getColor(it.resources, R.color.aircasting_green, null)
         } ?: Color.GRAY
