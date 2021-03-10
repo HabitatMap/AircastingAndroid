68d8e0fbd1bf57ba54d8b724bbfe2c08e1067ec4
Ika Grabon <ika.grabon@lunarlogic.io>
2021-03-10 15:31:02 +0100

    Added comments for next generations

--- app/src/main/java/io/lunarlogic/aircasting/networking/services/RemoveOldMeasurementsService.kt
+++ app/src/main/java/io/lunarlogic/aircasting/networking/services/RemoveOldMeasurementsService.kt
@@ -13,6 +13,12 @@ class RemoveOldMeasurementsService() {
     private val TWENTY_FOUR_HOURS_MEASUREMENTS_COUNT = 24 * 60
 
     fun removeMeasurementsFromSessions() {
+        // For next generations:
+        // We want to remove measurements older than 24h,
+        // but we treat 24 not like a date, but as a sum of measurements.
+        // We know that we have 60 measurements per hour,
+        // so we take 1440 last measurements for each stream in fixed sessions
+        // and we remove older than the first of them.
         val fixedSessionsIds = sessionRepository.sessionsIdsByType(Session.Type.FIXED)
         val streamsForFixedSessionsIds = measurementStreamsRepository.getStreamsIdsBySessionIds(fixedSessionsIds)
 
@@ -27,6 +33,7 @@ class RemoveOldMeasurementsService() {
     }
 
     private fun shouldDeleteMeasurements(measurementsCount: Int): Boolean {
+        // if measurementsCount is smaller than 1440 we don't want to remove anything
         return measurementsCount == TWENTY_FOUR_HOURS_MEASUREMENTS_COUNT
     }
 }
