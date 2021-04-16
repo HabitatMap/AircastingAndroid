69743a49c409c2c849104f0385a340e05cf16e3f
michalrazny2 <michalrazny2@o2.pl>
2021-04-16 15:07:33 +0200

    checking if exists Mobile Active session in DashboardController.kt

--- app/src/main/java/io/lunarlogic/aircasting/screens/dashboard/DashboardController.kt
+++ app/src/main/java/io/lunarlogic/aircasting/screens/dashboard/DashboardController.kt
@@ -1,12 +1,19 @@
 package io.lunarlogic.aircasting.screens.dashboard
 
+import io.lunarlogic.aircasting.database.DatabaseProvider
+import io.lunarlogic.aircasting.database.repositories.SessionsRepository
+import io.lunarlogic.aircasting.models.Session
 import io.lunarlogic.aircasting.screens.common.BaseController
 
 class DashboardController(
-    viewMvc: DashboardViewMvcImpl?
+    private val viewMvc: DashboardViewMvcImpl?
 ) : BaseController<DashboardViewMvcImpl>(viewMvc) {
+    private val mSessionRepository = SessionsRepository()
 
     fun onCreate(tabId: Int?) {
-        mViewMvc?.goToTab(tabId ?: 0)
+        DatabaseProvider.runQuery {
+            if (mSessionRepository.mobileActiveSessionExists()) viewMvc?.goToTab(DashboardPagerAdapter.tabIndexForSessionType(Session.Type.MOBILE, Session.Status.RECORDING))
+            else viewMvc?.goToTab(tabId ?: 0)
+        }
     }
 }
