e7d72ac8efbbc2cbc08dc0bd113d9dcfdc2d9df5
Maria Turnau-Gurgul <maria.turnau@lunarlogic.io>
2021-04-28 12:15:29 +0200

    Refactored MapGrid and description in comment

--- app/src/main/java/io/lunarlogic/aircasting/screens/session_view/map/MapGrid.kt
+++ app/src/main/java/io/lunarlogic/aircasting/screens/session_view/map/MapGrid.kt
@@ -12,46 +12,58 @@ import io.lunarlogic.aircasting.models.Measurement
 import io.lunarlogic.aircasting.models.SensorThreshold
 
 /**
- * Created by Maria Turnau on 23/04/2021.
+ * This is a custom "heatmap" for the Aircasting maps. The way it works is similar to what we already have
+ * on the website when you switch on "Crowdmap" but it works for single session and for mobile active measurements.
+ * We redraw the heatmap with every zoom and pan, because the grid squares are always calculated on the visible part of the map.
+ * We divide the visible are of the map into virtual squares (mGridSquares). We calculate the size of the single square
+ * using heatmap density and size of the visible area on the screen.
+ * For example, a "portrait" map may be divided into 10x12 grid. In this case gridSizeX = 10 and gridSizeY = 12.
+ * For each grid square we calculate coordinates for each corner so we can use them to draw a Polygon if needed.
+ *
+ * NW-----NE
+ * |      |
+ * |      |
+ * SW-----SE
+ *
+ * On heatmap redraw, we go through all measurements we want to map and assign them to squares. The algorithm for this
+ * is a simple binary search based on grid squares coordinates. On assigning measurements, we caclulate the average
+ * value of each square and assign an appropriate level.
+ *
+ * With every new measurement, the average of appropriate square is recalculated and polygon redrawn
+ * (only if the level has changed)
  */
 
-class MapGrid(val context: Context?, val map: GoogleMap, val sensorThreshold: SensorThreshold, mapWidth: Int, mapHeight: Int) {
+class MapGrid(val context: Context?, val mMap: GoogleMap, val sensorThreshold: SensorThreshold, mapWidth: Int, mapHeight: Int) {
     private val DENSITY = 10
 
     var mGridSquares: HashMap<String, GridSquare?> = HashMap()
-    var latNorth: Double
-    var latSouth: Double
-    var lonEast: Double
-    var lonWest: Double
+    var mVisibleRegion: VisibleRegionCoordinates
 
     var gridSizeX: Int
     var gridSizeY: Int
 
     init {
-        val visibleRegion = map.projection.visibleRegion
-        latNorth = visibleRegion.farLeft.latitude
-        latSouth = visibleRegion.nearLeft.latitude
-        lonEast = visibleRegion.farRight.longitude
-        lonWest = visibleRegion.nearLeft.longitude
+        mVisibleRegion = getVisibleRegionCoordinates()
 
         val size = Math.min(mapWidth, mapHeight) / DENSITY
 
         gridSizeX = mapWidth / size
         gridSizeY = mapHeight / size
 
-        val lonGridSide = (lonEast - lonWest) / gridSizeX
-        val latGridSide = (latNorth - latSouth) / gridSizeY
+        val lonGridSide = (mVisibleRegion.lonEast - mVisibleRegion.lonWest) / gridSizeX
+        val latGridSide = (mVisibleRegion.latNorth - mVisibleRegion.latSouth) / gridSizeY
 
 
         for (x in 1..gridSizeX) {
             for (y in 1..gridSizeY) {
                 val southWestLatLng =
-                    LatLng(latSouth + (y - 1) * latGridSide, lonWest + (x - 1) * lonGridSide)
+                    LatLng(southLatitude(latGridSide, y), westLongitude(lonGridSide, x))
                 val southEastLatLng =
-                    LatLng(latSouth + (y - 1) * latGridSide, lonWest + x * lonGridSide)
-                val northEastLatLng = LatLng(latSouth + y * latGridSide, lonWest + x * lonGridSide)
+                    LatLng(southLatitude(latGridSide, y), eastLongitude(lonGridSide, x))
+                val northEastLatLng =
+                    LatLng(northLatitude(latGridSide, y), eastLongitude(lonGridSide, x))
                 val northWestLatLng =
-                    LatLng((latSouth + y * latGridSide), lonWest + (x - 1) * lonGridSide)
+                    LatLng(northLatitude(latGridSide, y), westLongitude(lonGridSide, x))
 
                 setGridSquare(x, y, GridSquare(
                     southWestLatLng,
@@ -75,6 +87,33 @@ class MapGrid(val context: Context?, val map: GoogleMap, val sensorThreshold: Se
         drawAverages()
     }
 
+    private fun eastLongitude(longitudeSize: Double, x: Int) : Double {
+        return mVisibleRegion.lonWest + x * longitudeSize
+    }
+
+    private fun westLongitude(longitudeSize: Double, x: Int) : Double {
+        return mVisibleRegion.lonWest + (x - 1) * longitudeSize
+    }
+
+    private fun northLatitude(latitudeSize: Double, y: Int) : Double {
+        return mVisibleRegion.latSouth + y * latitudeSize
+    }
+
+    private fun southLatitude(latitudeSize: Double, y: Int) : Double {
+        return mVisibleRegion.latSouth + (y - 1) * latitudeSize
+    }
+
+    private fun getVisibleRegionCoordinates(): VisibleRegionCoordinates {
+        val visibleRegion = mMap.projection.visibleRegion
+
+        return VisibleRegionCoordinates(
+            visibleRegion.farLeft.latitude,
+            visibleRegion.nearLeft.latitude,
+            visibleRegion.farRight.longitude,
+            visibleRegion.nearLeft.longitude
+        )
+    }
+
     private fun getGridSquare(x: Int, y: Int): GridSquare? {
         return mGridSquares[gridSquareKey(x, y)]
     }
@@ -87,6 +126,7 @@ class MapGrid(val context: Context?, val map: GoogleMap, val sensorThreshold: Se
         return "${x}_${y}"
     }
     private fun getSquareXY(measurement: Measurement, indexXstart: Int, indexXend: Int, indexYstart: Int, indexYend: Int): Pair<Int, Int>? {
+        // TODO: actually, let's always check if coordinates are in bounds of the middle square?- sometimes we may skip couple of steps this way. In case of one square left, it shoudl be the middle one...)
         if (indexXstart == indexXend && indexYstart == indexYend) {
             val gridSquare = getGridSquare(indexXstart + 1, indexYstart + 1)
             if (gridSquare == null || measurement.longitude == null || measurement.latitude == null) return null
@@ -211,7 +251,7 @@ class MapGrid(val context: Context?, val map: GoogleMap, val sensorThreshold: Se
         }
 
         private fun addPolygon() {
-            val polygon = map.addPolygon(mPolygonOptions)
+            val polygon = mMap.addPolygon(mPolygonOptions)
             if (mPolygon != null) {
                 mPolygon?.remove()
             }
@@ -246,4 +286,6 @@ class MapGrid(val context: Context?, val map: GoogleMap, val sensorThreshold: Se
             mAveragedValue = mSum / mNumber
         }
     }
+
+    class VisibleRegionCoordinates(val latNorth: Double, val latSouth: Double, var lonEast: Double, val lonWest: Double)
 }
