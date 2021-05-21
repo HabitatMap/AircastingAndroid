f1adc21431c700301ca866984165ec6853990c04
Maria Turnau-Gurgul <maria.turnau@lunarlogic.io>
2021-05-21 13:11:17 +0200

    Small refactor

--- app/src/main/java/io/lunarlogic/aircasting/services/AveragingService.kt
+++ app/src/main/java/io/lunarlogic/aircasting/services/AveragingService.kt
@@ -38,8 +38,8 @@ class AveragingService {
     companion object {
         private val FIRST_TRESHOLD_TIME = 2 * 60 * 60 * 1000 // 2 hours
         val FIRST_THRESHOLD_FREQUENCY = 5
-        private val SECOND_THRESHOLD_FREQUENCY = 60
         private val SECOND_TRESHOLD_TIME = 9 * 60 * 60 * 1000 // 9 hours
+        private val SECOND_THRESHOLD_FREQUENCY = 60
 
         private val THRESHOLDS = arrayOf(
             AveragingThreshold(
