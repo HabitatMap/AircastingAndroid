c9fb24135a002b79c56c4f16e338c98f5beb9893
Maria Turnau-Gurgul <maria.turnau@lunarlogic.io>
2021-05-26 12:48:13 +0200

    Added missing alter table to migration

--- app/src/main/java/io/lunarlogic/aircasting/database/migrations/migration_26.kt
+++ app/src/main/java/io/lunarlogic/aircasting/database/migrations/migration_26.kt
@@ -6,5 +6,6 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 val MIGRATION_25_26 = object : Migration(25, 26) {
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL("ALTER TABLE `measurements` ADD `averaging_frequency` INTEGER NOT NULL DEFAULT 1")
+        database.execSQL("ALTER TABLE `sessions` ADD `averaging_frequency` INTEGER NOT NULL DEFAULT 1")
     }
 }
