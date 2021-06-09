eb66e193a3bf5a1f40e9bfd0bf4ae7986ada8078
Maria Turnau-Gurgul <maria.turnau@lunarlogic.io>
2021-06-09 12:51:39 +0200

    Formatted a class

--- app/src/main/java/io/lunarlogic/aircasting/networking/responses/SyncResponse.kt
+++ app/src/main/java/io/lunarlogic/aircasting/networking/responses/SyncResponse.kt
@@ -1,3 +1,7 @@
 package io.lunarlogic.aircasting.networking.responses
 
-class SyncResponse(val upload: List<String> = emptyList(), val download: List<String> = emptyList(), val deleted: List<String> = emptyList())
+class SyncResponse(
+    val upload: List<String> = emptyList(),
+    val download: List<String> = emptyList(),
+    val deleted: List<String> = emptyList()
+)
