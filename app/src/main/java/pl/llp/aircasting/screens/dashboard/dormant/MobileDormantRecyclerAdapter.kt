a6bb2a61274c46f990d2b3ff5eaeeead29f2272a
michalrazny2 <michalrazny2@o2.pl>
2021-06-28 14:19:57 +0200

    first attempt to fix mobile dormant> expand card: doesnt display avg values bug

--- app/src/main/java/pl/llp/aircasting/screens/dashboard/dormant/MobileDormantRecyclerAdapter.kt
+++ app/src/main/java/pl/llp/aircasting/screens/dashboard/dormant/MobileDormantRecyclerAdapter.kt
@@ -3,6 +3,10 @@ package pl.llp.aircasting.screens.dashboard.dormant
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 import pl.llp.aircasting.models.Session
 import pl.llp.aircasting.screens.dashboard.SessionsRecyclerAdapter
 
@@ -25,6 +29,24 @@ class MobileDormantRecyclerAdapter(
     }
 
     override fun prepareSession(session: Session, expanded: Boolean): Session {
+//        if (!expanded) {
+//            return session
+//        }
+//
+//        var reloadedSession: Session? = null
+//
+//        runBlocking {
+//            val query = GlobalScope.async(Dispatchers.IO) {
+//                val dbSessionWithMeasurements = mSessionsViewModel.reloadSessionWithMeasurements(session.uuid)
+//                dbSessionWithMeasurements?.let {
+//                    reloadedSession = Session(dbSessionWithMeasurements)
+//                }
+//            }
+//            query.await()
+//        }
+//
+//        return reloadedSession ?: session
+
         // We only have to reload measurements for fixed tab for expanded sessions. Mobile dormant sessions are not reloaded anymore
         return session
     }
