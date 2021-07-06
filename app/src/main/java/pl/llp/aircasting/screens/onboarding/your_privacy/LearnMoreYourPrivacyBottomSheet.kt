f209124b58662f1b1e828e467f4895965419e9b9
michalrazny2 <michalrazny2@o2.pl>
2021-07-06 11:51:07 +0200

    updated HabitatMap's privacy policy to a longer one

--- app/src/main/java/pl/llp/aircasting/screens/onboarding/your_privacy/LearnMoreYourPrivacyBottomSheet.kt
+++ app/src/main/java/pl/llp/aircasting/screens/onboarding/your_privacy/LearnMoreYourPrivacyBottomSheet.kt
@@ -1,7 +1,12 @@
 package pl.llp.aircasting.screens.onboarding.your_privacy
 
 import android.app.Dialog
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.text.bold
+import androidx.core.text.color
 import pl.llp.aircasting.R
 import pl.llp.aircasting.screens.common.BottomSheet
 import kotlinx.android.synthetic.main.learn_more_onboarding_your_privacy.view.*
@@ -27,6 +32,17 @@ class LearnMoreYourPrivacyBottomSheet: BottomSheet() {
     }
 
     private fun buildDescription(): String {
-        return getString(R.string.onboarding_bottomsheet_page4_paragraph_1) + "\n\n" + getString(R.string.onboarding_bottomsheet_page4_paragraph_2) + "\n\n" + getString(R.string.onboarding_bottomsheet_page4_paragraph_3) + "\n\n" + getString(R.string.onboarding_bottomsheet_page4_paragraph_4) + "\n\n" + getString(R.string.onboarding_bottomsheet_page4_paragraph_5) + "\n\n" + getString(R.string.onboarding_bottomsheet_page4_paragraph_6) + "\n\n" + getString(R.string.onboarding_bottomsheet_page4_paragraph_7)
+        return (getString(R.string.onboarding_bottomsheet_page4_paragraph_header) + "\n" +
+                getString(R.string.onboarding_bottomsheet_page4_paragraph_1) + "\n" + getString(R.string.onboarding_bottomsheet_page4_description_1) + "\n\n" +
+                getString(R.string.onboarding_bottomsheet_page4_paragraph_2) + "\n" + getString(R.string.onboarding_bottomsheet_page4_description_2) + "\n\n" +
+                getString(R.string.onboarding_bottomsheet_page4_paragraph_3) + "\n" + getString(R.string.onboarding_bottomsheet_page4_description_3) + "\n\n" +
+                getString(R.string.onboarding_bottomsheet_page4_paragraph_4) + "\n" + getString(R.string.onboarding_bottomsheet_page4_description_4) + "\n\n" +
+                getString(R.string.onboarding_bottomsheet_page4_paragraph_5) + "\n" + getString(R.string.onboarding_bottomsheet_page4_description_5) + "\n\n" +
+                getString(R.string.onboarding_bottomsheet_page4_paragraph_6) + "\n" + getString(R.string.onboarding_bottomsheet_page4_description_6) + "\n\n" +
+                getString(R.string.onboarding_bottomsheet_page4_paragraph_7) + "\n" + getString(R.string.onboarding_bottomsheet_page4_description_7) + "\n\n" +
+                getString(R.string.onboarding_bottomsheet_page4_paragraph_8) + "\n" + getString(R.string.onboarding_bottomsheet_page4_description_8) + "\n\n" +
+                getString(R.string.onboarding_bottomsheet_page4_paragraph_9) + "\n" + getString(R.string.onboarding_bottomsheet_page4_description_9) + "\n\n" +
+                getString(R.string.onboarding_bottomsheet_page4_paragraph_10) + "\n" + getString(R.string.onboarding_bottomsheet_page4_description_10) + "\n\n" +
+                getString(R.string.onboarding_bottomsheet_page4_paragraph_11) + "\n" + getString(R.string.onboarding_bottomsheet_page4_description_11) + "\n\n")
     }
 }
