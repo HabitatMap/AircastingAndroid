c976bbf4607cb03e3afce0234d5f58e39747ec8e
michalrazny2 <michalrazny2@o2.pl>
2021-08-02 12:14:14 +0200

    cleaning log

--- app/src/main/java/pl/llp/aircasting/networking/services/SessionDownloadService.kt
+++ app/src/main/java/pl/llp/aircasting/networking/services/SessionDownloadService.kt
@@ -1,6 +1,5 @@
 package pl.llp.aircasting.networking.services
 
-import android.util.Log
 import pl.llp.aircasting.exceptions.ErrorHandler
 import pl.llp.aircasting.exceptions.UnexpectedAPIError
 import pl.llp.aircasting.lib.DateConverter
@@ -36,7 +35,6 @@ class SessionDownloadService(private val apiService: ApiService, private val err
             }
 
             override fun onFailure(call: Call<SessionResponse>, t: Throwable) {
-                Log.i("SESS_FAIL", t.message.toString())
                 errorHandler.handle(UnexpectedAPIError(t))
                 finallyCallback?.invoke()
             }
