a480469bfe8af6d78ee22ba343f9dfa695027401
michalrazny2 <michalrazny2@o2.pl>
2021-08-11 15:47:38 +0200

    rebase 14

--- app/src/main/java/pl/llp/aircasting/networking/responses/UploadSessionResponse.kt
+++ app/src/main/java/pl/llp/aircasting/networking/responses/UploadSessionResponse.kt
@@ -3,4 +3,3 @@ package pl.llp.aircasting.networking.responses
 import pl.llp.aircasting.models.Note
 
 class UploadSessionResponse(val location: String, val notes: List<Note>) //todo: Note probably should be changed for UploadNoteResponse
-
