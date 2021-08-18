f4355eaed5430d928afc763d94e3c6135da7552c
michalrazny2 <michalrazny2@o2.pl>
2021-08-18 10:01:11 +0200

    optimized imports in DurationStringHelper.kt

--- app/src/main/java/pl/llp/aircasting/lib/DurationStringHelper.kt
+++ app/src/main/java/pl/llp/aircasting/lib/DurationStringHelper.kt
@@ -1,7 +1,5 @@
 package pl.llp.aircasting.lib
 
-import pl.llp.aircasting.models.Session
-import java.text.SimpleDateFormat
 import java.util.*
 
 class DurationStringHelper {
@@ -9,7 +7,6 @@ class DurationStringHelper {
     companion object {
 
         fun durationString(startTime: Date?, endTime: Date?): String {
-
             var durationString = "${DateConverter.toDateStringForDisplay(startTime!!)} ${DateConverter.toTimeStringForDisplay(startTime)}"
 
             if (endTime == null) return durationString
