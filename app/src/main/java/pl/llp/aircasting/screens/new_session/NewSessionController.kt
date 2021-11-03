ba2b563cc60965bb34e2a44aec8e018381bc2320
michalrazny2 <michalrazny2@o2.pl>
2021-11-03 10:09:36 +0100

    clearing flag in NewSessionController.kt is not needed, therefore removed

--- app/src/main/java/pl/llp/aircasting/screens/new_session/NewSessionController.kt
+++ app/src/main/java/pl/llp/aircasting/screens/new_session/NewSessionController.kt
@@ -96,7 +96,6 @@ class NewSessionController(
     }
 
     fun onStop() {
-        mContextActivity.window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         EventBus.getDefault().unregister(this)
     }
     
