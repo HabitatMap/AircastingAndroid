97ce5f6f2ed46371d785e1fd3cacffdc89a5b351
kgetman <kgetmanskii@gmail.com>
2022-02-22 15:15:55 +0300

    Commented out convertStream function

--- app/src/main/java/pl/llp/aircasting/lib/TemperatureConverter.kt
+++ app/src/main/java/pl/llp/aircasting/lib/TemperatureConverter.kt
@@ -1,6 +1,5 @@
 package pl.llp.aircasting.lib
 
-import pl.llp.aircasting.models.Measurement
 import pl.llp.aircasting.models.MeasurementStream
 
 class TemperatureConverter private constructor(settings: Settings) {
@@ -23,35 +22,35 @@ class TemperatureConverter private constructor(settings: Settings) {
     fun convertStream(stream: MeasurementStream): MeasurementStream {
         var measurementStream: MeasurementStream = stream
 
-        if (stream.detailedType == "F" && mSettings?.isCelsiusScaleEnabled() == true) {
-            measurementStream = MeasurementStream(
-                measurementStream.sensorPackageName,
-                measurementStream.sensorName,
-                measurementStream.measurementType,
-                measurementStream.measurementShortType,
-                measurementStream.unitName,
-                measurementStream.unitSymbol,
-                measurementStream.thresholdVeryLow,
-                measurementStream.thresholdLow,
-                measurementStream.thresholdMedium,
-                measurementStream.thresholdHigh,
-                measurementStream.thresholdVeryHigh,
-                measurementStream.deleted,
-                stream.measurements.map { measurement ->
-                    Measurement(
-                        temperatureFromFahrenheitToCelsius(measurement.value),
-                        measurement.time,
-                        measurement.latitude,
-                        measurement.longitude,
-                        measurement.averagingFrequency
-                    )
-
-                }
-            )
-            measurementStream.detailedType = "C"
-        }
-
-        return measurementStream
+//        if (stream.detailedType == "F" && mSettings?.isCelsiusScaleEnabled() == true) {
+//            measurementStream = MeasurementStream(
+//                measurementStream.sensorPackageName,
+//                measurementStream.sensorName,
+//                measurementStream.measurementType,
+//                measurementStream.measurementShortType,
+//                measurementStream.unitName,
+//                measurementStream.unitSymbol,
+//                measurementStream.thresholdVeryLow,
+//                measurementStream.thresholdLow,
+//                measurementStream.thresholdMedium,
+//                measurementStream.thresholdHigh,
+//                measurementStream.thresholdVeryHigh,
+//                measurementStream.deleted,
+//                stream.measurements.map { measurement ->
+//                    Measurement(
+//                        temperatureFromFahrenheitToCelsius(measurement.value),
+//                        measurement.time,
+//                        measurement.latitude,
+//                        measurement.longitude,
+//                        measurement.averagingFrequency
+//                    )
+//
+//                }
+//            )
+//            measurementStream.detailedType = "C"
+//        }
+        // set back to measurementStream
+        return stream
     }
 
 }
