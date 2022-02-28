90e48ccfdc8cd87ba22924f4888ecf1ffdf7500d
Mohsen Rezania <ash.wxrz@hotmail.com>
2022-02-28 14:04:01 +0200

    Added the Extension for Int

--- app/src/main/java/pl/llp/aircasting/lib/extensions.kt
+++ app/src/main/java/pl/llp/aircasting/lib/extensions.kt
@@ -15,11 +15,21 @@ fun Context.areLocationServicesOn(): Boolean {
     return manager!!.isProviderEnabled(LocationManager.GPS_PROVIDER)
 }
 
+/**
+ * Converting Extensions
+ * Here we have different extensions for converting from Fahrenheit to Celsius and Vise versa.
+ * It accepts Double, Float, Int.
+ */
+
 fun temperatureFromFahrenheitToCelsius(fahrenheitTemperature: Double): Double {
     return ((fahrenheitTemperature - 32) * 5) / 9
 }
 
 fun temperatureFromFahrenheitToCelsius(fahrenheitTemperature: Float): Float {
+    return ((fahrenheitTemperature - 32) * 5) / 9
+}
+
+fun temperatureFromFahrenheitToCelsius(fahrenheitTemperature: Int): Int {
     return ((fahrenheitTemperature - 32) * 5) / 9
 }
 
