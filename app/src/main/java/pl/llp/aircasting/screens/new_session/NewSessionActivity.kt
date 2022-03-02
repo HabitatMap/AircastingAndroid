19f31ad0103773a84f4cb7067364ce42fd3c46f9
Mohsen <ash.wxrz@hotmail.com>
2022-03-02 13:39:29 +0200

    Removed unnecessary checks and code cleanup

--- app/src/main/java/pl/llp/aircasting/screens/new_session/NewSessionActivity.kt
+++ app/src/main/java/pl/llp/aircasting/screens/new_session/NewSessionActivity.kt
@@ -4,13 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentActivity
 import pl.llp.aircasting.AircastingApplication
 import pl.llp.aircasting.bluetooth.BluetoothManager
 import pl.llp.aircasting.lib.AppBar
 import pl.llp.aircasting.lib.NavigationController
-import pl.llp.aircasting.lib.Settings
 import pl.llp.aircasting.models.Session
 import pl.llp.aircasting.models.SessionBuilder
 import pl.llp.aircasting.permissions.PermissionsManager
@@ -37,33 +35,32 @@ class NewSessionActivity : BaseActivity() {
         private var mobileLauncher: ActivityResultLauncher<Intent>? = null
 
         fun register(rootActivity: FragmentActivity?, sessionType: Session.Type) {
-            rootActivity?.let{
-                val launcher = it.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-                    val tabId = DashboardPagerAdapter.tabIndexForSessionType(sessionType, Session.Status.RECORDING)
-                    if (it.resultCode == RESULT_OK) {
-                        NavigationController.goToDashboard(tabId)
+            rootActivity?.let {
+                val launcher =
+                    it.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                        val tabId = DashboardPagerAdapter.tabIndexForSessionType(
+                            sessionType,
+                            Session.Status.RECORDING
+                        )
+                        if (it.resultCode == RESULT_OK) {
+                            NavigationController.goToDashboard(tabId)
+                        }
                     }
-                }
-                if (sessionType == Session.Type.FIXED)
-                    fixedLauncher = launcher
-                else if (sessionType == Session.Type.MOBILE)
-                    mobileLauncher = launcher
+                if (sessionType == Session.Type.FIXED) fixedLauncher =
+                    launcher else mobileLauncher = launcher
+
             }
         }
 
         fun start(rootActivity: FragmentActivity?, sessionType: Session.Type) {
-            rootActivity?.let{
+            rootActivity?.let {
                 val intent = Intent(it, NewSessionActivity::class.java)
                 intent.putExtra(SESSION_TYPE_KEY, sessionType)
 
-                if (sessionType == Session.Type.FIXED)
-                    fixedLauncher?.launch(intent)
-                else {
-                    if (sessionType == Session.Type.MOBILE) {
-                        mobileLauncher?.launch(intent)
-                    }
-                    else {}
-                }
+                if (sessionType == Session.Type.FIXED) fixedLauncher?.launch(intent) else mobileLauncher?.launch(
+                    intent
+                )
+
             }
         }
     }
@@ -109,8 +106,10 @@ class NewSessionActivity : BaseActivity() {
         controller?.onBackPressed()
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         controller?.onRequestPermissionsResult(requestCode, grantResults)
     }
