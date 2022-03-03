c32b1338bdcadfdae4142da477a2bf79a064cac4
kgetman <kgetmanskii@gmail.com>
2022-03-03 17:56:23 +0300

    Moved converting methods to TemperatureConverter. Now everything related to temperature is in one place

--- app/src/main/java/pl/llp/aircasting/lib/TemperatureConverter.kt
+++ app/src/main/java/pl/llp/aircasting/lib/TemperatureConverter.kt
@@ -22,6 +22,22 @@ class TemperatureConverter private constructor(settings: Settings) {
         fun isCelsiusToggleEnabled(): Boolean {
             return singleton?.mSettings?.isCelsiusScaleEnabled() == true
         }
+
+        fun fahrenheitToCelsius(fahrenheitTemperature: Double): Double {
+            return (Math.round(((fahrenheitTemperature - 32) * 5) / 9)).toDouble()
+        }
+
+        fun fahrenheitToCelsius(fahrenheitTemperature: Float): Float {
+            return (Math.round(((fahrenheitTemperature - 32) * 5) / 9)).toFloat()
+        }
+
+        fun celsiusToFahrenheit(fahrenheitTemperature: Int): Int {
+            return fahrenheitTemperature * 9 / 5 + 32
+        }
+
+        fun celsiusToFahrenheit(fahrenheitTemperature: Double): Double {
+            return (Math.round(fahrenheitTemperature * 9 / 5 + 32)).toDouble()
+        }
     }
 
     private var mSettings: Settings? = settings
