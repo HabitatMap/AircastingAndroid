c80b4cf6899e393d3b8117d6cf229d376b43dfeb
Mohsen <ash.wxrz@hotmail.com>
2022-03-06 01:41:13 +0200

    Added the missing part for MobileSession

--- app/src/main/java/pl/llp/aircasting/screens/new_session/confirmation/MobileSessionConfirmationViewMvcImpl.kt
+++ app/src/main/java/pl/llp/aircasting/screens/new_session/confirmation/MobileSessionConfirmationViewMvcImpl.kt
@@ -3,8 +3,12 @@ package pl.llp.aircasting.screens.new_session.confirmation
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import pl.llp.aircasting.R
+import pl.llp.aircasting.lib.BitmapHelper
 import pl.llp.aircasting.models.Session
 
 class MobileSessionConfirmationViewMvcImpl(
@@ -26,7 +30,15 @@ class MobileSessionConfirmationViewMvcImpl(
         updateMarkerPosition(latitude, longitude)
     }
 
-    override fun onMapReady(p0: GoogleMap) {
-        // TODO: Check this later.
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val sessionLocation = session?.location ?: return
+        val location = LatLng(sessionLocation.latitude, sessionLocation.longitude)
+        val icon = BitmapHelper.bitmapFromVector(context, R.drawable.ic_dot_20)
+        val marker = MarkerOptions()
+            .position(location)
+            .icon(icon)
+        mMarker = googleMap.addMarker(marker)
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, DEFAULT_ZOOM))
     }
 }
