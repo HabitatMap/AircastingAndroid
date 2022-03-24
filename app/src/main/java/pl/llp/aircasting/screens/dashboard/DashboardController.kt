95a8cf3e52b68d112d96e6bbd14602bd7d7c7956
Mohsen <ash.wxrz@hotmail.com>
2022-03-24 02:27:09 +0200

    Replaced the unnecessary operation with just one line

--- app/src/main/java/pl/llp/aircasting/screens/dashboard/DashboardController.kt
+++ app/src/main/java/pl/llp/aircasting/screens/dashboard/DashboardController.kt
@@ -1,32 +1,12 @@
 package pl.llp.aircasting.screens.dashboard
 
-import pl.llp.aircasting.database.DatabaseProvider
-import pl.llp.aircasting.database.repositories.SessionsRepository
-import pl.llp.aircasting.models.Session
 import pl.llp.aircasting.screens.common.BaseController
 
 class DashboardController(
     private val viewMvc: DashboardViewMvcImpl?
 ) : BaseController<DashboardViewMvcImpl>(viewMvc) {
-    private val mSessionRepository = SessionsRepository()
 
     fun onCreate(tabId: Int?) {
-        DatabaseProvider.runQuery { scope ->
-            if (mSessionRepository.mobileActiveSessionExists()) {
-                DatabaseProvider.backToUIThread(scope) {
-                    viewMvc?.goToTab(
-                        DashboardPagerAdapter.tabIndexForSessionType(
-                            Session.Type.MOBILE,
-                            Session.Status.RECORDING
-                        )
-                    )
-                }
-            }
-            else {
-                DatabaseProvider.backToUIThread(scope) {
-                    viewMvc?.goToTab(tabId ?: 0)
-                }
-            }
-        }
+        viewMvc?.goToTab(tabId ?: 0)
     }
 }
