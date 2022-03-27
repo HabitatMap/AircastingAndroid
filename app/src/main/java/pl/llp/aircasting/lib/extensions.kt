b86f19010f6af9bfcfa48a32c0a1e4c14ab3281d
Mohsen <ash.wxrz@hotmail.com>
2022-03-27 02:04:47 +0200

    Added isEmailValid as an extension function

--- app/src/main/java/pl/llp/aircasting/lib/extensions.kt
+++ app/src/main/java/pl/llp/aircasting/lib/extensions.kt
@@ -4,6 +4,8 @@ import android.app.Activity
 import android.content.Context
 import android.location.LocationManager
 import android.os.Build
+import android.text.TextUtils
+import android.util.Patterns
 import android.view.View
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -53,4 +55,8 @@ fun adjustMenuVisibility(
         if (isFollowingTab && followingSessionsNumber >= 2) View.VISIBLE else View.INVISIBLE
     activity.topAppBar?.findViewById<ConstraintLayout>(R.id.reorder_buttons_group)?.visibility =
         visibility
+}
+
+fun isValidEmail(target: String): Boolean {
+    return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches())
 }
