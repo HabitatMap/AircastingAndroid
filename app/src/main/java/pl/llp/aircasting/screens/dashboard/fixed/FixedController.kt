1341ea80756658031ffe87c2817caa8bc7f48484
Mohsen <ash.wxrz@hotmail.com>
2022-03-28 18:47:20 +0300

    Navigating to lets start navigation/fragment indirectly

--- app/src/main/java/pl/llp/aircasting/screens/dashboard/fixed/FixedController.kt
+++ app/src/main/java/pl/llp/aircasting/screens/dashboard/fixed/FixedController.kt
@@ -4,18 +4,19 @@ import android.content.Context
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.LifecycleOwner
-import pl.llp.aircasting.lib.NavigationController
+import androidx.navigation.Navigation
+import pl.llp.aircasting.R
 import pl.llp.aircasting.lib.Settings
-import pl.llp.aircasting.models.observers.FixedSessionsObserver
-import pl.llp.aircasting.models.SessionsViewModel
 import pl.llp.aircasting.models.Session
+import pl.llp.aircasting.models.SessionsViewModel
+import pl.llp.aircasting.models.observers.FixedSessionsObserver
 import pl.llp.aircasting.networking.services.ApiServiceFactory
 import pl.llp.aircasting.screens.dashboard.EditSessionBottomSheet
 import pl.llp.aircasting.screens.dashboard.SessionsController
 import pl.llp.aircasting.screens.dashboard.SessionsViewMvc
 
 class FixedController(
-    mRootActivity: FragmentActivity?,
+    private val mRootActivity: FragmentActivity?,
     mViewMvc: SessionsViewMvc?,
     private val mSessionsViewModel: SessionsViewModel,
     mLifecycleOwner: LifecycleOwner,
@@ -23,10 +24,19 @@ class FixedController(
     mApiServiceFactory: ApiServiceFactory,
     fragmentManager: FragmentManager,
     mContext: Context?
-): SessionsController(mRootActivity, mViewMvc, mSessionsViewModel, mSettings, mApiServiceFactory, fragmentManager, mContext),
+) : SessionsController(
+    mRootActivity,
+    mViewMvc,
+    mSessionsViewModel,
+    mSettings,
+    mApiServiceFactory,
+    fragmentManager,
+    mContext
+),
     SessionsViewMvc.Listener, EditSessionBottomSheet.Listener {
 
-    private var mSessionsObserver = FixedSessionsObserver(mLifecycleOwner, mSessionsViewModel, mViewMvc)
+    private var mSessionsObserver =
+        FixedSessionsObserver(mLifecycleOwner, mSessionsViewModel, mViewMvc)
 
     override fun registerSessionsObserver() {
         mSessionsObserver.observe(mSessionsViewModel.loadFixedSessionsWithMeasurements())
@@ -37,7 +47,10 @@ class FixedController(
     }
 
     override fun onRecordNewSessionClicked() {
-        NavigationController.goToLetsStart()
+        mRootActivity?.let {
+            Navigation.findNavController(it, R.id.nav_host_fragment)
+                .navigate(R.id.navigation_lets_start)
+        }
     }
 
     override fun onFinishSessionConfirmed(session: Session) {
