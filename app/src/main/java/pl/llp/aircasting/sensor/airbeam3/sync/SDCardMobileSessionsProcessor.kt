9954660c074dc513d316f1b94f8449ac44f3eb10
kgetman <kgetmanskii@gmail.com>
2022-04-01 17:23:06 +0200

    Made SDCardMobileSessionsProcessor to inherit SDCardSessionsProcessor

--- app/src/main/java/pl/llp/aircasting/sensor/airbeam3/sync/SDCardMobileSessionsProcessor.kt
+++ app/src/main/java/pl/llp/aircasting/sensor/airbeam3/sync/SDCardMobileSessionsProcessor.kt
@@ -6,18 +6,23 @@ import pl.llp.aircasting.database.repositories.MeasurementsRepository
 import pl.llp.aircasting.database.repositories.SessionsRepository
 import pl.llp.aircasting.models.Session
 import pl.llp.aircasting.sensor.airbeam3.sync.SDCardCSVFileFactory.Header
+import java.io.File
 
 class SDCardMobileSessionsProcessor(
-    private val mCSVFileFactory: SDCardCSVFileFactory,
-    private val mSDCardCSVIterator: SDCardCSVIterator,
-    private val mSessionsRepository: SessionsRepository,
-    private val mMeasurementStreamsRepository: MeasurementStreamsRepository,
-    private val mMeasurementsRepository: MeasurementsRepository
+    mCSVFileFactory: SDCardCSVFileFactory,
+    mSDCardCSVIterator: SDCardCSVIterator,
+    mSessionsRepository: SessionsRepository,
+    mMeasurementStreamsRepository: MeasurementStreamsRepository,
+    mMeasurementsRepository: MeasurementsRepository
+) : SDCardSessionsProcessor(
+    mCSVFileFactory,
+    mSDCardCSVIterator,
+    mSessionsRepository,
+    mMeasurementStreamsRepository,
+    mMeasurementsRepository
 ) {
-    private val mProcessedSessionsIds: MutableList<Long> = mutableListOf()
-
-    fun run(deviceId: String, onFinishCallback: (MutableList<Long>) -> Unit) {
-        val file = mCSVFileFactory.getMobileFile()
+    override val file: File
+        get() = mCSVFileFactory.getMobileFile()
 
         DatabaseProvider.runQuery {
             mSDCardCSVIterator.run(file).forEach { csvSession ->
@@ -53,33 +58,21 @@ class SDCardMobileSessionsProcessor(
         }
     }
 
-    private fun processMeasurements(deviceId: String, sessionId: Long, streamHeaderValue: Int, csvMeasurements: List<CSVMeasurement>) {
-        val streamHeader = Header.fromInt(streamHeaderValue)
-        val csvMeasurementStream = CSVMeasurementStream.fromHeader(
-            streamHeader
-        ) ?: return
-
-        val measurementStream = csvMeasurementStream.toMeasurementStream(deviceId)
-        val measurementStreamId = mMeasurementStreamsRepository.getIdOrInsert(
-            sessionId,
-            measurementStream
-        )
-
-        // filtering measurements to save only the once we don't already have
-        val filteredCSVMeasurements = filterMeasurements(sessionId, measurementStreamId, csvMeasurements)
-        val measurements = filteredCSVMeasurements.map { csvMeasurement -> csvMeasurement.toMeasurement() }
-        mMeasurementsRepository.insertAll(measurementStreamId, sessionId, measurements)
-    }
-
-    private fun filterMeasurements(sessionId: Long, measurementStreamId: Long, csvMeasurements: List<CSVMeasurement>): List<CSVMeasurement> {
-        val lastMeasurementTime = mMeasurementsRepository.lastMeasurementTime(sessionId, measurementStreamId) ?: return csvMeasurements
-
-        return csvMeasurements.filter { csvMeasurement -> csvMeasurement.time > lastMeasurementTime }
-    }
-
     private fun finishSession(sessionId: Long, session: Session) {
         val lastMeasurementTime = mMeasurementsRepository.lastMeasurementTime(sessionId)
         session.stopRecording(lastMeasurementTime)
         mSessionsRepository.update(session)
+    }
+
+    override fun filterMeasurements(
+        sessionId: Long,
+        measurementStreamId: Long,
+        csvMeasurements: List<CSVMeasurement>
+    ): List<CSVMeasurement> {
+        val lastMeasurementTime =
+            mMeasurementsRepository.lastMeasurementTime(sessionId, measurementStreamId)
+                ?: return csvMeasurements
+
+        return csvMeasurements.filter { csvMeasurement -> csvMeasurement.time > lastMeasurementTime }
     }
 }
