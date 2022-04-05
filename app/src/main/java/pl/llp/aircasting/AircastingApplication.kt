21f9f51c5061d1994232981557b7c0db138fb761
Mohsen <ash.wxrz@hotmail.com>
2022-04-05 19:40:42 +0300

    Added work configuration

--- app/src/main/java/pl/llp/aircasting/AircastingApplication.kt
+++ app/src/main/java/pl/llp/aircasting/AircastingApplication.kt
@@ -3,13 +3,26 @@ package pl.llp.aircasting
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.ProcessLifecycleOwner
+import androidx.work.Configuration
 import pl.llp.aircasting.di.AppModule
 import pl.llp.aircasting.di.PermissionsModule
 import pl.llp.aircasting.lib.Settings
 import pl.llp.aircasting.models.observers.AppLifecycleObserver
 
+class AircastingApplication : Application(), Configuration.Provider {
 
-class AircastingApplication: Application() {
+    override fun getWorkManagerConfiguration(): Configuration {
+        return if (BuildConfig.DEBUG) {
+            Configuration.Builder()
+                .setMinimumLoggingLevel(android.util.Log.DEBUG)
+                .build()
+        } else {
+            Configuration.Builder()
+                .setMinimumLoggingLevel(android.util.Log.ERROR)
+                .build()
+        }
+    }
+
     lateinit var appComponent: AppComponent
     lateinit var permissionsModule: PermissionsModule
     private var mSettings: Settings? = null
