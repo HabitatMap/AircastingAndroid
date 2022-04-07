ee6e454ddbdd7445a8020c36458681cb21d2d6d9
Mohsen <ash.wxrz@hotmail.com>
2022-04-07 12:54:47 +0300

    Work manager implementation with specified and scheduled time

--- app/src/main/java/pl/llp/aircasting/networking/services/PeriodicallyDownloadFixedSessionMeasurementsService.kt
+++ app/src/main/java/pl/llp/aircasting/networking/services/PeriodicallyDownloadFixedSessionMeasurementsService.kt
@@ -1,5 +1,10 @@
 package pl.llp.aircasting.networking.services
 
+import android.content.Context
+import androidx.work.*
+import androidx.work.PeriodicWorkRequest.MIN_PERIODIC_FLEX_MILLIS
+import androidx.work.WorkRequest.MIN_BACKOFF_MILLIS
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -8,70 +13,79 @@ import pl.llp.aircasting.exceptions.ErrorHandler
 import pl.llp.aircasting.models.Session
 import pl.llp.aircasting.networking.responses.SessionWithMeasurementsResponse
 import retrofit2.Call
+import java.util.concurrent.TimeUnit
 
-class PeriodicallyDownloadFixedSessionMeasurementsService(private val apiService: ApiService, private val errorHandler: ErrorHandler) {
-    private val sessionsRepository = SessionsRepository()
-    private val thread = DownloadThread()
+class PeriodicallyDownloadFixedSessionMeasurementsService(
+    mContext: Context,
+    apiService: ApiService,
+    errorHandler: ErrorHandler
+) {
+    companion object {
+        const val workerTag = "download_measurements_worker"
+        var call: Call<SessionWithMeasurementsResponse>? = null
+        val sessionsRepository = SessionsRepository()
+    }
+
     private val downloadMeasurementsService = DownloadMeasurementsService(apiService, errorHandler)
+    var mWorkManager = WorkManager.getInstance(mContext)
 
+    fun downloadMeasurements() {
+        val dbSessions = sessionsRepository.fixedSessions()
+        dbSessions.forEach { dbSession ->
+            val session = Session(dbSession)
+            downloadMeasurements(dbSession.id, session)
+        }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    fun downloadMeasurements(sessionId: Long, session: Session) {
+        GlobalScope.launch(Dispatchers.IO) {
+            call = downloadMeasurementsService.enqueueDownloadingMeasurementsForFixed(
+                sessionId,
+                session
+            )
+        }
+    }
+
+    /*
+    * Can be configured in repeatIntervals() / setInitialDelay() / and setBackoffCriteria() for settings attempts.
+    * Currently, it should be running after "5 * 60 * 1000L" -> (5 minutes) periodically.
+    * Initial delay is set to 1 minute.
+    * Attempt: Default is 10 attempts.
+    * This also checks if battery is not low.
+    */
     fun start() {
-        thread.start()
+        val constraints: Constraints = Constraints.Builder().apply {
+            setRequiredNetworkType(NetworkType.CONNECTED)
+            setRequiresBatteryNotLow(true)
+        }.build()
+
+        val request: PeriodicWorkRequest = PeriodicWorkRequest.Builder(
+            DownloadFixedSessionMeasurementsWorker::class.java,
+            MIN_PERIODIC_FLEX_MILLIS,
+            TimeUnit.MINUTES
+        )
+            .setConstraints(constraints)
+            .setInitialDelay(1, TimeUnit.MINUTES)
+            .setBackoffCriteria(BackoffPolicy.LINEAR, MIN_BACKOFF_MILLIS, TimeUnit.SECONDS)
+            .build()
+        mWorkManager.enqueueUniquePeriodicWork(
+            workerTag,
+            ExistingPeriodicWorkPolicy.KEEP,
+            request
+        )
     }
 
     fun stop() {
-        thread.cancel()
+        mWorkManager.cancelAllWorkByTag(workerTag)
     }
 
     fun pause() {
-        thread.paused = true
+        // TODO
     }
 
     fun resume() {
-        thread.paused = false
-    }
-
-    // TODO: consider using WorkManager
-    // https://developer.android.com/topic/libraries/architecture/workmanager/basics
-    private inner class DownloadThread() : Thread() {
-        private val POLL_INTERVAL = 60 * 1000L // 1 minute
-        var paused = false
-        private var call: Call<SessionWithMeasurementsResponse>? = null
-
-        override fun run() {
-            try {
-                while (!isInterrupted) {
-                    downloadMeasurements()
-                    sleep(POLL_INTERVAL)
-
-                    while (paused) {
-                        sleep(1000)
-                    }
-                }
-            } catch (e: InterruptedException) {
-                return
-            }
-        }
-
-        fun cancel() {
-            interrupt()
-            call?.cancel()
-        }
-
-        private fun downloadMeasurements() {
-            val dbSessions = sessionsRepository.fixedSessions()
-            dbSessions.forEach { dbSession ->
-                val session = Session(dbSession)
-                downloadMeasurements(dbSession.id, session)
-            }
-        }
-
-        private fun downloadMeasurements(sessionId: Long, session: Session) {
-            GlobalScope.launch(Dispatchers.IO) {
-                call = downloadMeasurementsService.enqueueDownloadingMeasurementsForFixed(
-                    sessionId,
-                    session
-                )
-            }
-        }
+        // TODO
     }
 }
+
