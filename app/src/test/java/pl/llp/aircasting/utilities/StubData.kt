32e3a3c109a0a6eb4d8b07efd57c224b37469682
kgetman <kgetmanskii@gmail.com>
2022-04-12 10:44:26 +0200

    Created StubData class to give stub data for tests; added method to give stub measurements from csv file obtained from real DB

---
+++ app/src/test/java/pl/llp/aircasting/utilities/StubData.kt
@@ -0,0 +1,40 @@
+package pl.llp.aircasting.utilities
+
+import com.opencsv.CSVParserBuilder
+import com.opencsv.CSVReaderBuilder
+import pl.llp.aircasting.models.Measurement
+import java.io.File
+import java.io.FileReader
+import java.util.*
+
+class StubData {
+    companion object {
+        fun measurementsFrom(file: File): MutableList<Measurement> {
+            val measurements = LinkedList<Measurement>()
+
+            val reader = CSVReaderBuilder(FileReader(file))
+                .withCSVParser(
+                    CSVParserBuilder()
+                        .withSeparator(';')
+                        .build()
+                )
+                .build()
+            reader.readNext()
+
+            var line = reader.readNext()
+            while (line != null) {
+                val value = line[2].toDouble()
+                val time = Date(line[3].toLong())
+                val latitude = line[4].toDouble()
+                val longitude = line[5].toDouble()
+                val averagingFrequency = line[6].toInt()
+
+                measurements.add(Measurement(value, time, latitude, longitude, averagingFrequency))
+
+                line = reader.readNext()
+            }
+
+            return measurements
+        }
+    }
+}
