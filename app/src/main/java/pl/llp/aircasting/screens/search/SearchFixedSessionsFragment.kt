71e1007f1905bce111781f006d33b7be8cf1c09e
kgetman <kgetmanskii@gmail.com>
2022-04-13 18:02:28 +0200

    Added base classes for search_fixed_sessions fragment

---
+++ app/src/main/java/pl/llp/aircasting/screens/search/SearchFixedSessionsFragment.kt
@@ -0,0 +1,7 @@
+package pl.llp.aircasting.screens.search
+
+import pl.llp.aircasting.screens.common.BaseFragment
+
+class SearchFixedSessionsFragment :
+    BaseFragment<SearchFixedSessionsViewMvcImpl, SearchFixedSessionsController>() {
+}
