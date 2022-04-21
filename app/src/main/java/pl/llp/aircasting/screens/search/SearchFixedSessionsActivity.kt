ba9bc54568ff82eef38a56b6b7b1f9538943e7c2
kgetman <kgetmanskii@gmail.com>
2022-04-21 10:06:25 +0200

    Added SearchFixedSessionsActivity

---
+++ app/src/main/java/pl/llp/aircasting/screens/search/SearchFixedSessionsActivity.kt
@@ -0,0 +1,12 @@
+package pl.llp.aircasting.screens.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pl.llp.aircasting.R
+
+class SearchFixedSessionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.search_fixed_sessions)
+    }
+}
