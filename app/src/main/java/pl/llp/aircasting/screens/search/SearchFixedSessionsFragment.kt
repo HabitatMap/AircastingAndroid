a768e336584333b874a88ed32b2287d9dcb32566
kgetman <kgetmanskii@gmail.com>
2022-04-21 11:54:02 +0200

    Deleted the base classes (for now)

--- app/src/main/java/pl/llp/aircasting/screens/search/SearchFixedSessionsFragment.kt
+++
@@ -1,7 +0,0 @@
-package pl.llp.aircasting.screens.search
-
-import pl.llp.aircasting.screens.common.BaseFragment
-
-class SearchFixedSessionsFragment :
-    BaseFragment<SearchFixedSessionsViewMvcImpl, SearchFixedSessionsController>() {
-}
