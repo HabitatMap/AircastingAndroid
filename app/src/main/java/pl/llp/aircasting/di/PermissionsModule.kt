6f8fbf430f3d15cd9b6979e19369b77b71f67eb7
kgetman <kgetmanskii@gmail.com>
2022-04-22 17:10:05 +0200

    Lifted providing bluetooth manager out of PermissionsModule

--- app/src/main/java/pl/llp/aircasting/di/PermissionsModule.kt
+++ app/src/main/java/pl/llp/aircasting/di/PermissionsModule.kt
@@ -1,11 +1,7 @@
 package pl.llp.aircasting.di
 
-import android.app.Activity
 import dagger.Module
 import dagger.Provides
-import pl.llp.aircasting.bluetooth.BluetoothManager
-import pl.llp.aircasting.bluetooth.BluetoothManagerDefault
-import pl.llp.aircasting.bluetooth.BluetoothRuntimePermissionManager
 import pl.llp.aircasting.permissions.PermissionsManager
 import javax.inject.Singleton
 
@@ -14,13 +10,4 @@ open class PermissionsModule {
     @Provides
     @Singleton
     open fun providesPermissionsManager(): PermissionsManager = PermissionsManager()
-
-    @Provides
-    @Singleton
-    open fun providesBluetoothManager(activity: Activity): BluetoothManager {
-        return if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S)
-            BluetoothRuntimePermissionManager(activity)
-        else
-            BluetoothManagerDefault()
-    }
 }
