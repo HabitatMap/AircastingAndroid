38744db5212c4ab6e6cec1bf3ab192ce6dc1405c
kgetman <kgetmanskii@gmail.com>
2022-04-25 15:11:37 +0200

    Cleaned imports

--- app/src/main/java/pl/llp/aircasting/bluetooth/BluetoothManager.kt
+++ app/src/main/java/pl/llp/aircasting/bluetooth/BluetoothManager.kt
@@ -1,12 +1,6 @@
 package pl.llp.aircasting.bluetooth
 
-import android.Manifest
 import android.app.Activity
-import android.bluetooth.BluetoothAdapter
-import android.content.Intent
-import android.content.pm.PackageManager
-import androidx.core.app.ActivityCompat
-import pl.llp.aircasting.lib.ResultCodes
 import pl.llp.aircasting.screens.new_session.select_device.DeviceItem
 
 interface BluetoothManager {
