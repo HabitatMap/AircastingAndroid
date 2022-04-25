daf376c937e0bb07bdd839218358cbea6add3812
mohsen <ash.wxrz@hotmail.com>
2022-04-25 15:56:47 +0430

    Handling backbutton click

--- app/src/main/java/pl/llp/aircasting/ui/view/screens/search/SearchFixedSessionsActivity.kt
+++ app/src/main/java/pl/llp/aircasting/ui/view/screens/search/SearchFixedSessionsActivity.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentActivity
+import kotlinx.android.synthetic.main.app_bar.*
 import pl.llp.aircasting.R
 
 class SearchFixedSessionsActivity : AppCompatActivity() {
+
     companion object {
         fun start(rootActivity: FragmentActivity?) {
             rootActivity ?: return
@@ -15,8 +17,21 @@ class SearchFixedSessionsActivity : AppCompatActivity() {
             rootActivity.startActivity(intent)
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_fixed_sessions)
+
+        setupUI()
+    }
+
+    private fun setupUI() {
+        setSupportActionBar(topAppBar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
