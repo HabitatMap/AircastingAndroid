5a761db236f395c62eb19af12aaf233385ab0d29
kgetman <kgetmanskii@gmail.com>
2022-04-27 10:33:49 +0200

    Replaced jupiter imports

--- app/src/test/java/pl/llp/aircasting/LastMeasurementSyncCalculatorTest.kt
+++ app/src/test/java/pl/llp/aircasting/LastMeasurementSyncCalculatorTest.kt
@@ -3,7 +3,7 @@ package pl.llp.aircasting
 import pl.llp.aircasting.util.DateConverter
 import pl.llp.aircasting.data.api.services.LastMeasurementSyncCalculator
 import org.junit.Assert
-import org.junit.jupiter.api.Test
+import org.junit.Test
 
 class LastMeasurementSyncCalculatorTest {
     val sessionEndTime = DateConverter.fromString("1987-07-23T11:00:00")!!
