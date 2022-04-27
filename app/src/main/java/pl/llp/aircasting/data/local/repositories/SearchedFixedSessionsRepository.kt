0bb20879545183dbe828b478bd4d4ecbc2d7ddd4
kgetman <kgetmanskii@gmail.com>
2022-04-27 10:34:36 +0200

    Added whenGivenCoordinatesPair_shouldReturnListOfSessions test method and got it compiled and passed

--- app/src/main/java/pl/llp/aircasting/data/local/repositories/SearchedFixedSessionsRepository.kt
+++ app/src/main/java/pl/llp/aircasting/data/local/repositories/SearchedFixedSessionsRepository.kt
@@ -1,4 +1,10 @@
 package pl.llp.aircasting.data.local.repositories
 
+import pl.llp.aircasting.data.model.Session
+
 class SearchedFixedSessionsRepository {
+
+    fun getSessionsInside(lat: Double, lng: Double): List<Session> {
+        return listOf()
+    }
 }
--- app/src/test/java/pl/llp/aircasting/data/local/repositories/SearchedFixedSessionsRepositoryTest.kt
+++ app/src/test/java/pl/llp/aircasting/data/local/repositories/SearchedFixedSessionsRepositoryTest.kt
@@ -1,11 +1,19 @@
 package pl.llp.aircasting.data.local.repositories
 
 import org.junit.Test
+import pl.llp.aircasting.data.model.Session
+import kotlin.test.assertTrue
 
 
 class SearchedFixedSessionsRepositoryTest {
     @Test
-    fun whenGivenCoordinatesPair_shouldReturnListOfSessions() {
+    fun whenGivenCoordinatesPair_whereNoSessionsArePresent_shouldReturnEmptyList() {
+        val repository = SearchedFixedSessionsRepository()
+        val lat = 0.0
+        val lng = 0.0
 
+        val result: List<Session> = repository.getSessionsInside(lat, lng)
+
+        assertTrue(result.isEmpty())
     }
 }
