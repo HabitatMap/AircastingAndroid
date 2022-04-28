04aed2d1e1897d6f6f00c8703cd6762d4d047e30
kgetman <kgetmanskii@gmail.com>
2022-04-28 15:20:01 +0200

    Imports for testing

--- app/src/main/java/pl/llp/aircasting/data/api/services/SessionsInRegionDownloadService.kt
+++ app/src/main/java/pl/llp/aircasting/data/api/services/SessionsInRegionDownloadService.kt
@@ -1,6 +1,10 @@
 package pl.llp.aircasting.data.api.services
 
+import pl.llp.aircasting.data.api.responses.SessionsInRegionResponse
 import pl.llp.aircasting.data.model.Session
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SessionsInRegionDownloadService {
     val sessions: MutableList<Session> = mutableListOf()
