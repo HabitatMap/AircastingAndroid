8feeb1f5968b15b63b5e6657404764847bc7f0d6
mohsen <ash.wxrz@hotmail.com>
2022-04-30 13:34:15 +0430

    Implementing the onMapReady to show the map

--- app/src/main/java/pl/llp/aircasting/ui/view/screens/search/SearchFixedResultActivity.kt
+++ app/src/main/java/pl/llp/aircasting/ui/view/screens/search/SearchFixedResultActivity.kt
@@ -2,9 +2,15 @@ package pl.llp.aircasting.ui.view.screens.search
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import pl.llp.aircasting.R
 
-class SearchFixedResultActivity : AppCompatActivity() {
+class SearchFixedResultActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +20,27 @@ class SearchFixedResultActivity : AppCompatActivity() {
     }
 
     private fun setupUI() {
+        // retrieving data from the previous activity
+        val lat = intent.getStringExtra("lat")
+        val long = intent.getStringExtra("long")
 
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.mapView) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val sydney = LatLng(-33.852, 151.211)
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney")
+        )
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
