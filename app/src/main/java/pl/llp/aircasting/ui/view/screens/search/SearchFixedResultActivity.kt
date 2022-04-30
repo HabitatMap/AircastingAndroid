9ab07d3c77bc4f8f1cecb4ca3d09b56d6c798ef6
mohsen <ash.wxrz@hotmail.com>
2022-04-30 13:09:00 +0430

    Added searchResult activity

---
+++ app/src/main/java/pl/llp/aircasting/ui/view/screens/search/SearchFixedResultActivity.kt
@@ -0,0 +1,19 @@
+package pl.llp.aircasting.ui.view.screens.search
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import pl.llp.aircasting.R
+
+class SearchFixedResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_follow_result)
+
+        setupUI()
+    }
+
+    private fun setupUI() {
+
+    }
+}
