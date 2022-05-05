4191752cf818a41afd7df97bcb066db43cbc88d6
mohsen <ash.wxrz@hotmail.com>
2022-05-05 19:35:44 +0430

    Implementation for the ViewModel using livedata and Coroutines

--- app/src/main/java/pl/llp/aircasting/ui/viewmodel/SearchFollowViewModel.kt
+++ app/src/main/java/pl/llp/aircasting/ui/viewmodel/SearchFollowViewModel.kt
@@ -1,8 +1,24 @@
 package pl.llp.aircasting.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import kotlinx.coroutines.Dispatchers
+import pl.llp.aircasting.data.api.repositories.SearchFollowRepository
+import pl.llp.aircasting.util.Resource
+import javax.inject.Inject
 
-class SearchFollowViewModel : ViewModel() {
+class SearchFollowViewModel @Inject constructor(
+    private val searchFollowRepo: SearchFollowRepository
+) : ViewModel() {
+    fun getSessionsInRegion(query: String) = liveData(Dispatchers.IO) {
+        emit(Resource.loading(data = null))
 
+        try {
+                val getSessions = searchFollowRepo.getSessionsInRegion(query)
+                emit(Resource.success(data = getSessions))
 
+        } catch (e: Exception) {
+            emit(Resource.error(data = null, message = e.message ?: "Error Occurred!"))
+        }
+    }
 }
