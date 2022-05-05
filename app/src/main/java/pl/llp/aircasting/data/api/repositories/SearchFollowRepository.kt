1a2c5e6987fd0a59932d2e3ceff866d8191bb5f1
mohsen <ash.wxrz@hotmail.com>
2022-05-05 19:35:17 +0430

    Added only one query to the repository

--- app/src/main/java/pl/llp/aircasting/data/api/repositories/SearchFollowRepository.kt
+++ app/src/main/java/pl/llp/aircasting/data/api/repositories/SearchFollowRepository.kt
@@ -4,29 +4,5 @@ import pl.llp.aircasting.data.api.services.ApiService
 import javax.inject.Inject
 
 class SearchFollowRepository @Inject constructor(private val apiService: ApiService) {
-    suspend fun getSessionsInRegion(
-        north: Double,
-        south: Double,
-        east: Double,
-        west: Double,
-        time_from: Int,
-        time_to: Int,
-        measurement_type: String,
-        sensor_name: String,
-        unit_symbol: String,
-        tags: String,
-        usernames: String
-    ) = apiService.getSessionsInRegion(
-        north,
-        south,
-        east,
-        west,
-        time_from,
-        time_to,
-        measurement_type,
-        sensor_name,
-        unit_symbol,
-        tags,
-        usernames
-    )
+    suspend fun getSessionsInRegion(query: String) = apiService.getSessionsInRegion(query)
 }
