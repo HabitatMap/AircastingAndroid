68b2a4a2992158bddbf4aed5a081f5f07184f47a
kgetman <kgetmanskii@gmail.com>
2022-05-11 17:20:01 +0200

    Added View Model and Binding to the SearchFixedBottomSheet class; setup binding

--- app/src/main/java/pl/llp/aircasting/ui/view/screens/search/SearchFixedBottomSheet.kt
+++ app/src/main/java/pl/llp/aircasting/ui/view/screens/search/SearchFixedBottomSheet.kt
@@ -1,11 +1,23 @@
 package pl.llp.aircasting.ui.view.screens.search
 
+import androidx.fragment.app.activityViewModels
 import pl.llp.aircasting.R
+import pl.llp.aircasting.databinding.SearchFollowBottomSheetBinding
 import pl.llp.aircasting.ui.view.screens.common.BottomSheet
+import pl.llp.aircasting.ui.viewmodel.SearchFollowViewModel
 
 class SearchFixedBottomSheet : BottomSheet() {
+    private val viewModel: SearchFollowViewModel by activityViewModels()
+    private var binding: SearchFollowBottomSheetBinding? = null
     // TODO: Get data from ViewModel about the selected session
     override fun layoutId(): Int {
         return R.layout.search_follow_bottom_sheet
+    }
+
+    override fun setup() {
+        super.setup()
+
+        binding = contentView?.let { SearchFollowBottomSheetBinding.bind(it) }
+        binding?.viewModel = viewModel
     }
 }
