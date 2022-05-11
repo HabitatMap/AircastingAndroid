1ecffa479fa9301dcb1c646a4e88b554f8870eba
kgetman <kgetmanskii@gmail.com>
2022-05-11 12:43:22 +0200

    Created test class for viewmodel;

--- app/src/main/java/pl/llp/aircasting/ui/view/screens/search/SearchFixedBottomSheet.kt
+++ app/src/main/java/pl/llp/aircasting/ui/view/screens/search/SearchFixedBottomSheet.kt
@@ -4,6 +4,7 @@ import pl.llp.aircasting.R
 import pl.llp.aircasting.ui.view.screens.common.BottomSheet
 
 class SearchFixedBottomSheet : BottomSheet() {
+    // TODO: Get data from ViewModel about the selected session
     override fun layoutId(): Int {
         return R.layout.search_follow_bottom_sheet
     }
