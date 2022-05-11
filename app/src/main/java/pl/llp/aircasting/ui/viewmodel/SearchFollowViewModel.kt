d6e64b28c3489c0e9252b3aff44f18d565f0c64c
kgetman <kgetmanskii@gmail.com>
2022-05-11 17:21:33 +0200

    Added Live Data for selected session to View Model; Added method to change it. This is to be provided to the bottom sheet

--- app/src/main/java/pl/llp/aircasting/ui/viewmodel/SearchFollowViewModel.kt
+++ app/src/main/java/pl/llp/aircasting/ui/viewmodel/SearchFollowViewModel.kt
@@ -1,5 +1,7 @@
 package pl.llp.aircasting.ui.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import kotlinx.coroutines.Dispatchers
@@ -9,12 +11,20 @@ import pl.llp.aircasting.data.api.util.SensorInformation
 import pl.llp.aircasting.data.model.GeoSquare
 import pl.llp.aircasting.util.Resource
 import pl.llp.aircasting.util.isConnected
+import pl.llp.aircasting.data.api.responses.search.Session
 import javax.inject.Inject
 
 class SearchFollowViewModel @Inject constructor(
     private val activeFixedRepo: ActiveFixedSessionsInRegionRepository,
     private val mApp: AircastingApplication
 ) : ViewModel() {
+    private val mutableSelectedSession = MutableLiveData<Session>()
+    val selectedSession: LiveData<Session> get() = mutableSelectedSession
+
+    fun selectSession(session: Session) {
+        mutableSelectedSession.value = session
+    }
+
     fun getSessionsInRegion(square: GeoSquare, sensorInfo: SensorInformation) =
         liveData(Dispatchers.IO) {
             emit(Resource.loading(null))
