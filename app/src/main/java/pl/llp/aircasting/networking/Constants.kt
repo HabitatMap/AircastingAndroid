515847e8d5a609bc5705deb4aede927de0440f7f
kgetman <kgetmanskii@gmail.com>
2022-05-13 17:41:43 +0200

    Added constant for number of measurements in hour

--- app/src/main/java/pl/llp/aircasting/networking/Constants.kt
+++ app/src/main/java/pl/llp/aircasting/networking/Constants.kt
@@ -20,4 +20,7 @@ object Constants {
     const val MILLIS_IN_HOUR = 60 * 60 * 1000
     const val MILLIS_IN_MINUTE = 60 * 1000
     const val MILLIS_IN_SECOND = 1000
+
+    /* Miscellaneous */
+    const val MEASUREMENTS_IN_HOUR = 60
 }
