caa7c83a5d245c0818a58bc8ee2842b74302f5a3
mohsen <ash.wxrz@hotmail.com>
2022-05-17 18:02:46 +0430

    Merging with the bottomsheet branch

--- app/src/main/java/pl/llp/aircasting/ui/view/screens/search/SearchFixedBottomSheet.kt
+++ app/src/main/java/pl/llp/aircasting/ui/view/screens/search/SearchFixedBottomSheet.kt
@@ -5,19 +5,50 @@ import pl.llp.aircasting.R
 import pl.llp.aircasting.databinding.SearchFollowBottomSheetBinding
 import pl.llp.aircasting.ui.view.screens.common.BottomSheet
 import pl.llp.aircasting.ui.viewmodel.SearchFollowViewModel
+import pl.llp.aircasting.util.DateConverter
 
 class SearchFixedBottomSheet : BottomSheet() {
     private val viewModel: SearchFollowViewModel by activityViewModels()
     private var binding: SearchFollowBottomSheetBinding? = null
-    // TODO: Get data from ViewModel about the selected session
+
+    companion object {
+        // I am not sure about this approach, maybe all these things should be in a different place
+        private val dateConverter = DateConverter.get()
+        private fun formatTime(time: String = ""): String? = DateConverter.fromString(time)
+            ?.let { dateConverter?.toTimeStringForDisplay(it) }
+
+        private fun formatDate(date: String = ""): String? = DateConverter.fromString(date)
+            ?.let { DateConverter.toDateStringForDisplay(it) }
+
+        private fun formatType(type: String = ""): String {
+            val splitByCapitalLetter = type.split(Regex("(?=[A-Z])"))
+            return splitByCapitalLetter[1]
+        }
+        private fun formatSensorName(sensor: String = ""): String {
+            val splitByHyphen = sensor.split("-")
+            return splitByHyphen[0]
+        }
+    }
+
     override fun layoutId(): Int {
         return R.layout.search_follow_bottom_sheet
     }
 
     override fun setup() {
         super.setup()
-
         binding = contentView?.let { SearchFollowBottomSheetBinding.bind(it) }
-        binding?.viewModel = viewModel
+        setObserver()
+    }
+
+    private fun setObserver() {
+        viewModel.selectedSession.observe(this) {
+            binding?.title = it.title
+            binding?.startDate = formatDate(it.startTimeLocal)
+            binding?.startTime = formatTime(it.startTimeLocal)
+            binding?.endDate = formatDate(it.endTimeLocal)
+            binding?.endTime = formatTime(it.endTimeLocal)
+            binding?.type = formatType(it.type)
+            binding?.sensorName = formatSensorName(it.streams.airBeam2PM25.sensorName)
+        }
     }
 }
