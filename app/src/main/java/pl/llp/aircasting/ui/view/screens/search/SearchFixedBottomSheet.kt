b2b392e64f31486007ba08d48ec989a97833db8b
mohsen <ash.wxrz@hotmail.com>
2022-05-19 18:10:20 +0430

    Added map fragment inside the bottom sheet

--- app/src/main/java/pl/llp/aircasting/ui/view/screens/search/SearchFixedBottomSheet.kt
+++ app/src/main/java/pl/llp/aircasting/ui/view/screens/search/SearchFixedBottomSheet.kt
@@ -2,6 +2,12 @@ package pl.llp.aircasting.ui.view.screens.search
 
 import android.util.Log
 import androidx.fragment.app.activityViewModels
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import pl.llp.aircasting.R
 import pl.llp.aircasting.databinding.SearchFollowBottomSheetBinding
 import pl.llp.aircasting.ui.view.screens.common.BottomSheet
@@ -10,10 +16,15 @@ import pl.llp.aircasting.util.SearchHelper.Companion.formatDate
 import pl.llp.aircasting.util.SearchHelper.Companion.formatSensorName
 import pl.llp.aircasting.util.SearchHelper.Companion.formatTime
 import pl.llp.aircasting.util.SearchHelper.Companion.formatType
+import pl.llp.aircasting.util.styleGoogleMap
 
-class SearchFixedBottomSheet : BottomSheet() {
+class SearchFixedBottomSheet : BottomSheet(), OnMapReadyCallback {
     private val viewModel: SearchFollowViewModel by activityViewModels()
     private var binding: SearchFollowBottomSheetBinding? = null
+
+    private lateinit var mMap: GoogleMap
+    private var txtLat: Double? = null
+    private var txtLong: Double? = null
 
     override fun layoutId(): Int {
         return R.layout.search_follow_bottom_sheet
@@ -22,7 +33,18 @@ class SearchFixedBottomSheet : BottomSheet() {
     override fun setup() {
         super.setup()
         binding = contentView?.let { SearchFollowBottomSheetBinding.bind(it) }
+        setupUI()
         setObserver()
+    }
+
+    private fun setupUI(){
+        val mapFragment =
+            requireActivity().supportFragmentManager.findFragmentById(R.id.mapViewBottomSheet) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+
+        // Dummy data for showing on the map
+        txtLat = 44.82112
+        txtLong = 20.459113
     }
 
     private fun setObserver() {
@@ -41,6 +63,20 @@ class SearchFixedBottomSheet : BottomSheet() {
                 Log.i("Color", tintColor.toString())
                 Log.i("Resource", R.color.aircasting_pink.toString())
             }
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        styleGoogleMap(mMap, requireActivity())
+
+        if (txtLat != null && txtLong != null) {
+            val myLocation = LatLng(txtLat!!, txtLong!!)
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(myLocation)
+            )
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 12f), 1000, null)
         }
     }
 }
