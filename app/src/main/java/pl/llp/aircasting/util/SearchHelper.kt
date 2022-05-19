9bbed85f5426ecff6d50fe842ecedfca017a413a
mohsen <ash.wxrz@hotmail.com>
2022-05-19 15:22:18 +0430

    Removing unnecessary double quats

--- app/src/main/java/pl/llp/aircasting/util/SearchHelper.kt
+++ app/src/main/java/pl/llp/aircasting/util/SearchHelper.kt
@@ -4,18 +4,18 @@ class SearchHelper {
     companion object {
         private val dateConverter = DateConverter.get()
 
-        fun formatTime(time: String = ""): String? = DateConverter.fromString(time)
+        fun formatTime(time: String): String? = DateConverter.fromString(time)
             ?.let { dateConverter?.toTimeStringForDisplay(it) }
 
-        fun formatDate(date: String = ""): String? = DateConverter.fromString(date)
+        fun formatDate(date: String): String? = DateConverter.fromString(date)
             ?.let { DateConverter.toDateStringForDisplay(it) }
 
-        fun formatType(type: String = ""): String {
+        fun formatType(type: String): String {
             val splitByCapitalLetter = type.split(Regex("(?=[A-Z])"))
             return splitByCapitalLetter[1]
         }
 
-        fun formatSensorName(sensor: String = ""): String {
+        fun formatSensorName(sensor: String): String {
             val splitByHyphen = sensor.split("-")
             return splitByHyphen[0]
         }
