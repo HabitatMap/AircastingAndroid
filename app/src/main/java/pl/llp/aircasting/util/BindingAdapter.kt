e97a6c8b280a97b6f3710ab01ad57ee11dcfe837
mohsen <ash.wxrz@hotmail.com>
2022-05-19 16:14:46 +0430

    Added binding adapter to use it for the dates on sessions cards

--- app/src/main/java/pl/llp/aircasting/util/BindingAdapter.kt
+++ app/src/main/java/pl/llp/aircasting/util/BindingAdapter.kt
@@ -1,10 +1,14 @@
 package pl.llp.aircasting.util
 
+import android.annotation.SuppressLint
 import android.content.res.ColorStateList
 import android.util.Log
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.annotation.ColorInt
 import androidx.databinding.BindingAdapter
+import pl.llp.aircasting.util.SearchHelper.Companion.formatDate
+import pl.llp.aircasting.util.SearchHelper.Companion.formatTime
 
 object BindingAdapter {
     @JvmStatic
@@ -13,5 +17,21 @@ object BindingAdapter {
         setColorFilter(color)
         imageTintList = ColorStateList.valueOf(color)
         Log.i("ImageView", this.toString())
+    }
+
+    @SuppressLint("SetTextI18n")
+    @JvmStatic
+    @BindingAdapter("formatDateStart", "formatDateEnd", requireAll = true)
+    fun TextView.setFormatDate(startTimeLocal: String, endTimeLocal: String) {
+        try {
+            val startDate = formatDate(startTimeLocal)
+            val startTime = formatTime(startTimeLocal)
+            val endDate = formatDate(endTimeLocal)
+            val endTime = formatTime(endTimeLocal)
+
+            this.text = "$startDate $startTime - $endDate $endTime"
+        } catch (e: Exception) {
+            Log.d("TAG", e.message.toString())
+        }
     }
 }
