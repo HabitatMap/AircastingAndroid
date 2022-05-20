fd192dcff1f3bf882b3e55a7336cdd97beb7f2ca
mohsen <ash.wxrz@hotmail.com>
2022-05-20 11:57:00 +0430

    Removing searchIcon visibility check inside the DashboardFragment.kt

--- app/src/main/java/pl/llp/aircasting/ui/view/screens/dashboard/DashboardFragment.kt
+++ app/src/main/java/pl/llp/aircasting/ui/view/screens/dashboard/DashboardFragment.kt
@@ -4,10 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.fragment_dashboard.*
-import kotlinx.android.synthetic.main.prominent_app_bar.*
 import pl.llp.aircasting.AircastingApplication
 import pl.llp.aircasting.ui.view.screens.common.BaseFragment
 import pl.llp.aircasting.util.Settings
@@ -18,7 +16,6 @@ class DashboardFragment : BaseFragment<DashboardViewMvcImpl, DashboardController
 
     @Inject
     lateinit var settings: Settings
-    private var mSearchIcon: ImageView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,8 +35,6 @@ class DashboardFragment : BaseFragment<DashboardViewMvcImpl, DashboardController
         controller = DashboardController(view)
         val tabId = arguments?.get("tabId") as Int?
         controller?.onCreate(tabId)
-
-        mSearchIcon = activity?.search_follow_icon
 
         return view?.rootView
     }
@@ -68,13 +63,12 @@ class DashboardFragment : BaseFragment<DashboardViewMvcImpl, DashboardController
 
     private fun isAdjustable(position: Int) {
         activity?.let {
-            if (position == 0) {
-                adjustMenuVisibility(it, true, settings.getFollowedSessionsNumber())
-                mSearchIcon?.visibility = View.VISIBLE
-            } else {
-                adjustMenuVisibility(it, false)
-                mSearchIcon?.visibility = View.INVISIBLE
-            }
+            if (position == 0)
+                adjustMenuVisibility(
+                    it,
+                    true,
+                    settings.getFollowedSessionsNumber()
+                ) else adjustMenuVisibility(it, false)
         }
     }
 
