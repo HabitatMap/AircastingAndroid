6fb2421cf94f4fa10b6cb15a0b07154afe36b413
mohsen <ash.wxrz@hotmail.com>
2022-05-26 21:44:33 +0430

    The logic for checking if it is the exact session we're checking by `id` | this way it is more precise

--- app/src/main/java/pl/llp/aircasting/ui/view/adapters/FixedFollowAdapter.kt
+++ app/src/main/java/pl/llp/aircasting/ui/view/adapters/FixedFollowAdapter.kt
@@ -1,29 +1,43 @@
 package pl.llp.aircasting.ui.view.adapters
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import pl.llp.aircasting.R
 import pl.llp.aircasting.data.api.response.search.Session
 import pl.llp.aircasting.databinding.ItemSesssionsListFixedFollowBinding
 
 class FixedFollowAdapter constructor(private val onItemClicked: (Session) -> Unit) :
     RecyclerView.Adapter<FixedFollowAdapter.DataViewHolder>() {
     private val sessions: ArrayList<Session> = ArrayList()
+    private var selectedSession: Session? = null
+    private var sessionView: View? = null
 
     inner class DataViewHolder(
         private val binding: ItemSesssionsListFixedFollowBinding,
         private val onItemClicked: (Int) -> Unit
     ) : RecyclerView.ViewHolder(binding.root) {
-
         fun bind(sessions: Session) {
             binding.mySessions = sessions
             binding.executePendingBindings()
 
             binding.root.setOnClickListener { onItemClicked(bindingAdapterPosition) }
+
+            sessionView = binding.root
+            binding.root.apply {
+                if (selectedSession?.id == sessions.id) setBackgroundResource(R.drawable.card_view_border_search) else setBackgroundColor(
+                    ContextCompat.getColor(context, R.color.aircasting_white)
+                )
+            }
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataViewHolder {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): FixedFollowAdapter.DataViewHolder {
         val binding = ItemSesssionsListFixedFollowBinding.inflate(
             LayoutInflater.from(parent.context),
             parent,
@@ -46,5 +60,22 @@ class FixedFollowAdapter constructor(private val onItemClicked: (Session) -> Uni
     fun getSessionPositionBasedOnId(uid: String): Int {
         val session = sessions.first { it.uuid == uid }
         return sessions.indexOf(session)
+    }
+
+    fun addCardBorder(position: Int) {
+        selectedSession = sessions[position]
+        removeItemBorder()
+        notifyItemChanged(position)
+    }
+
+    fun removeItemBorder() {
+        sessionView?.apply {
+            setBackgroundColor(
+                ContextCompat.getColor(
+                    context,
+                    R.color.aircasting_white
+                )
+            )
+        }
     }
 }
