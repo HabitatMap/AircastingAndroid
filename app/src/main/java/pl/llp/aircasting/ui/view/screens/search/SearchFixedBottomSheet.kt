6cf16d738e599cca4b81cc87b617ff059ee78642
mohsen <ash.wxrz@hotmail.com>
2022-05-26 12:56:19 +0430

    Fix for the crash, it was happening because we needed to remove the mapFragment before we recreate it again

--- app/src/main/java/pl/llp/aircasting/ui/view/screens/search/SearchFixedBottomSheet.kt
+++ app/src/main/java/pl/llp/aircasting/ui/view/screens/search/SearchFixedBottomSheet.kt
@@ -1,6 +1,5 @@
 package pl.llp.aircasting.ui.view.screens.search
 
-import android.util.Log
 import androidx.fragment.app.activityViewModels
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -8,26 +7,22 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import pl.llp.aircasting.R
 import pl.llp.aircasting.databinding.SearchFollowBottomSheetBinding
 import pl.llp.aircasting.ui.view.common.BottomSheet
 import pl.llp.aircasting.ui.viewmodel.SearchFollowViewModel
-import pl.llp.aircasting.util.SearchHelper.Companion.formatDate
-import pl.llp.aircasting.util.SearchHelper.Companion.formatSensorName
-import pl.llp.aircasting.util.SearchHelper.Companion.formatTime
-import pl.llp.aircasting.util.SearchHelper.Companion.formatType
 import pl.llp.aircasting.util.styleGoogleMap
 
 class SearchFixedBottomSheet : BottomSheet(), OnMapReadyCallback {
     private val searchFollowViewModel: SearchFollowViewModel by activityViewModels()
     private var binding: SearchFollowBottomSheetBinding? = null
 
+    private var mapFragment: SupportMapFragment? = null
     private lateinit var mMap: GoogleMap
     private var txtLat: Double? = null
     private var txtLng: Double? = null
 
     override fun layoutId(): Int {
-        return R.layout.search_follow_bottom_sheet
+        return pl.llp.aircasting.R.layout.search_follow_bottom_sheet
     }
 
     override fun setup() {
@@ -35,13 +30,12 @@ class SearchFixedBottomSheet : BottomSheet(), OnMapReadyCallback {
         binding = contentView?.let { SearchFollowBottomSheetBinding.bind(it) }
 
         setupUI()
-        setObserver()
         getLatlngObserver()
     }
 
     private fun setupUI() {
-        val mapFragment =
-            requireActivity().supportFragmentManager.findFragmentById(R.id.mapViewBottomSheet) as? SupportMapFragment
+        mapFragment =
+            requireActivity().supportFragmentManager.findFragmentById(pl.llp.aircasting.R.id.mapViewBottomSheet) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
 
         binding?.followBtn?.setOnClickListener { onFollowClicked() }
@@ -66,25 +60,6 @@ class SearchFixedBottomSheet : BottomSheet(), OnMapReadyCallback {
         }
     }
 
-    private fun setObserver() {
-        searchFollowViewModel.selectedSession.observe(this) {
-            binding?.apply {
-                title = it.title
-                startDate = formatDate(it.startTimeLocal)
-                startTime = formatTime(it.startTimeLocal)
-                endDate = formatDate(it.endTimeLocal)
-                endTime = formatTime(it.endTimeLocal)
-                type = formatType(it.type)
-                sensorName = formatSensorName(it.streams.sensor.sensorName)
-                header = it.streams.sensor.measurementShortType
-                // Testing
-                tintColor = R.color.aircasting_pink
-                Log.i("Color", tintColor.toString())
-                Log.i("Resource", R.color.aircasting_pink.toString())
-            }
-        }
-    }
-
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         styleGoogleMap(mMap, requireActivity())
@@ -97,5 +72,12 @@ class SearchFixedBottomSheet : BottomSheet(), OnMapReadyCallback {
             )
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 15f))
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        if (mapFragment != null) parentFragmentManager.beginTransaction().remove(mapFragment!!)
+            .commit()
     }
 }
