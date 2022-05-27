614e043ecb2d309ac0d208d20a87b4237981740e
mohsen <ash.wxrz@hotmail.com>
2022-05-27 19:51:54 +0430

    External Session DAO - Room DB

--- app/src/main/java/pl/llp/aircasting/data/local/dao/ExtSessionDao.kt
+++ app/src/main/java/pl/llp/aircasting/data/local/dao/ExtSessionDao.kt
@@ -1,16 +1,18 @@
 package pl.llp.aircasting.data.local.dao
 
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
+import androidx.lifecycle.LiveData
+import androidx.room.*
 import pl.llp.aircasting.data.local.entity.ExtSessionsDBObject
 
 @Dao
 interface ExtSessionDao {
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(extSession: ExtSessionsDBObject)
+    suspend fun insert(extSession: ExtSessionsDBObject)
 
     @Delete
-    fun delete(extSession: ExtSessionsDBObject)
+    suspend fun delete(extSession: ExtSessionsDBObject)
+
+    @Query("Select * from ext_sessions order by id ASC")
+    fun getAllFollowedSessions(): LiveData<List<ExtSessionsDBObject>>
 }
