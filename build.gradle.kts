13265b43319a5a3ca307f654b880752ef8e16e3f
mohsen <ash.wxrz@hotmail.com>
2022-05-27 13:31:32 +0430

    Removing the comments and updated the jCenter to mavenCentral()

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,25 @@
 buildscript {
-    val kotlin_version = "1.6.21"
+    val kotlinVersion = "1.6.21"
+    val navVersion = "2.4.2"
+
     repositories {
         google()
-        jcenter()
+        mavenCentral()
     }
+
     dependencies {
         classpath("com.android.tools.build:gradle:7.2.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("com.google.gms:google-services:4.3.10")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.8.1")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.2")
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
     }
 }
 
 allprojects {
     repositories {
         google()
-        jcenter()
+        mavenCentral()
         maven { url = uri("https://jitpack.io") }
     }
 }
