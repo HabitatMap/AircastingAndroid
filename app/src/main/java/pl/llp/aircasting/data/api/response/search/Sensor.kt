58f9ff3162523c1e21c842c54662560bb01a3107
kgetman <kgetmanskii@gmail.com>
2022-05-27 14:56:01 +0200

    Removed unused fields

--- app/src/main/java/pl/llp/aircasting/data/api/response/search/Sensor.kt
+++ app/src/main/java/pl/llp/aircasting/data/api/response/search/Sensor.kt
@@ -6,24 +6,12 @@ import com.google.gson.annotations.SerializedName
 
 @Keep
 data class Sensor(
-    @SerializedName("average_value")
-    val averageValue: Double,
     @SerializedName("id")
     val id: Int,
-    @SerializedName("max_latitude")
-    val maxLatitude: Double,
-    @SerializedName("max_longitude")
-    val maxLongitude: Double,
     @SerializedName("measurement_short_type")
     val measurementShortType: String,
     @SerializedName("measurement_type")
     val measurementType: String,
-    @SerializedName("measurements_count")
-    val measurementsCount: Int,
-    @SerializedName("min_latitude")
-    val minLatitude: Double,
-    @SerializedName("min_longitude")
-    val minLongitude: Double,
     @SerializedName("sensor_name")
     val sensorName: String,
     @SerializedName("sensor_package_name")
