9f81e4d95e708c23135dd912d8463218307e7e19
mohsen <ash.wxrz@hotmail.com>
2022-05-27 20:03:45 +0430

    Changing the Repository | removing liveData from it

--- app/src/main/java/pl/llp/aircasting/data/local/repository/ExtSessionsLocalRepository.kt
+++ app/src/main/java/pl/llp/aircasting/data/local/repository/ExtSessionsLocalRepository.kt
@@ -1,14 +1,10 @@
 package pl.llp.aircasting.data.local.repository
 
-import androidx.lifecycle.LiveData
 import pl.llp.aircasting.data.local.DatabaseProvider
 import pl.llp.aircasting.data.local.entity.ExtSessionsDBObject
 
 class ExtSessionsLocalRepository {
     private val mDatabase = DatabaseProvider.get().extSession()
-
-    val getAllFollowedExtSessions: LiveData<List<ExtSessionsDBObject>> =
-        mDatabase.getAllFollowedSessions()
 
     suspend fun insert(extSession: ExtSessionsDBObject) {
         mDatabase.insert(extSession)
@@ -16,5 +12,9 @@ class ExtSessionsLocalRepository {
 
     suspend fun deleteFollowedSession(extSession: ExtSessionsDBObject) {
         mDatabase.delete(extSession)
+    }
+
+    suspend fun getAllFollowedExtSessions() {
+        mDatabase.getAllFollowedSessions()
     }
 }
