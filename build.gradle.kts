115942bd5dafdc756aa469b5f9da9479ddab3541
mohsen <ash.wxrz@hotmail.com>
2022-05-27 12:04:44 +0430

    Converted project level build.gradle to DSL

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+buildscript {
+    val kotlin_version = "1.6.21"
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.2.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath("com.google.gms:google-services:4.3.10")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.8.1")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.2")
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+tasks.register(name = "type", type = Delete::class) {
+    delete(rootProject.buildDir)
+}
