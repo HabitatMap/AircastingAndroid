db6991637a70b43efffed20f1be0d865f8de122c
mohsen <ash.wxrz@hotmail.com>
2022-05-27 13:43:08 +0430

    Converted the build.gradle project level to dsl

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+buildscript {
+    val kotlinVersion = "1.6.21"
+    val navVersion = "2.4.2"
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.2.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.google.gms:google-services:4.3.10")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.8.1")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+tasks.register(name = "type", type = Delete::class) {
+    delete(rootProject.buildDir)
+}
