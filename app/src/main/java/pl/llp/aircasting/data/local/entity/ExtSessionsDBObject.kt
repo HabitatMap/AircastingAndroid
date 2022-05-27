4619bc1374020bae04f020009d23ca4473985d86
kgetman <kgetmanskii@gmail.com>
2022-05-27 15:14:47 +0200

    Added foreign key and reference to Measurement Stream from ExtSessions

--- app/src/main/java/pl/llp/aircasting/data/local/entity/ExtSessionsDBObject.kt
+++ app/src/main/java/pl/llp/aircasting/data/local/entity/ExtSessionsDBObject.kt
@@ -2,21 +2,31 @@ package pl.llp.aircasting.data.local.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "ext_sessions")
+@Entity(
+    tableName = "ext_sessions",
+    foreignKeys = [
+        ForeignKey(
+            entity = MeasurementStreamDBObject::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("stream_id"),
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class ExtSessionsDBObject(
     @PrimaryKey val id: Int,
     @ColumnInfo(name = "uuid") val uuid: String,
     @ColumnInfo(name = "title") val title: String,
     @ColumnInfo(name = "type") val type: String,
     @ColumnInfo(name = "username") val username: String,
-    @ColumnInfo(name = "endTimeLocal") val endTimeLocal: String,
-    @ColumnInfo(name = "startTimeLocal") val startTimeLocal: String,
-    @ColumnInfo(name = "lastHourAverage") val lastHourAverage: Double,
+    @ColumnInfo(name = "end_time_local") val endTimeLocal: String,
+    @ColumnInfo(name = "start_time_local") val startTimeLocal: String,
+    @ColumnInfo(name = "last_hour_average") val lastHourAverage: Double,
     @ColumnInfo(name = "latitude") val latitude: Double,
     @ColumnInfo(name = "longitude") val longitude: String,
-    @ColumnInfo(name = "isIndoor") val isIndoor: Boolean,
-    //@ColumnInfo(name = "streams") val streams: Streams,
+    @ColumnInfo(name = "is_indoor") val isIndoor: Boolean,
+    @ColumnInfo(name = "stream_id") val streamId: Int
 )
-// TODO: Need to do sth with the streams since we cannot save it like that on DB
