ddb48b7274934ac38412d006648bdff71c49c1ff
kgetman <kgetmanskii@gmail.com>
2022-05-27 14:07:23 +0200

    Added migration to DatabaseProvider

--- app/src/main/java/pl/llp/aircasting/data/local/DatabaseProvider.kt
+++ app/src/main/java/pl/llp/aircasting/data/local/DatabaseProvider.kt
@@ -72,7 +72,8 @@ class DatabaseProvider {
                         MIGRATION_25_26,
                         MIGRATION_26_27,
                         MIGRATION_27_28,
-                        MIGRATION_28_29
+                        MIGRATION_28_29,
+                        MIGRATION_30_31,
                     )
                     .build()
             }
