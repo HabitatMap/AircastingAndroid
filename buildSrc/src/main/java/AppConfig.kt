f77a9c56d41f8799615b6f11f10d5477f5e85298
mohsen <ash.wxrz@hotmail.com>
2022-05-28 12:52:37 +0430

    Added appConfig to separate app level properties

---
+++ buildSrc/src/main/java/AppConfig.kt
@@ -0,0 +1,11 @@
+object AppConfig {
+    const val compileSdk = 32
+    const val minSdk = 21
+    const val targetSdk = 32
+    const val versionCode = 197
+    const val versionName = "2.0.49"
+    const val buildToolsVersion = "30.0.3"
+
+    const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
+    const val proguardConsumerRules = "consumer-rules.pro"
+}
