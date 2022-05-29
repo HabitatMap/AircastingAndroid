b0910db423c1f32299739059b7586f747175d8ff
mohsen <ash.wxrz@hotmail.com>
2022-05-29 15:19:44 +0430

    Added all resConfigs = resConfigs was deprecated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,3 @@
-
 import java.io.FileInputStream
 import java.util.*
 
@@ -23,14 +22,27 @@ android {
         versionCode = AppConfig.versionCode
         versionName = AppConfig.versionName
         testInstrumentationRunner = AppConfig.androidTestInstrumentation
-        resConfigs("en", "fr", "sp")
+        resourceConfigurations.addAll(arrayOf("en", "fr", "sp"))
 
         val secureProps = Properties().apply {
             val file = file("../secure.properties")
             if (file.exists()) load(FileInputStream(File(rootProject.rootDir, "secure.properties")))
         }
         resValue("string", "maps_api_key", secureProps.getProperty("MAPS_API_KEY") ?: "")
-        buildConfigField("String", "PLACES_API_KEY", secureProps.getProperty("PLACES_API_KEY") ?: "\"\"")
+        buildConfigField(
+            "String",
+            "PLACES_API_KEY",
+            secureProps.getProperty("PLACES_API_KEY") ?: "\"\""
+        )
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
     }
 
     buildTypes {
@@ -47,22 +59,11 @@ android {
         }
     }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
     lint {
         abortOnError = false
     }
 
-    buildFeatures {
-        dataBinding = true
-    }
-
-    packagingOptions {
-        resources.excludes.add("META-INF/notice.txt")
-    }
+    buildFeatures.dataBinding = true
 
     sourceSets {
         this.getByName("main") {
@@ -86,21 +87,21 @@ dependencies {
     kaptAndroidTest(AppDependencies.kaptTestLibraries)
 
     // Tests
-    testImplementation("junit:junit:${Versions.junit}")
+    testImplementation("junit:junit:4.13.2")
     testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.8.2")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.6.20")
     testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
-    testImplementation("org.robolectric:robolectric:4.6")
-    testImplementation("com.squareup.okhttp3:mockwebserver:4.9.3")
+    testImplementation("org.robolectric:robolectric:4.8.1")
+    testImplementation("com.squareup.okhttp3:mockwebserver:5.0.0-alpha.7")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
     androidTestImplementation("androidx.test.espresso:espresso-intents:3.4.0")
     implementation("androidx.test.espresso:espresso-idling-resource:3.4.0")
 
     // for mocking in tests
-    androidTestImplementation("org.mockito:mockito-core:${Versions.mockitoVersion}")
-    androidTestImplementation("org.mockito:mockito-android:${Versions.mockitoVersion}")
-    androidTestImplementation("com.nhaarman:mockito-kotlin:1.5.0")
-    testImplementation("org.mockito:mockito-core:${Versions.mockitoVersion}")
+    androidTestImplementation("org.mockito:mockito-core:4.6.0")
+    androidTestImplementation("org.mockito:mockito-android:4.6.0")
+    androidTestImplementation("com.nhaarman:mockito-kotlin:1.6.0")
+    testImplementation("org.mockito:mockito-core:4.6.0")
 }
 
