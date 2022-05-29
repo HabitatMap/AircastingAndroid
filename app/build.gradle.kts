8c1a23af38e2c765d4f519b8bc4f5a574ac0d69f
mohsen <ash.wxrz@hotmail.com>
2022-05-29 10:52:31 +0430

    Using the dependencies files and separating them in the app build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,6 @@ android {
         getByName("debug") {
             manifestPlaceholders["crashlyticsCollectionEnabled"] = false
         }
-
         getByName("release") {
             isMinifyEnabled = false
             manifestPlaceholders["crashlyticsCollectionEnabled"] = true
@@ -78,64 +77,12 @@ android {
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}")
-    implementation("androidx.legacy:legacy-support-v4:${Versions.legacySupport}")
 
-    implementation("androidx.appcompat:appcompat:${Versions.androidX}")
-    implementation("androidx.fragment:fragment-ktx:${Versions.androidX}")
-    implementation("androidx.activity:activity-ktx:${Versions.activityKtx}")
-    implementation("androidx.core:core-ktx:${Versions.coreKtx}")
-    implementation("androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}")
-    implementation("androidx.lifecycle:lifecycle-extensions:${Versions.lifeCycleExtension}")
-    implementation("androidx.navigation:navigation-fragment-ktx:${Versions.fragmentKtx}")
-    implementation("androidx.navigation:navigation-ui-ktx:${Versions.fragmentKtx}")
-    implementation("androidx.swiperefreshlayout:swiperefreshlayout:${Versions.swipeRefresh}")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifeCycle}")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifeCycle}")
-    implementation("androidx.recyclerview:recyclerview:${Versions.recyclerView}")
+    implementation(AppDependencies.appLibraries)
+    kapt(AppDependencies.kaptLibraries)
+    kapt(AppDependencies.kaptTestLibraries)
 
-    implementation("com.google.android.material:material:${Versions.material}")
-    implementation("com.xwray:groupie:${Versions.groupie}")
-    implementation("com.xwray:groupie-kotlin-android-extensions:${Versions.groupie}")
-
-    implementation("com.google.guava:guava:${Versions.guava}")
-    implementation("org.greenrobot:eventbus:${Versions.eventBus}")
-
-    implementation("com.jakewharton:process-phoenix:2.0.0")
-
-    implementation("com.google.dagger:dagger:${Versions.dagger}")
-    kapt("com.google.dagger:dagger-compiler:${Versions.dagger}")
-    kaptAndroidTest("com.google.dagger:dagger-compiler:${Versions.dagger}")
-
-    implementation("com.squareup.retrofit2:retrofit:${Versions.retrofit}")
-    androidTestImplementation("com.squareup.retrofit2:retrofit-mock:${Versions.retrofit}")
-    implementation("com.squareup.retrofit2:converter-gson:${Versions.retrofit}")
-    implementation("com.squareup.okhttp3:logging-interceptor:${Versions.loggingInterceptor}")
-    androidTestImplementation("com.squareup.okhttp3:mockwebserver:${Versions.mockServerWeb}")
-    implementation("com.google.code.gson:gson:${Versions.gson}")
-    implementation("com.opencsv:opencsv:${Versions.openCv}")
-
-    implementation("com.google.android.gms:play-services-location:${Versions.playServicesLocation}")
-    implementation("com.google.android.gms:play-services-maps:${Versions.playServicesMaps}")
-
-    implementation("com.google.android.libraries.places:places:${Versions.places}")
-    implementation("com.google.android.gms:play-services-gcm:${Versions.playServicesGCM}")
-    implementation("com.google.auto.value:auto-value-annotations:${Versions.autoAnnotations}")
-
-    implementation(platform("com.google.firebase:firebase-bom:${Versions.firebaseBom}"))
-    implementation("com.google.firebase:firebase-crashlytics-ktx")
-    implementation("com.google.firebase:firebase-analytics-ktx")
-
-    implementation("androidx.room:room-runtime:${Versions.roomDB}")
-    kapt("androidx.room:room-compiler:${Versions.roomDB}")
-
-    implementation("commons-codec:commons-codec:${Versions.commonsCodec}")
-    implementation("no.nordicsemi.android:ble:${Versions.androidBle}")
-
-    implementation("com.github.PhilJay:MPAndroidChart:${Versions.mpAndroidChart}")
-
+    // Tests
     testImplementation("junit:junit:${Versions.junit}")
     testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.8.2")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.6.20")
@@ -148,10 +95,9 @@ dependencies {
     implementation("androidx.test.espresso:espresso-idling-resource:3.4.0")
 
     // for mocking in tests
-    val mockitoVersion = "4.0.0"
-    androidTestImplementation("org.mockito:mockito-core:$mockitoVersion")
-    androidTestImplementation("org.mockito:mockito-android:$mockitoVersion")
+    androidTestImplementation("org.mockito:mockito-core:${Versions.mockitoVersion}")
+    androidTestImplementation("org.mockito:mockito-android:${Versions.mockitoVersion}")
     androidTestImplementation("com.nhaarman:mockito-kotlin:1.5.0")
-    testImplementation("org.mockito:mockito-core:$mockitoVersion")
+    testImplementation("org.mockito:mockito-core:${Versions.mockitoVersion}")
 }
 
