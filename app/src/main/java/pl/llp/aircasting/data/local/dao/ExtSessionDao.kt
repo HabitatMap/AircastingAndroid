f816903a48494d439d31bb2ae8a87768486e6ab2
kgetman <kgetmanskii@gmail.com>
2022-06-01 13:12:05 +0200

    Added new queries to the DAO

--- app/src/main/java/pl/llp/aircasting/data/local/dao/ExtSessionDao.kt
+++ app/src/main/java/pl/llp/aircasting/data/local/dao/ExtSessionDao.kt
@@ -1,17 +1,26 @@
 package pl.llp.aircasting.data.local.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
 import pl.llp.aircasting.data.local.entity.ExtSessionsDBObject
+import pl.llp.aircasting.data.local.entity.ExternalSessionWithStreamsAndMeasurementsDBObject
+import pl.llp.aircasting.data.local.entity.ExternalSessionWithStreamsDBObject
 
 @Dao
 interface ExtSessionDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(extSession: ExtSessionsDBObject)
+    fun insert(extSession: ExtSessionsDBObject)
 
     @Delete
-    suspend fun delete(extSession: ExtSessionsDBObject)
+    fun delete(extSession: ExtSessionsDBObject)
 
     @Query("Select * from ext_sessions order by id ASC")
-    suspend fun getAllFollowedSessions(): List<ExtSessionsDBObject>
+    fun getAllFollowedSessions(): LiveData<List<ExtSessionsDBObject>>
+
+    @Query("Select * from ext_sessions order by id ASC")
+    fun getAllFollowedSessionsWithStreams(): LiveData<List<ExternalSessionWithStreamsDBObject>>
+
+    @Query("Select * from ext_sessions order by id ASC")
+    fun getAllFollowedSessionsWithStreamsAndMeasurements(): LiveData<List<ExternalSessionWithStreamsAndMeasurementsDBObject>>
 }
