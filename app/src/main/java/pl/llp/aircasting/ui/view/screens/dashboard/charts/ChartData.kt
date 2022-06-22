88d01773297cb5e5660a0e593b7893c7eba7c0d6
kgetman <kgetmanskii@gmail.com>
2022-06-22 16:21:35 +0200

    Delegated necessary chart data calculation to separate class

--- app/src/main/java/pl/llp/aircasting/ui/view/screens/dashboard/charts/ChartData.kt
+++ app/src/main/java/pl/llp/aircasting/ui/view/screens/dashboard/charts/ChartData.kt
@@ -1,149 +1,39 @@
 package pl.llp.aircasting.ui.view.screens.dashboard.charts
 
 import com.github.mikephil.charting.data.Entry
-import org.apache.commons.lang3.time.DateUtils
 import pl.llp.aircasting.data.model.MeasurementStream
 import pl.llp.aircasting.data.model.Session
-import pl.llp.aircasting.util.DateConverter
-import pl.llp.aircasting.util.helpers.services.AveragedMeasurementsService
-import java.util.*
 
-class ChartData(var session: Session) {
+class ChartData(private var mSession: Session) {
     var entriesStartTime: String = ""
     var entriesEndTime: String = ""
 
-    private var mSession = session
-    private lateinit var mEndTime: Date
-    private lateinit var mEntriesPerStream: HashMap<String, List<Entry>>
-    private var mMaxEntriesCount: Int = 0
-    private lateinit var mMeasurementStreams: MutableList<MeasurementStream>
-    private var mChartRefreshService = ChartRefreshService(session)
+    private val chartDataProvider: SessionChartDataCalculator = if (mSession.isExternal)
+        ExternalSessionChartDataCalculator(mSession)
+    else SessionChartDataCalculator(mSession)
+
+    private var mEntriesPerStream: HashMap<String, List<Entry>>
+    private var mChartRefreshService = ChartRefreshService(mSession)
 
     init {
-        initData()
-        calculateAverages()
-        calculateTimes()
+        entriesStartTime = chartDataProvider.mStartTime
+        entriesEndTime = chartDataProvider.mEndTime
+        mEntriesPerStream = chartDataProvider.mEntriesPerStream
 
         mChartRefreshService.setLastRefreshTime()
     }
 
     fun getEntries(stream: MeasurementStream?): List<Entry>? {
-        return mEntriesPerStream[streamKey(stream)]
+        return mEntriesPerStream[chartDataProvider.streamKey(stream)]
     }
 
     fun refresh(session: Session) {
         val hourChanged = mSession.endTime?.hours != session.endTime?.hours
 
         mSession = session
-        mEndTime = mSession.endTime ?: Date()
         if (mChartRefreshService.isTimeToRefresh() || hourChanged) {
-            initData()
-            calculateAverages()
-            calculateTimes()
+            chartDataProvider.refresh(session)
             mChartRefreshService.setLastRefreshTime()
         }
-    }
-
-    private fun initData() {
-        mEndTime = mSession.endTime ?: Date()
-        mEntriesPerStream = HashMap()
-        mMaxEntriesCount = 0
-        mMeasurementStreams = initStreams()
-    }
-
-    private fun averageFrequency(): Int {
-        return when (mSession.type) {
-            Session.Type.MOBILE -> Calendar.MINUTE
-            Session.Type.FIXED -> Calendar.HOUR
-        }
-    }
-
-    private fun averagesCount(): Int {
-        return if (mMaxEntriesCount > 0) {
-            -mMaxEntriesCount + 1
-        } else {
-            -mMaxEntriesCount
-        }
-    }
-
-    private fun startTimeString(): String {
-        val calendar = Calendar.getInstance()
-        calendar.time = mEndTime
-        calendar.add(averageFrequency(), averagesCount())
-        val startString = DateConverter.get()
-            ?.toTimeStringForDisplay(timeToDisplay(calendar.time), TimeZone.getDefault())
-
-        return startString ?: ""
-    }
-
-    private fun endTimeString(): String {
-        val endString = DateConverter.get()
-            ?.toTimeStringForDisplay(timeToDisplay(mEndTime), TimeZone.getDefault())
-
-        return endString ?: ""
-    }
-
-    private fun timeToDisplay(time: Date): Date {
-        return when (mSession.type) {
-            Session.Type.FIXED -> DateUtils.truncate(time, Calendar.HOUR)
-            Session.Type.MOBILE -> time
-        }
-    }
-
-    private fun initStreams(): MutableList<MeasurementStream> {
-        val streams: MutableList<MeasurementStream> = mutableListOf()
-        mSession.streamsSortedByDetailedType().forEach { stream ->
-            streams.add(stream)
-        }
-        return streams
-    }
-
-    private fun calculateAverages() {
-        for (stream in mMeasurementStreams) {
-            val entries: MutableList<Entry>? = createEntries(stream)
-            val entriesSize = entries?.size ?: 0
-            if (entriesSize > mMaxEntriesCount) {
-                mMaxEntriesCount = entries?.size ?: 0
-            }
-
-            entries?.toList()?.let {
-                mEntriesPerStream.put(
-                    streamKey(stream),
-                    it
-                )
-            }
-        }
-    }
-
-    private fun createEntries(stream: MeasurementStream?): MutableList<Entry>? {
-        var entries: MutableList<Entry>? = null
-
-        stream?.let { stream ->
-            entries = when (mSession.type) {
-                Session.Type.MOBILE -> {
-                    val averagedMeasurementsService = AveragedMeasurementsService(session.uuid)
-                    val measurementsOverSecondThreshold =
-                        averagedMeasurementsService.getMeasurementsOverSecondThreshold(stream)
-                    if (measurementsOverSecondThreshold.isNullOrEmpty()) {
-                        ChartAveragesCreator().getMobileEntries(stream)
-                    } else {
-                        ChartAveragesCreator().getMobileEntriesForSessionOverSecondThreshold(
-                            measurementsOverSecondThreshold
-                        )
-                    }
-                }
-                Session.Type.FIXED -> ChartAveragesCreator().getFixedEntries(stream)
-            }
-        }
-        return entries
-    }
-
-    private fun calculateTimes() {
-        entriesStartTime = startTimeString()
-        entriesEndTime = endTimeString()
-    }
-
-    private fun streamKey(stream: MeasurementStream?): String {
-        return "${mSession.uuid}_${stream?.sensorName}"
     }
 }
