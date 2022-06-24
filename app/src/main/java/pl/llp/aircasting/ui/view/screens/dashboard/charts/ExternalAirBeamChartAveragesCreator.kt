0b6762dc0d00eb9a5695b1496c947ce8764a99f1
kgetman <kgetmanskii@gmail.com>
2022-06-24 10:34:05 +0200

    ExternalAirBeamChartAveragesCreator for calculating of external airbeam sessions - their last non full hour of measurements needs to be cut out here

---
+++ app/src/main/java/pl/llp/aircasting/ui/view/screens/dashboard/charts/ExternalAirBeamChartAveragesCreator.kt
@@ -0,0 +1,20 @@
+package pl.llp.aircasting.ui.view.screens.dashboard.charts
+
+import org.apache.commons.lang3.time.DateUtils
+import pl.llp.aircasting.data.model.Measurement
+import pl.llp.aircasting.data.model.MeasurementStream
+import java.util.*
+
+class ExternalAirBeamChartAveragesCreator : ExternalChartAveragesCreator() {
+    override fun getAllowedEndTimeBoundary(stream: MeasurementStream): Date {
+        val lastMeasurementTime = super.getAllowedEndTimeBoundary(stream)
+        return DateUtils.truncate(lastMeasurementTime, Calendar.HOUR_OF_DAY)
+    }
+
+    override fun getMeasurementsInAllowedTimeBoundaries(
+        stream: MeasurementStream,
+        boundary: Calendar
+    ): List<Measurement> {
+        return super.getMeasurementsInAllowedTimeBoundaries(stream, boundary)
+    }
+}
