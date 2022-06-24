88f63a56e2595213df02df5f6d345f67769eb5f9
kgetman <kgetmanskii@gmail.com>
2022-06-24 11:48:49 +0200

    Override getAllowedEndTimeBoundary for external PurpleAir and AirBeam as we don't need the last hour measurements

--- app/src/main/java/pl/llp/aircasting/ui/view/screens/dashboard/charts/ExternalAirBeamChartAveragesCreator.kt
+++ app/src/main/java/pl/llp/aircasting/ui/view/screens/dashboard/charts/ExternalAirBeamChartAveragesCreator.kt
@@ -1,22 +1,14 @@
 package pl.llp.aircasting.ui.view.screens.dashboard.charts
 
 import org.apache.commons.lang3.time.DateUtils
-import pl.llp.aircasting.data.model.Measurement
 import pl.llp.aircasting.data.model.MeasurementStream
 import java.util.*
 
 class ExternalAirBeamChartAveragesCreator : ExternalChartAveragesCreator() {
     override fun getAllowedEndTimeBoundary(stream: MeasurementStream): Date {
         val lastMeasurementTime = super.getAllowedEndTimeBoundary(stream)
-        return DateUtils.truncate(lastMeasurementTime, Calendar.HOUR_OF_DAY)
-    }
-
-    override fun getMeasurementsInAllowedTimeBoundaries(
-        stream: MeasurementStream,
-        boundary: Calendar
-    ): List<Measurement> {
-        val withStartTimeBoundary = super.getMeasurementsInAllowedTimeBoundaries(stream, boundary)
-        return withStartTimeBoundary.filter { it.time < getAllowedEndTimeBoundary(stream) }
+        val lastMeasurementHour = DateUtils.truncate(lastMeasurementTime, Calendar.HOUR_OF_DAY)
+        return Date(lastMeasurementHour.time - 1)
     }
 
     override fun modifyHours(date: Date, hours: Int): Date {
