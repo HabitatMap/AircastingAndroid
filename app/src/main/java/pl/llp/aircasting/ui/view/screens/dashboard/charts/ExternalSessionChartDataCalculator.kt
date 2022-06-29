867bbc2cea15bba9892690345db801b31695ec45
kgetman <kgetmanskii@gmail.com>
2022-06-29 13:28:31 +0200

    Overrode the TimeStampSetter for external sessions to apply UTC timezone

--- app/src/main/java/pl/llp/aircasting/ui/view/screens/dashboard/charts/ExternalSessionChartDataCalculator.kt
+++ app/src/main/java/pl/llp/aircasting/ui/view/screens/dashboard/charts/ExternalSessionChartDataCalculator.kt
@@ -4,21 +4,23 @@ import com.github.mikephil.charting.data.Entry
 import pl.llp.aircasting.data.api.util.StringConstants
 import pl.llp.aircasting.data.model.MeasurementStream
 import pl.llp.aircasting.data.model.Session
+import java.util.*
 
 class ExternalSessionChartDataCalculator(session: Session) : SessionChartDataCalculator(session) {
-
     override fun calculateEntriesAndTimestamps(stream: MeasurementStream?): MutableList<Entry>? {
+        val timeStampsSetter = UTCTimeStampsSetter()
+
         return when {
             stream == null -> null
 
             isFromOpenAQ(stream) -> OpenAQChartAveragesCreator().getFixedEntries(
                 stream,
-                timeSetter
+                timeStampsSetter
             )
 
             else -> ChartAveragesCreator().getFixedEntries(
                 stream,
-                timeSetter
+                timeStampsSetter
             )
         }
     }
@@ -26,4 +28,10 @@ class ExternalSessionChartDataCalculator(session: Session) : SessionChartDataCal
     private fun isFromOpenAQ(stream: MeasurementStream) =
         stream.sensorName.contains(StringConstants.responseOpenAQSensorNamePM, true) ||
                 stream.sensorName.contains(StringConstants.responseOpenAQSensorNameOzone, true)
+
+    inner class UTCTimeStampsSetter : TimeStampsSetter() {
+        override fun setStartEndTimeToDisplay(start: Date, end: Date, timeZone: TimeZone) {
+            super.setStartEndTimeToDisplay(start, end, TimeZone.getTimeZone("UTC"))
+        }
+    }
 }
