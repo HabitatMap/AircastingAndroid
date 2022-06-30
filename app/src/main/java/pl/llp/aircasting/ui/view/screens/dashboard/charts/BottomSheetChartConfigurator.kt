57b5570c74d4835fd7582ccff201fa57538000dc
kgetman <kgetmanskii@gmail.com>
2022-06-30 16:04:22 +0200

    Made axis configuration go before super class' method is called

--- app/src/main/java/pl/llp/aircasting/ui/view/screens/dashboard/charts/BottomSheetChartConfigurator.kt
+++ app/src/main/java/pl/llp/aircasting/ui/view/screens/dashboard/charts/BottomSheetChartConfigurator.kt
@@ -6,14 +6,12 @@ import com.github.mikephil.charting.data.LineDataSet
 
 class BottomSheetChartConfigurator(mContext: Context) : ChartConfigurator(mContext) {
     override fun configure(chart: LineChart?, dataset: LineDataSet?) {
-        super.configure(chart, dataset)
-
         // Squeezing chart so that the max value does not jump to the top drastically
         chart?.apply {
             axisLeft.axisMinimum = -5f
             axisLeft.spaceTop = 80f
-            axisRight.axisMinimum = -5f
             axisRight.spaceTop = 80f
         }
+        super.configure(chart, dataset)
     }
 }
