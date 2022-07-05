9f0ea040f545ab98fa97d5be55dc9000a61ff5d7
Mohsen <ash.wxrz@hotmail.com>
2022-07-05 13:39:43 +0200

    Extracting code to a method + setContentView to have a better layout with constraintlayiut and frameLayout

--- app/src/main/java/pl/llp/aircasting/ui/view/screens/settings/MySettingsActivity.kt
+++ app/src/main/java/pl/llp/aircasting/ui/view/screens/settings/MySettingsActivity.kt
@@ -7,6 +7,12 @@ import pl.llp.aircasting.R
 class MySettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_settings)
+
+        setupFragmentTransaction()
+    }
+
+    private fun setupFragmentTransaction() {
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.settings_container, MySettingsFragment())
