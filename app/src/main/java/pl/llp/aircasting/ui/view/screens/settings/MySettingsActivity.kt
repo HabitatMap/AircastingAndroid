dfb4d49d9e32c038544f125a82a5ab3ce25254f8
Mohsen <ash.wxrz@hotmail.com>
2022-07-05 17:33:17 +0200

    Added the previous setups

--- app/src/main/java/pl/llp/aircasting/ui/view/screens/settings/MySettingsActivity.kt
+++ app/src/main/java/pl/llp/aircasting/ui/view/screens/settings/MySettingsActivity.kt
@@ -3,13 +3,19 @@ package pl.llp.aircasting.ui.view.screens.settings
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import pl.llp.aircasting.R
+import pl.llp.aircasting.util.adjustMenuVisibility
 
 class MySettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_settings)
 
+        setupUI()
         setupFragmentTransaction()
+    }
+
+    private fun setupUI() {
+        adjustMenuVisibility(this, false)
     }
 
     private fun setupFragmentTransaction() {
