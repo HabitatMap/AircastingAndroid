a20a69aa16ae79cacf6f2b91847f9312625c5a79
kgetman <kgetmanskii@gmail.com>
2022-07-06 15:25:34 +0200

    Added onCollapseSessionCard to SessionMvcListener; added blank implementations in FinishSessionListener, so that there would be no need to override them for those classes that don't need them

--- app/src/main/java/pl/llp/aircasting/ui/view/screens/dashboard/SessionsViewMvc.kt
+++ app/src/main/java/pl/llp/aircasting/ui/view/screens/dashboard/SessionsViewMvc.kt
@@ -23,6 +23,7 @@ interface SessionsViewMvc : ObservableViewMvc<SessionsViewMvc.Listener> {
         fun onMapButtonClicked(session: Session, sensorName: String?)
         fun onGraphButtonClicked(session: Session, sensorName: String?)
         fun onExpandSessionCard(session: Session)
+        fun onCollapseSessionCard(session: Session) { /* Do nothing */ }
     }
 
     fun showSessionsView(sessions: List<Session>, sensorThresholds: HashMap<String, SensorThreshold>)
--- app/src/main/java/pl/llp/aircasting/ui/view/screens/dashboard/active/FinishSessionListener.kt
+++ app/src/main/java/pl/llp/aircasting/ui/view/screens/dashboard/active/FinishSessionListener.kt
@@ -3,6 +3,6 @@ package pl.llp.aircasting.ui.view.screens.dashboard.active
 import pl.llp.aircasting.data.model.Session
 
 interface FinishSessionListener {
-    fun onFinishSessionConfirmed(session: Session)
-    fun onFinishAndSyncSessionConfirmed(session: Session)
+    fun onFinishSessionConfirmed(session: Session) { /* Do nothing */ }
+    fun onFinishAndSyncSessionConfirmed(session: Session) { /* Do nothing */ }
 }
