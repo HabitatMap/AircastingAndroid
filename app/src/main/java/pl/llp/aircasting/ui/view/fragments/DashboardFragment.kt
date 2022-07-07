8ac03a24e7eceeeaf6d03056dbe77c9e50fda9e9
Mohsen <ash.wxrz@hotmail.com>
2022-07-07 11:10:28 +0200

    Added the logic to show Dialog when it is in the following tab

--- app/src/main/java/pl/llp/aircasting/ui/view/fragments/DashboardFragment.kt
+++ app/src/main/java/pl/llp/aircasting/ui/view/fragments/DashboardFragment.kt
@@ -4,21 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
+import androidx.fragment.app.FragmentActivity
 import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.fragment_dashboard.*
 import pl.llp.aircasting.AircastingApplication
+import pl.llp.aircasting.R
 import pl.llp.aircasting.ui.view.common.BaseFragment
+import pl.llp.aircasting.ui.view.common.BatteryAlertDialog
 import pl.llp.aircasting.ui.view.screens.dashboard.DashboardController
 import pl.llp.aircasting.ui.view.screens.dashboard.DashboardPagerAdapter
 import pl.llp.aircasting.ui.view.screens.dashboard.DashboardViewMvcImpl
 import pl.llp.aircasting.util.Settings
 import pl.llp.aircasting.util.adjustMenuVisibility
+import pl.llp.aircasting.util.isIgnoringBatteryOptimizations
+import pl.llp.aircasting.util.isSDKGreaterOrEqualToM
 import javax.inject.Inject
 
 class DashboardFragment : BaseFragment<DashboardViewMvcImpl, DashboardController>() {
 
     @Inject
     lateinit var settings: Settings
+
+    private var mTabPosition: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,6 +47,7 @@ class DashboardFragment : BaseFragment<DashboardViewMvcImpl, DashboardController
         val tabId = arguments?.get("tabId") as Int?
         controller?.onCreate(tabId)
 
+        handleBackButtonPress()
         return view?.rootView
     }
 
@@ -52,14 +61,17 @@ class DashboardFragment : BaseFragment<DashboardViewMvcImpl, DashboardController
         tabs?.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
                 isAdjustable(tab.position)
+                mTabPosition = tab.position
             }
 
             override fun onTabUnselected(tab: TabLayout.Tab) {
                 isAdjustable(tab.position)
+                mTabPosition = tab.position
             }
 
             override fun onTabReselected(tab: TabLayout.Tab) {
                 isAdjustable(tab.position)
+                mTabPosition = tab.position
             }
         })
     }
@@ -67,12 +79,36 @@ class DashboardFragment : BaseFragment<DashboardViewMvcImpl, DashboardController
     private fun isAdjustable(position: Int) {
         activity?.let {
             if (position == 0)
-                adjustMenuVisibility(
-                    it,
-                    true,
-                    settings.getFollowedSessionsNumber()
-                ) else adjustMenuVisibility(it, false)
+                adjustMenuVisibility(it, true, settings.getFollowedSessionsNumber())
+            else adjustMenuVisibility(it, false)
         }
     }
 
+    private fun handleBackButtonPress() {
+        requireActivity().let {
+            it.onBackPressedDispatcher
+                .addCallback(viewLifecycleOwner) {
+
+                    if (isEnabled && mTabPosition == 0) {
+                        isEnabled = false
+                        checkIfBatteryOptimizationShouldBeShown()
+                    } else it.onBackPressed()
+
+                }
+        }
+    }
+
+    private fun checkIfBatteryOptimizationShouldBeShown() {
+        if (isSDKGreaterOrEqualToM() && !isIgnoringBatteryOptimizations(requireContext())) showBatteryOptimizationHelperDialog(
+            requireActivity()
+        )
+    }
+
+    private fun showBatteryOptimizationHelperDialog(fragmentActivity: FragmentActivity) {
+        BatteryAlertDialog(
+            fragmentActivity.supportFragmentManager,
+            fragmentActivity.getString(R.string.running_background),
+            fragmentActivity.getString(R.string.battery_desc)
+        ).show()
+    }
 }
