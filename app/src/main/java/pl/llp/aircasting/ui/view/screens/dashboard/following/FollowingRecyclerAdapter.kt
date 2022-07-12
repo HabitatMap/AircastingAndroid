a6b81fcf375e1e6998b4d8cf9532f9f51926b2a3
kgetman <kgetmanskii@gmail.com>
2022-07-12 13:07:34 +0200

    Moved retrieving saved card state to FollowingRecyclerAdapter only - overrode  initSessionPresenter

--- app/src/main/java/pl/llp/aircasting/ui/view/screens/dashboard/following/FollowingRecyclerAdapter.kt
+++ app/src/main/java/pl/llp/aircasting/ui/view/screens/dashboard/following/FollowingRecyclerAdapter.kt
@@ -3,15 +3,18 @@ package pl.llp.aircasting.ui.view.screens.dashboard.following
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
+import pl.llp.aircasting.data.model.SensorThreshold
 import pl.llp.aircasting.data.model.Session
 import pl.llp.aircasting.ui.view.screens.dashboard.SessionCardListener
+import pl.llp.aircasting.ui.view.screens.dashboard.SessionPresenter
 import pl.llp.aircasting.ui.view.screens.dashboard.SessionsRecyclerAdapter
+import pl.llp.aircasting.util.expandedCards
 
 open class FollowingRecyclerAdapter(
     private val mInflater: LayoutInflater,
     private val mListener: SessionCardListener,
     supportFragmentManager: FragmentManager
-): SessionsRecyclerAdapter<SessionCardListener>(mInflater, supportFragmentManager) {
+) : SessionsRecyclerAdapter<SessionCardListener>(mInflater, supportFragmentManager) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val viewMvc =
@@ -22,6 +25,14 @@ open class FollowingRecyclerAdapter(
             )
         viewMvc.registerListener(mListener)
         return MyViewHolder(viewMvc)
+    }
+
+    override fun initSessionPresenter(
+        session: Session,
+        sensorThresholds: HashMap<String, SensorThreshold>
+    ): SessionPresenter {
+        val expandedState = expandedCards()?.contains(session.uuid) ?: false
+        return SessionPresenter(session, sensorThresholds, expanded = expandedState)
     }
 
     override fun prepareSession(session: Session, expanded: Boolean): Session {
