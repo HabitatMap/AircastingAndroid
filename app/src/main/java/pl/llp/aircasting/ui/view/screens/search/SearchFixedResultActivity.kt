d7e73f883cea0ba42bb9efc2ac604212f353cb56
Mohsen <ash.wxrz@hotmail.com>
2022-07-14 12:56:38 +0200

    Removing the clearTop flag as its not working beside the popBackStack

--- app/src/main/java/pl/llp/aircasting/ui/view/screens/search/SearchFixedResultActivity.kt
+++ app/src/main/java/pl/llp/aircasting/ui/view/screens/search/SearchFixedResultActivity.kt
@@ -161,7 +161,6 @@ class SearchFixedResultActivity : AppCompatActivity(), OnMapReadyCallback,
 
     private fun goToDashboard() {
         val intent = Intent(this, MainActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
         startActivity(intent)
     }
 
