797a01107c02d108dccc8e17d4924d74ed6a1bbd
Mohsen <ash.wxrz@hotmail.com>
2022-07-20 11:38:02 +0200

    Added ext func + using it

--- app/src/main/java/pl/llp/aircasting/util/extensions/ContextExtensions.kt
+++ app/src/main/java/pl/llp/aircasting/util/extensions/ContextExtensions.kt
@@ -182,3 +182,5 @@ fun Context.hideKeyboard(view: View) {
     val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
 }
+
+fun Context.hideKeyboard() = (this as? Activity)?.hideKeyboard()
