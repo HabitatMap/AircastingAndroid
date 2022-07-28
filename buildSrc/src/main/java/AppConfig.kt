466257c2f7b590b2ea7f6bd60a976c7eb3873ca8
Mohsen <ash.wxrz@hotmail.com>
2022-07-28 18:54:03 +0200

    Bumped 2.1.9

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -2,10 +2,9 @@ object AppConfig {
     const val compileSdk = 32
     const val minSdk = 21
     const val targetSdk = 32
-    const val versionCode = 209
-    const val versionName = "2.1.7"
+    const val versionCode = 211
+    const val versionName = "2.1.9"
     const val buildToolsVersion = "30.0.3"
 
     const val androidTestInstrumentation = "pl.llp.aircasting.helpers.EspressoRunner"
-    const val proguardConsumerRules = "consumer-rules.pro"
 }
