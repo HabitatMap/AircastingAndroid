8ba5b0318358c03e87f65d9ac9020debfff35e8f
kgetman <kgetmanskii@gmail.com>
2022-08-30 10:45:27 +0200

    Set wifi SSID to unfocusable editText

--- app/src/main/java/pl/llp/aircasting/ui/view/screens/new_session/session_details/NetworkPasswordDialog.kt
+++ app/src/main/java/pl/llp/aircasting/ui/view/screens/new_session/session_details/NetworkPasswordDialog.kt
@@ -3,15 +3,15 @@ package pl.llp.aircasting.ui.view.screens.new_session.session_details
 import android.view.LayoutInflater
 import android.view.View
 import androidx.fragment.app.FragmentManager
+import kotlinx.android.synthetic.main.network_password_dialog.view.*
 import pl.llp.aircasting.R
 import pl.llp.aircasting.ui.view.common.BaseDialog
-import kotlinx.android.synthetic.main.network_password_dialog.view.*
 
 class NetworkPasswordDialog(
     private val wifiSSID: String,
     private val mFragmentManager: FragmentManager,
     private val listener: FixedSessionDetailsViewMvc.OnPasswordProvidedListener
-): BaseDialog(mFragmentManager) {
+) : BaseDialog(mFragmentManager) {
     private lateinit var mView: View
 
     override fun setupView(inflater: LayoutInflater): View {
@@ -20,6 +20,8 @@ class NetworkPasswordDialog(
         mView.wifi_password_dialog_header.text = requireActivity()
             .getString(R.string.fixed_session_details_wifi_password_dialog_header)
             .format(wifiSSID)
+
+        mView.wifi_ssid_input.setText(wifiSSID)
 
         mView.ok_button.setOnClickListener {
             okButtonClicked()
