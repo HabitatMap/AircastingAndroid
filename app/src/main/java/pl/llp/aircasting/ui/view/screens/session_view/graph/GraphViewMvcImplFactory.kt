960b066ca7c55850e2c1ad6b24d5ccd12a1bc546
mohsen <ash.wxrz@hotmail.com>
2022-09-02 11:21:41 +0200

    Code format - refactor

--- app/src/main/java/pl/llp/aircasting/ui/view/screens/session_view/graph/GraphViewMvcImplFactory.kt
+++ app/src/main/java/pl/llp/aircasting/ui/view/screens/session_view/graph/GraphViewMvcImplFactory.kt
@@ -5,20 +5,31 @@ import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
 import pl.llp.aircasting.ui.view.screens.dashboard.SessionsTab
 
-
 class GraphViewMvcImplFactory {
     companion object {
-        open fun get(
+        fun get(
             inflater: LayoutInflater,
             parent: ViewGroup?,
             supportFragmentManager: FragmentManager?,
             sessionTab: SessionsTab
         ): GraphViewMvcImpl {
-            return when(sessionTab){
-                SessionsTab.FOLLOWING -> GraphViewFollowingMvcImpl(inflater, parent, supportFragmentManager)
+            return when (sessionTab) {
+                SessionsTab.FOLLOWING -> GraphViewFollowingMvcImpl(
+                    inflater,
+                    parent,
+                    supportFragmentManager
+                )
                 SessionsTab.FIXED -> GraphViewFixedMvcImpl(inflater, parent, supportFragmentManager)
-                SessionsTab.MOBILE_DORMANT -> GraphViewMobileDormantMvcImpl(inflater, parent, supportFragmentManager)
-                SessionsTab.MOBILE_ACTIVE -> GraphViewMobileActiveMvcImpl(inflater, parent, supportFragmentManager)
+                SessionsTab.MOBILE_DORMANT -> GraphViewMobileDormantMvcImpl(
+                    inflater,
+                    parent,
+                    supportFragmentManager
+                )
+                SessionsTab.MOBILE_ACTIVE -> GraphViewMobileActiveMvcImpl(
+                    inflater,
+                    parent,
+                    supportFragmentManager
+                )
             }
         }
     }
