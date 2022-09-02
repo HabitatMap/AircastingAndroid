535054b7526c49a0abf878edf66d7af2e4c95e4c
kgetman <kgetmanskii@gmail.com>
2022-09-02 18:00:56 +0200

    Revert save wifi with autofill commits

--- app/src/main/java/pl/llp/aircasting/ui/view/screens/new_session/session_details/SessionDetailsController.kt
+++ app/src/main/java/pl/llp/aircasting/ui/view/screens/new_session/session_details/SessionDetailsController.kt
@@ -16,9 +16,6 @@ open class SessionDetailsController(
     }
 
     open fun onCreate() {}
-    open fun onPause() {}
-    open fun onResume() {}
-    open fun onStop() {}
 
     fun onDestroy() {
         mViewMvc = null
