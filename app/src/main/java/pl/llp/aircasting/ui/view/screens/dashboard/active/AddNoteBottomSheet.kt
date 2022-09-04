6ee7a024ac081255fca8b9af66c19dbd9a00d7d6
Mohsen <ash.wxrz@hotmail.com>
2022-09-04 22:09:37 +0200

    BottomSheet implementation, plus layout

--- app/src/main/java/pl/llp/aircasting/ui/view/screens/dashboard/active/AddNoteBottomSheet.kt
+++ app/src/main/java/pl/llp/aircasting/ui/view/screens/dashboard/active/AddNoteBottomSheet.kt
@@ -1,7 +1,13 @@
 package pl.llp.aircasting.ui.view.screens.dashboard.active
 
+import android.app.Activity
 import android.content.Context
+import android.net.Uri
 import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import com.github.dhaval2404.imagepicker.ImagePicker
 import kotlinx.android.synthetic.main.add_note_bottom_sheet.view.*
 import pl.llp.aircasting.R
 import pl.llp.aircasting.data.model.Note
@@ -9,6 +15,7 @@ import pl.llp.aircasting.data.model.Session
 import pl.llp.aircasting.ui.view.common.BottomSheet
 import pl.llp.aircasting.util.exceptions.ErrorHandler
 import pl.llp.aircasting.util.exceptions.NotesNoLocationError
+import pl.llp.aircasting.util.extensions.visible
 import java.util.*
 
 class AddNoteBottomSheet(
@@ -22,6 +29,7 @@ class AddNoteBottomSheet(
     }
 
     private var noteInput: EditText? = null
+    private lateinit var mPhotoPath: Uri
 
     override fun layoutId(): Int {
         return R.layout.add_note_bottom_sheet
@@ -55,21 +63,57 @@ class AddNoteBottomSheet(
                 noteText,
                 lastMeasurement.latitude,
                 lastMeasurement.longitude,
-                0
-            ) // todo: add "photoPath" later on
+                0,
+                mPhotoPath.toString())
         else
             note = Note(
                 date,
                 noteText,
                 lastMeasurement.latitude,
                 lastMeasurement.longitude,
-                mSession.notes.last().number + 1
+                mSession.notes.last().number + 1,
+                null
             )
+        // TODO: More validation needed
 
         mListener.addNotePressed(mSession, note)
     }
 
+    /**
+     * This is customizable - currently we have crop option + camera only.
+     * We are also able to add the gallery option to this library.
+     * More info: https://github.com/Dhaval2404/ImagePicker#customization
+     **/
     private fun addPictureButton() {
-        CameraPermissionHelperDialog(childFragmentManager).show()
+        ImagePicker.with(this)
+            .crop()
+            .cameraOnly()
+            .createIntent { intent -> startForProfileImageResult.launch(intent) }
     }
+
+    private val startForProfileImageResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            val resultCode = result.resultCode
+            val data = result.data
+
+            when (resultCode) {
+                Activity.RESULT_OK -> {
+                    val fileUri = data?.data
+                    fileUri ?: return@registerForActivityResult
+
+                    contentView?.captured_image?.apply {
+                        setImageURI(fileUri)
+                        mPhotoPath = fileUri
+                        visible()
+                    }
+
+                }
+                ImagePicker.RESULT_ERROR -> Toast.makeText(
+                    mContext,
+                    ImagePicker.getError(data),
+                    Toast.LENGTH_SHORT
+                ).show()
+                else -> {}
+            }
+        }
 }
