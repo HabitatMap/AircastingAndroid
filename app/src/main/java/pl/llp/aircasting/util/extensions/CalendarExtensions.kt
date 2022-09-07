5703382a992986f5da1d58dbbdb8202b8efe610e
kgetman <kgetmanskii@gmail.com>
2022-09-07 13:48:44 +0200

    Remove long comments, as code describes itself fully. Left comment regarding UTC timestamp

--- app/src/main/java/pl/llp/aircasting/util/extensions/CalendarExtensions.kt
+++ app/src/main/java/pl/llp/aircasting/util/extensions/CalendarExtensions.kt
@@ -17,17 +17,9 @@ fun Calendar.dayOfMonth(date: Date, isExternalSession: Boolean): Int {
         timeZone = TimeZone.getTimeZone("UTC")
     return this[DAY_OF_MONTH]
 }
-/**
- * This method returns the Start of the current day - a year ago in milliseconds.
- * timeZone is UTC.
- * EXAMPLE: if the current day is Aug 2:
- * @return "1627862400"
- * Assuming that this timestamp is in seconds:
- * GMT: Monday, August 2, 2021 12:00:00 AM
- * Your time zone: Monday, August 2, 2021 2:00:00 AM GMT+02:00 DST
- * Relative: A year ago
- **/
+
 fun Calendar.getStartOfTodaySecondsFromYearAgo(): Long {
+    // UTC timezone here is ESSENTIAL, as backend assumes receiving timestamp in UTC
     timeZone = TimeZone.getTimeZone("UTC")
     add(YEAR, -1)
     set(HOUR_OF_DAY, 0)
@@ -37,17 +29,9 @@ fun Calendar.getStartOfTodaySecondsFromYearAgo(): Long {
 
     return timeInMillis / 1000
 }
-/**
- * This method returns the end of the current day in milliseconds.
- * timeZone is UTC.
- * EXAMPLE: if the current day is Aug 2, the method returns:
- * @return "1659484799"
- * Assuming that this timestamp is in seconds:
- * GMT: Tuesday, August 2, 2022 11:59:59 PM
- * Your time zone: Wednesday, August 3, 2022 1:59:59 AM GMT+02:00 DST
- * Relative: In 13 hours
- **/
+
 fun Calendar.getEndOfTodaySeconds(): Long {
+    // UTC timezone here is ESSENTIAL, as backend assumes receiving timestamp in UTC
     timeZone = TimeZone.getTimeZone("UTC")
     set(HOUR_OF_DAY, 23)
     set(MINUTE, 59)
