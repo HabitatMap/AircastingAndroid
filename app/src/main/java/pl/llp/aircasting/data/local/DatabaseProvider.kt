eeb397560662779781e9ba0508e59f955b00c096
Mohsen <ash.wxrz@hotmail.com>
2022-09-13 12:48:03 +0200

    Added auto migration feature

--- app/src/main/java/pl/llp/aircasting/data/local/DatabaseProvider.kt
+++ app/src/main/java/pl/llp/aircasting/data/local/DatabaseProvider.kt
@@ -2,24 +2,21 @@ package pl.llp.aircasting.data.local
 
 import android.annotation.SuppressLint
 import android.content.Context
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
+import androidx.room.*
 import pl.llp.aircasting.data.local.dao.*
 import pl.llp.aircasting.data.local.entity.*
-import pl.llp.aircasting.data.local.migrations.*
+import pl.llp.aircasting.data.local.migrations.MIGRATION_16_17
 import pl.llp.aircasting.util.converters.*
 
 @Database(
+    version = 32,
     entities = [SessionDBObject::class,
         MeasurementStreamDBObject::class,
         MeasurementDBObject::class,
         SensorThresholdDBObject::class,
         NoteDBObject::class,
         ActiveSessionMeasurementDBObject::class],
-    version = 31,
-    exportSchema = true
+    autoMigrations = [AutoMigration(from = 16, to = 32)]
 )
 @TypeConverters(
     DateConverter::class,
@@ -56,22 +53,7 @@ class DatabaseProvider {
                     AppDatabase::class.java, DB_NAME
                 )
                     .fallbackToDestructiveMigration()
-                    .addMigrations(
-                        MIGRATION_16_17,
-                        MIGRATION_17_18,
-                        MIGRATION_18_19,
-                        MIGRATION_19_20,
-                        MIGRATION_20_21,
-                        MIGRATION_21_22,
-                        MIGRATION_22_23,
-                        MIGRATION_23_24,
-                        MIGRATION_24_25,
-                        MIGRATION_25_26,
-                        MIGRATION_26_27,
-                        MIGRATION_27_28,
-                        MIGRATION_28_29,
-                        MIGRATION_30_31,
-                    )
+                    .addMigrations(MIGRATION_16_17)
                     .build()
             }
 
