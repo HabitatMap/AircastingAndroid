758568cb04f22e185ab95e09402efbd5bb2dae5f
kgetman <kgetmanskii@gmail.com>
2022-09-16 13:46:58 +0200

    Revert "Data class to hold the notes data after sync"
    This reverts commit d09f41e746a6fc97a10843b486d978bfd31c71a6.

--- app/src/main/java/pl/llp/aircasting/data/api/response/UploadSessionResponse.kt
+++ app/src/main/java/pl/llp/aircasting/data/api/response/UploadSessionResponse.kt
@@ -1,7 +1,3 @@
 package pl.llp.aircasting.data.api.response
 
-class UploadSessionResponse(
-    val location: String, val notes: List<Notes>
-)
-
-
+class UploadSessionResponse(val location: String)
