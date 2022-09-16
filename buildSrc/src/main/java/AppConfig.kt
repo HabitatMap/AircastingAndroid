9911fb836173b538a95bf9ca6b814c6dad9c3618
kgetman <kgetmanskii@gmail.com>
2022-09-16 16:17:07 +0200

    Bump 2.1.19

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -2,8 +2,8 @@ object AppConfig {
     const val compileSdk = 33
     const val minSdk = 21
     const val targetSdk = 33
-    const val versionCode = 220
-    const val versionName = "2.1.18"
+    const val versionCode = 221
+    const val versionName = "2.1.19"
     const val buildToolsVersion = "30.0.3"
 
     const val androidTestInstrumentation = "pl.llp.aircasting.helpers.EspressoRunner"
