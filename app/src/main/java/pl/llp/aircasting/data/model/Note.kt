034ee61880b9825e45f0ba3e8687487552c352ba
kgetman <kgetmanskii@gmail.com>
2022-09-16 10:35:29 +0200

    Generate equals() and hashCode() for Note to remove() it from presenter's list

--- app/src/main/java/pl/llp/aircasting/data/model/Note.kt
+++ app/src/main/java/pl/llp/aircasting/data/model/Note.kt
@@ -20,6 +20,32 @@ class Note(
         noteDBObject.number,
         noteDBObject.photo_location
     )
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Note
+
+        if (date != other.date) return false
+        if (text != other.text) return false
+        if (latitude != other.latitude) return false
+        if (longitude != other.longitude) return false
+        if (number != other.number) return false
+        if (photo_location != other.photo_location) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = date.hashCode()
+        result = 31 * result + text.hashCode()
+        result = 31 * result + (latitude?.hashCode() ?: 0)
+        result = 31 * result + (longitude?.hashCode() ?: 0)
+        result = 31 * result + number
+        result = 31 * result + (photo_location?.hashCode() ?: 0)
+        return result
+    }
 }
 
 
