02ec096e5513bd08ebd5ea6d3877671430cb7e68
kgetman <kgetmanskii@gmail.com>
2023-03-16 16:14:37 +0100

    bump 2.4.4(238)

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -2,8 +2,8 @@ object AppConfig {
     const val compileSdk = 33
     const val minSdk = 21
     const val targetSdk = 33
-    const val versionCode = 237
-    const val versionName = "2.4.3"
+    const val versionCode = 238
+    const val versionName = "2.4.4"
     const val buildToolsVersion = "30.0.3"
 
     const val androidTestInstrumentation = "pl.llp.aircasting.helpers.EspressoRunner"
