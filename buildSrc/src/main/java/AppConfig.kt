40705b14ac86aa40995a0549f314bfd651212693
kgetman <kgetmanskii@gmail.com>
2023-03-25 18:31:07 +0100

    bump 2.5.2(243)

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -2,8 +2,8 @@ object AppConfig {
     const val compileSdk = 33
     const val minSdk = 21
     const val targetSdk = 33
-    const val versionCode = 242
-    const val versionName = "2.5.1"
+    const val versionCode = 243
+    const val versionName = "2.5.2"
     const val buildToolsVersion = "30.0.3"
 
     const val androidTestInstrumentation = "pl.llp.aircasting.helpers.EspressoRunner"
