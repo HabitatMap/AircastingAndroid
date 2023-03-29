38d02f5a0057423b6c947543067e5a64223b9d8b
kgetman <kgetmanskii@gmail.com>
2023-03-29 12:27:02 +0200

    refactor(DI > ViewModelModule): remove bind method for ViewModels that don't use ViewModelFactory and are injected directly

--- app/src/main/java/pl/llp/aircasting/di/modules/ViewModelModule.kt
+++ app/src/main/java/pl/llp/aircasting/di/modules/ViewModelModule.kt
@@ -7,42 +7,11 @@ import dagger.MapKey
 import dagger.Module
 import dagger.multibindings.IntoMap
 import pl.llp.aircasting.di.factories.ViewModelFactory
-import pl.llp.aircasting.ui.view.screens.dashboard.bottomsheet.menu_options.edit.EditSessionBottomSheetViewModel
-import pl.llp.aircasting.ui.view.screens.settings.my_account.MyAccountViewModel
-import pl.llp.aircasting.ui.viewmodel.CreateThresholdAlertBottomSheetViewModel
 import pl.llp.aircasting.ui.viewmodel.SearchFollowViewModel
-import pl.llp.aircasting.ui.viewmodel.SessionsViewModel
 import kotlin.reflect.KClass
 
 @Module
 abstract class ViewModelModule {
-    @Binds
-    @IntoMap
-    @ViewModelKey(SessionsViewModel::class)
-    internal abstract fun bindSessionsViewModel(
-        viewModel: SessionsViewModel
-    ): ViewModel
-    @Binds
-    @IntoMap
-    @ViewModelKey(MyAccountViewModel::class)
-    internal abstract fun bindMyAccountViewModel(
-        viewModel: MyAccountViewModel
-    ): ViewModel
-
-    @Binds
-    @IntoMap
-    @ViewModelKey(EditSessionBottomSheetViewModel::class)
-    internal abstract fun bindEditSessionBottomSheetViewModel(
-        viewModel: EditSessionBottomSheetViewModel
-    ): ViewModel
-
-    @Binds
-    @IntoMap
-    @ViewModelKey(CreateThresholdAlertBottomSheetViewModel::class)
-    internal abstract fun bindCreateThresholdAlertBottomSheetViewModel(
-        viewModel: CreateThresholdAlertBottomSheetViewModel
-    ): ViewModel
-
     @Binds
     @IntoMap
     @ViewModelKey(SearchFollowViewModel::class)
