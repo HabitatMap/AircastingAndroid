6a421efa745d0be247c85aba138a4af6ba416084
sebastianstrycharz <strycharz97@gmail.com>
2023-09-08 15:12:54 +0200

    feat(AB Mini sync): add last known location for mobile session where missing

--- app/src/main/java/pl/llp/aircasting/util/helpers/sensor/airbeam3/sync/sessionProcessor/SDCardSessionsProcessor.kt
+++ app/src/main/java/pl/llp/aircasting/util/helpers/sensor/airbeam3/sync/sessionProcessor/SDCardSessionsProcessor.kt
@@ -14,7 +14,7 @@ import java.io.File
 abstract class SDCardSessionsProcessor(
     private val mSDCardSessionFileHandler: SDCardSessionFileHandler,
     val mSessionsRepository: SessionsRepository,
-    private val mMeasurementStreamsRepository: MeasurementStreamsRepository,
+    val mMeasurementStreamsRepository: MeasurementStreamsRepository,
     val mMeasurementsRepository: MeasurementsRepositoryImpl,
     private val lineParameterHandler: CSVLineParameterHandler,
 ) {
