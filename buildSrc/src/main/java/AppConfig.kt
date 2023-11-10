262ca13d8d33ebc8e27597cfc87981fdee72acfe
sebastianstrycharz <strycharz97@gmail.com>
2023-11-10 16:39:39 +0100

    bump 3.0.3-exp(253)

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -2,8 +2,8 @@ object AppConfig {
     const val compileSdk = 33
     const val minSdk = 21
     const val targetSdk = 33
-    const val versionCode = 252
-    const val versionName = "3.0.2-exp"
+    const val versionCode = 253
+    const val versionName = "3.0.3-exp"
     const val buildToolsVersion = "30.0.3"
 
     const val androidTestInstrumentation = "pl.llp.aircasting.helpers.EspressoRunner"
