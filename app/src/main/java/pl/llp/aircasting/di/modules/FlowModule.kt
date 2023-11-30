913605ba0b0e977802c70419af86d4c85ae5db52
Seb <strycharz97@gmail.com>
2023-11-30 15:46:52 +0100

    fix(stuck at connecting fragment): add DI for connection status flow

--- app/src/main/java/pl/llp/aircasting/di/modules/FlowModule.kt
+++ app/src/main/java/pl/llp/aircasting/di/modules/FlowModule.kt
@@ -3,6 +3,8 @@ package pl.llp.aircasting.di.modules
 import dagger.Module
 import dagger.Provides
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import pl.llp.aircasting.data.model.AirbeamConnectionStatus
 import pl.llp.aircasting.di.UserSessionScope
 import javax.inject.Qualifier
 
@@ -12,8 +14,17 @@ class FlowModule {
     @BatteryLevelFlow
     @UserSessionScope
     fun provideBatteryLevelFlow(): MutableSharedFlow<Int> = MutableSharedFlow()
+
+    @Provides
+    @AirbeamConnectionStatusFlow
+    @UserSessionScope
+    fun provideAirbeamConnectionStatusFlow(): MutableStateFlow<AirbeamConnectionStatus?> = MutableStateFlow(null)
 }
 
 @Retention(AnnotationRetention.BINARY)
 @Qualifier
 annotation class BatteryLevelFlow
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class AirbeamConnectionStatusFlow
