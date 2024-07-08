5a06c74d3e9b6198054498e21d61059152d30f57
kgetman <kgetmanskii@gmail.com>
2024-07-08 17:10:02 +0200

    fix(Google play notice): update target SDK to 34

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -1,7 +1,7 @@
 object AppConfig {
-    const val compileSdk = 33
+    const val compileSdk = 34
     const val minSdk = 21
-    const val targetSdk = 33
+    const val targetSdk = 34
     const val versionCode = 263
     const val versionName = "3.0.5"
     const val buildToolsVersion = "30.0.3"
