1e7387ef9ac07d85c0fe6c8034adb009769eff95
Marta Panuszewska <marta.panuszewska@lunarlogic.io>
2024-12-03 17:17:44 +0100

    chore: Bump version

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -2,8 +2,8 @@ object AppConfig {
     const val compileSdk = 34
     const val minSdk = 21
     const val targetSdk = 34
-    const val versionCode = 270
-    const val versionName = "3.0.8"
+    const val versionCode = 271
+    const val versionName = "3.0.9"
     const val buildToolsVersion = "30.0.3"
 
     const val androidTestInstrumentation = "pl.llp.aircasting.helpers.EspressoRunner"
