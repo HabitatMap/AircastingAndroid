27c897cd01fbab2a3e74396a4f91c6807eb85d7c
kgetman <kgetmanskii@gmail.com>
2025-06-03 11:23:37 +0200

    refactor(AB Mini session under 1 minute not getting finished during SD Sync): add missing logic to finisher

--- app/src/main/java/pl/llp/aircasting/util/helpers/sensor/common/SessionFinisher.kt
+++ app/src/main/java/pl/llp/aircasting/util/helpers/sensor/common/SessionFinisher.kt
@@ -2,8 +2,11 @@ package pl.llp.aircasting.util.helpers.sensor.common
 
 import android.util.Log
 import pl.llp.aircasting.data.api.util.TAG
+import pl.llp.aircasting.data.local.repository.MeasurementsRepository
 import pl.llp.aircasting.data.local.repository.SessionsRepository
 import pl.llp.aircasting.data.model.Session
+import pl.llp.aircasting.util.Settings
+import java.util.Date
 import javax.inject.Inject
 
 fun interface SessionFinisher {
@@ -12,13 +15,24 @@ fun interface SessionFinisher {
 
 class DefaultSessionFinisher @Inject constructor(
     private val sessionsRepository: SessionsRepository,
-): SessionFinisher {
+    private val settings: Settings,
+    private val measurementRepository: MeasurementsRepository,
+) : SessionFinisher {
     override suspend fun invoke(uuid: String) {
         sessionsRepository.getSessionByUUID(uuid)
             ?.takeIf { it.isNotFinished }
-            .also { if (it == null)
-                Log.w(TAG, "Tried to finish $uuid session which is already finished!")
+            .also {
+                if (it == null)
+                    Log.w(TAG, "Tried to finish $uuid session which is already finished!")
             }
-            ?.let { sessionsRepository.updateSessionStatus(it.uuid, Session.Status.FINISHED) }
+            ?.apply {
+                copy(
+                    status = Session.Status.FINISHED,
+                    endTime = measurementRepository.lastMeasurementTime(id) ?: Date()
+                ).let { updatedSession ->
+                    sessionsRepository.update(updatedSession)
+                    settings.decreaseActiveMobileSessionsCount()
+                }
+            }
     }
 }
