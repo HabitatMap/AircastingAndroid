ed061b1d4627968d7d4d18b0954edda212ebdb06
kgetman <kgetmanskii@gmail.com>
2025-07-01 12:15:44 +0200

    fix(can't share fixed session): update the urlLocation after uploading a fixed session

--- app/src/main/java/pl/llp/aircasting/data/api/services/FixedSessionUploadService.kt
+++ app/src/main/java/pl/llp/aircasting/data/api/services/FixedSessionUploadService.kt
@@ -3,18 +3,19 @@ package pl.llp.aircasting.data.api.services
 import pl.llp.aircasting.data.api.GzippedParams
 import pl.llp.aircasting.data.api.params.CreateSessionBody
 import pl.llp.aircasting.data.api.params.SessionParams
+import pl.llp.aircasting.data.local.repository.SessionsRepository
 import pl.llp.aircasting.data.model.Session
 import pl.llp.aircasting.di.UserSessionScope
 import pl.llp.aircasting.util.exceptions.ErrorHandler
 import pl.llp.aircasting.util.exceptions.UnexpectedAPIError
-import java.util.*
+import java.util.Date
 import javax.inject.Inject
-import javax.inject.Singleton
 
 @UserSessionScope
 class FixedSessionUploadService @Inject constructor(
     @Authenticated private val apiService: ApiService,
-    private val errorHandler: ErrorHandler
+    private val errorHandler: ErrorHandler,
+    private val sessionsRepository: SessionsRepository,
 ) {
     suspend fun upload(session: Session): Result<Unit> = runCatching {
         session.endTime = Date()
@@ -27,6 +28,7 @@ class FixedSessionUploadService @Inject constructor(
         if (!response.isSuccessful) {
             throw UnexpectedAPIError()
         }
+        sessionsRepository.updateUrlLocation(session, response.body()?.location)
     }.onFailure { throwable ->
         errorHandler.handle(UnexpectedAPIError(throwable))
     }
