34cf3080e1b4f9dd3cba532f2ac9ef7961576256
kgetman <kgetmanskii@gmail.com>
2025-07-02 11:03:34 +0200

    fix(Play Store target sdk too low): set target sdk to 36 (Android 16)

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -1,7 +1,7 @@
 object AppConfig {
     const val compileSdk = 34
     const val minSdk = 21
-    const val targetSdk = 34
+    const val targetSdk = 36
     const val versionCode = 282
     const val versionName = "3.3.0"
     const val buildToolsVersion = "30.0.3"
