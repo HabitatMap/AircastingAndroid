a90390be050983a670eb88e22b20e32e47381748
kgetman <kgetmanskii@gmail.com>
2026-05-20 15:10:34 +0200

    bump version 4.0.4-exp(293)

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -2,8 +2,8 @@ object AppConfig {
     const val compileSdk = 34
     const val minSdk = 21
     const val targetSdk = 36
-    const val versionCode = 292
-    const val versionName = "4.0.3-exp"
+    const val versionCode = 293
+    const val versionName = "4.0.4-exp"
     const val buildToolsVersion = "30.0.3"
 
     const val androidTestInstrumentation = "pl.llp.aircasting.helpers.EspressoRunner"
